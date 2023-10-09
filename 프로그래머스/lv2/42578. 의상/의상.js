113eb808370868350319d8b6057bb45b7b24d3d8
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2023-10-09 13:11:21 +0900

    [level 2] Title: 의상, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.js
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -1,23 +1,19 @@
 function solution(clothes) {
     var answer = 1;
-    // categoryLengthOfclothes
     // headgear : 2  , eyewear : 1 -> 3*2 - 1(전부 선택하지 않음)
     const categoryNumber = new Map()
-    const categorySet = new Set()
-    for(const [clothe,category] of clothes){
-        categorySet.add(category)
-    }
     
-    for (const whatCategory of categorySet){
-            categoryNumber.set(whatCategory,0)
+    
+    for(const [clothe,category] of clothes){
+        if (categoryNumber.has(category)){
+            categoryNumber.set(category,categoryNumber.get(category)+1)
+        }else{
+            categoryNumber.set(category,1)
         }
-    
-    for(const [clothe,category] of clothes){
-        const number = categoryNumber.get(category)
-        categoryNumber.set(category,number+1)
+
+        
     }
     for (let num of categoryNumber.values()){
-        // console.log(num)
         answer *= (num+1)
     }
     
