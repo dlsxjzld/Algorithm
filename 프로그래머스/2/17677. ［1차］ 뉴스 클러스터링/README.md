425d695493a784576e9aab0202c627038ca3ee1a
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2023-10-12 13:21:07 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 뉴스 클러스터링 - 17677 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17677) 
 
 ### 성능 요약
 
-메모리: 33.3 MB, 시간: 0.25 ms
+메모리: 33.4 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 0:50:39
+2023년 10월 4일 13:21:5
 
 ### 문제 설명
 
--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,16 +1,21 @@
 function solution(str1, str2) {
     var answer = 0;
+    // 대소문자 구분 안함 -> 소문자로 통일
     str1 = str1.toLowerCase()
     str2 = str2.toLowerCase()
     const arr1 = []
     const arr2 = []
+    // str1 과 str2 영문자만 있는지 확인 후 새 배열 만들기
     for (let i = 0; i < str1.length - 1; i++) {
+        // 두 글자씩 끊어서 확인
         const str = str1.slice(i, i+2);
         if (str[0] >= "a" && str[0] <= "z" && str[1] >= "a" && str[1] <= "z") {
           arr1.push(str);
         }
       }
+    // str1 과 str2 영문자만 있는지 확인 후 새 배열 만들기
     for (let i = 0; i < str2.length - 1; i++) {
+        // 두 글자씩 끊어서 확인
         const str = str2.slice(i, i+2);
         if (str[0] >= "a" && str[0] <= "z" && str[1] >= "a" && str[1] <= "z") {
           arr2.push(str);
@@ -23,11 +28,16 @@ function solution(str1, str2) {
     for(let i =0;i<arr1.length;i++){
         if(arr2.indexOf(arr1[i]) >=0){
             inter.push(arr1[i])
+            // arr2 에서 교집합 빼기 
             arr2.splice(arr2.indexOf(arr1[i]),1)
         }
+        // 합집합에 arr1 넣기
         union.push(arr1[i])
     }
+    // arr2 는 arr2에서 교집합이 빠진 상태
+    // 합집합 = arr1 + arr2 - 교집합
     for(let i=0;i<arr2.length;i++){
+        // 합집합에 바뀐 arr2 넣기
         union.push(arr2[i])
     }
 
