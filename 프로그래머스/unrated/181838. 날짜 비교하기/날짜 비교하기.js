ac0950cb35b23a65faf8c4b68ecef8e95e8101d1
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2023-10-17 09:59:45 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,21 +1,5 @@
 function solution(date1, date2) {
     var answer = 0;
-    if(date1[0]<date2[0]){
-        answer = 1
-    }else if (date1[0] === date2[0]){
-        if(date1[1] < date2[1]){
-            answer = 1
-        }else if(date1[1] === date2[1]){
-            if(date1[2] < date2[2]){
-            answer = 1
-            }else{
-                answer = 0
-            }
-        }else{
-            answer = 0
-        }
-    }else{
-        answer = 0
-    }
+    answer = date1.join('') - date2.join('') <0 ? 1:0
     return answer;
 }
