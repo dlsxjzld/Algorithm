5f36dffaf41d6c6335be4e61a69ad6a391cb9c3c
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2023-10-20 09:23:24 +0900

    [level 2] Title: 모음 사전, Time: 2.94 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=javascript) 
 
 ### 성능 요약
 
-메모리: 37.1 MB, 시간: 3.27 ms
+메모리: 37 MB, 시간: 2.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 18:20:13
+2023년 10월 5일 9:23:22
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,13 +1,16 @@
 function solution(word) {
     var answer = 0;
     const letter = ['A','E','I','O','U']
-    const words = []
-    const dict = []
+    const words = [] // 사전에 들어갈 단어
+    const dict = [] // 사전
     
     const backtracking = function(array){
         if(array.length <=5){
+            // 길이가 5이하인 단어면 사전에 추가
             dict.push(array.join(''))
             for(let i=0;i<5;i++){
+                // 알파벳이 중복으로 들어가도 되므로 
+                // for문 i=0부터 다시 시작
                 array.push(letter[i])
                 backtracking(array)
                 array.pop(letter[i])
@@ -16,6 +19,9 @@ function solution(word) {
         }
         return
     }
+    // 백트래킹 시작
+    // 가지치기 조건 -> 길이가 5이하인 단어만 찾기
+    
     for(let i=0;i<5;i++){
         words.push(letter[i])
         backtracking(words)
