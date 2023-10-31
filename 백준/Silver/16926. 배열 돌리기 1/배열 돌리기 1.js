c9e98f99f522b2a827812c8472a8e7a999f25a21
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2023-10-31 16:51:24 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 496 ms, Memory: 19140 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.js
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.js
@@ -1,4 +1,3 @@
-
 const input = require("fs")
     .readFileSync("/dev/stdin")
     .toString()
@@ -7,42 +6,50 @@ const input = require("fs")
 
 const [n, m, r] = input[0].split(" ").map((v) => Number(v));
 input.splice(0, 1);
-const array = input.map((v) => v.split(" ").map((num) => Number(num)));
+const array = input.map((v) => v.split(" ").map(Number));
 
-// console.log(array);
 let x = 0;
 let y = 0;
 let cnt = Math.floor(Math.min(n, m) / 2);
 
 for (let ridx = 0; ridx < r; ridx++) {
     for (let cidx = 0; cidx < cnt; cidx++) {
+        // cidx == depth
         x = cidx;
         y = cidx;
-        let prevValue = array[x][y];
-        // 좌
+        let prevValue = array[x][y]; // [0][0] , [1][1] ....
+
+        // 좌 ( y === 0 으로 고정 ) x 증가
         for (let idx = cidx + 1; idx < n - cidx; idx++) {
             x = idx;
+
             let temp = array[x][y];
             array[x][y] = prevValue;
             prevValue = temp;
         }
-        // 하
+
+        // 하 ( x === n-1 으로 고정 ) y 증가
         for (let idx = cidx + 1; idx < m - cidx; idx++) {
             y = idx;
+
             let temp = array[x][y];
             array[x][y] = prevValue;
             prevValue = temp;
         }
-        // 우
+
+        // 우 ( y === m-1 으로 고정 ) x 감소
         for (let idx = cidx + 1; idx < n - cidx; idx++) {
             x = n - idx - 1;
+
             let temp = array[x][y];
             array[x][y] = prevValue;
             prevValue = temp;
         }
-        // 상
+
+        // 상 ( x === 0 으로 고정 ) y 감소
         for (let idx = cidx + 1; idx < m - cidx; idx++) {
             y = m - idx - 1;
+
             let temp = array[x][y];
             array[x][y] = prevValue;
             prevValue = temp;
@@ -50,7 +57,6 @@ for (let ridx = 0; ridx < r; ridx++) {
     }
 }
 
-// console.log(array);
 const answer = array
     .map((v) => v.map((num) => num.toString()).join(" "))
     .join("\n");
