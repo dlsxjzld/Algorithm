3e96ab8fa96ba3a35bbb2e06f9df454a527ae6b6
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2023-11-03 17:47:08 +0900

    [Silver II] Title: 비슷한 단어, Time: 120 ms, Memory: 9432 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.js
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.js
@@ -10,17 +10,27 @@ const target = input[0];
 input.splice(0, 1);
 const words = input;
 
+// target : 첫 단어
+// words : 비슷한 단어인지 확인할 단어들
 const replacedWord = words.map((word) => {
+    // targetWord : 첫 단어
     let targetWord = target.split("").join("");
+
     target.split("").forEach((char) => {
+        // 단어가 첫 단어의 문자들을 갖고 있는지 확인
         if (word.includes(char)) {
             word = word.replace(char, "");
+            // 단어에서 첫 단어와 겹치는 문자들 지우기
             targetWord = targetWord.replace(char, "");
+            // 첫 단어에서 겹치는 문자들 지우기
         }
     });
-    return [word, targetWord.length];
+    // 단어의 남은 문자의 길이 , 첫 단어에서 겹치는 문자 지우고 남은 길이
+    return [word.length, targetWord.length];
 });
 
 console.log(
-    replacedWord.filter(([v, cnt]) => v.length <= 1 && cnt <= 1).length
+    replacedWord.filter(
+        ([wordCnt, targetCnt]) => wordCnt <= 1 && targetCnt <= 1
+    ).length
 );
