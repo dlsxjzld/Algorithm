668bcd8922ccba9350c9058cb28ed4c7a3eb9dbe
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2023-12-05 13:50:48 +0900

    [Gold V] Title: 호텔, Time: 132 ms, Memory: 10812 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.js
@@ -0,0 +1,29 @@
+const input = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+
+const [c, n] = input[0].split(' ').map(Number)
+const cities = input.slice(1, 1 + n).map((row) => row.split(' ').map(Number))
+const sortedCities = cities.sort((a, b) => a[1] - b[1])
+const dp = Array.from({ length: 1101 }, () => Infinity)
+
+// c 명 늘리기
+// n 개의 도시
+dp[0] = 0
+// 각 도시 : [비용 , 고객의 수]
+for (let [cost, customer] of sortedCities) {
+  if (dp[customer] > cost) {
+    dp[customer] = cost
+  }
+  for (let idx = 1; idx < c + 1; idx++) {
+    if (idx < customer) {
+      dp[idx] = Math.min(dp[idx], dp[customer])
+    } else {
+      dp[idx] = Math.min(dp[idx], dp[idx - customer] + dp[customer])
+    }
+  }
+}
+
+console.log(dp[c])
