7b8d9f6fb3ab9d88f5239db6eb361fd622e958b4
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2023-12-09 23:56:07 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 132 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.js
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.js
@@ -2,57 +2,53 @@ const input = require('fs')
   .readFileSync('/dev/stdin')
   .toString()
   .trim()
-  .split('\n')
+  .split('\n');
 
 // 1. 지구 -> 달로 가는 경우 우주선이 움직일 수 있는 방향은 아래와 같다.
 // 2. 우주선은 전에 움직인 방향으로 움직일 수 없다. 즉, 같은 방향으로 두번 연속으로 움직일 수 없다.
 // 3. 연료를 최대한 아끼며 지구의 어느위치에서든 출발하여 달의 어느위치든 착륙하는 것
-const MAX = Number.MAX_SAFE_INTEGER
-const [n, m] = input[0].split(' ').map(Number)
+const MAX = Number.MAX_SAFE_INTEGER;
+const [n, m] = input[0].split(' ').map(Number);
 const distance = [
-  Array.from({ length: m }, () => 0),
-  ...input.slice(1, n + 1).map((row) => row.split(' ').map(Number)),
-]
+    Array.from({ length: m }, () => 0),
+  ...input.slice(1, 1 + n).map((row) => row.split(' ').map(Number)),
+];
+
 
 const dp = Array.from({ length: n + 1 }, () =>
-  Array.from({ length: m }, () => Array.from({ length: 3 }, () => 0))
-)
+  Array.from({ length: m }, () => Array.from({ length: 3 }, () => MAX))
+);
 
-// dp[ridx][cidx][direction] = direction에 해당하는 방향으로 들어오는 [ridx][cidx]]까지의 연료의 최소값
+const direction = [
+  [-1, 1],
+  [-1, 0],
+  [-1, -1],
+]; // 남서, 남, 남동
 
+for (let i = 0; i < m; i++) {
+  for (let j = 0; j < 3; j++) {
+    dp[0][i][j] = 0;
+  }
+}
 
-for (let ridx = 1; ridx < n + 1; ridx++) {
-  for (let cidx = 0; cidx < m; cidx++) {
-    if (cidx === 0) {
-      // 가장 왼쪽 벽
-      dp[ridx][cidx][0] =
-        distance[ridx][cidx] +
-        Math.min(dp[ridx - 1][cidx + 1][1], dp[ridx - 1][cidx + 1][2])
-      dp[ridx][cidx][1] =
-        distance[ridx][cidx] +
-        Math.min(dp[ridx - 1][cidx][0], dp[ridx - 1][cidx][2])
-      dp[ridx][cidx][2] = MAX
-    } else if (cidx === m - 1) {
-      // 가장 오른쪽 벽
-      dp[ridx][cidx][0] = MAX
-      dp[ridx][cidx][1] =
-        distance[ridx][cidx] +
-        Math.min(dp[ridx - 1][cidx][0], dp[ridx - 1][cidx][2])
-      dp[ridx][cidx][2] =
-        distance[ridx][cidx] +
-        Math.min(dp[ridx - 1][cidx - 1][0], dp[ridx - 1][cidx - 1][1])
-    } else {
-      dp[ridx][cidx][0] =
-        distance[ridx][cidx] +
-        Math.min(dp[ridx - 1][cidx + 1][1], dp[ridx - 1][cidx + 1][2])
-      dp[ridx][cidx][1] =
-        distance[ridx][cidx] +
-        Math.min(dp[ridx - 1][cidx][0], dp[ridx - 1][cidx][2])
-      dp[ridx][cidx][2] =
-        distance[ridx][cidx] +
-        Math.min(dp[ridx - 1][cidx - 1][0], dp[ridx - 1][cidx - 1][1])
+for (let x = 1; x < n + 1; x++) {
+  for (let y = 0; y < m; y++) {
+    for (let idx = 0; idx < 3; idx++) {
+      const prevX = x + direction[idx][0];
+      const prevY = y + direction[idx][1];
+
+      if (prevY < 0 || prevY >= m) {
+        dp[x][y][idx] = MAX;
+        continue;
+      }
+      dp[x][y][idx] =
+        distance[x][y] +
+        Math.min(
+          dp[prevX][prevY][(idx + 1) % 3],
+          dp[prevX][prevY][(idx + 2) % 3]
+        );
     }
   }
 }
 
-console.log(Math.min(...dp[n].flat()))
+console.log(Math.min(...dp[n].flat()));
