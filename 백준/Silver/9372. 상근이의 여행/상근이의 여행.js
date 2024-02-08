5fb886506bca4fac818292edb2c90a3a74184893
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-02-08 22:29:43 +0900

    [Silver IV] Title: 상근이의 여행, Time: 184 ms, Memory: 22708 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.js
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.js
@@ -1,38 +1,15 @@
-const fs = require("fs");
-const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
 
-let number = Number(input[0]);
-let result = [];
-let idx = 1;
+  const T = Number(input.shift())
 
-for (let i = 0; i < number; i++) {
-    let arr = [];
-    let [nodeNum, edgeNum] = input[idx++].split(" ").map(Number);
-    for (let j = 0; j < edgeNum; j++) {
-        arr[j] = input[idx++].split(" ").map(Number);
-    }
 
-    const visited = new Array(nodeNum).fill(false);
-    visited[0] = true;
-    const queue = [0];
-    let count = 0;
+  for(let testcase = 0; testcase<T;testcase++){
+    const [n,m]= input.shift().split(' ').map(Number)
+    input.splice(0,m)
+    console.log(n-1)
 
-    while (queue.length) {
-        const head = queue.shift();
-        for (let node of arr) {
-            if (node[0] - 1 === head && !visited[node[1] - 1]) {
-                queue.push(node[1] - 1);
-                visited[node[1] - 1] = true;
-                count++;
-            } else if (node[1] - 1 === head && !visited[node[0] - 1]) {
-                queue.push(node[0] - 1);
-                visited[node[0] - 1] = true;
-                count++;
-            }
-        }
-        if (!visited.includes(false)) break;
-    }
-    result[i] = count;
-}
-
-console.log(result.join('\n'));
+  }
