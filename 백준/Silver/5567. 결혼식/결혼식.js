301d6a4fa9920f1ef6332c944bd9163ecbe2732a
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-02-11 17:59:14 +0900

    [Silver II] Title: 결혼식, Time: 212 ms, Memory: 15168 KB -BaekjoonHub

---
+++ 백준/Silver/5567. 결혼식/결혼식.js
@@ -0,0 +1,41 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const n = Number(input[0])
+const m = Number(input[1])
+const graph = Array.from({ length: n + 1 }, () => [])
+const visited = Array.from({ length: n + 1 }, () => false)
+const target = input.slice(2, 2 + m).map((row) => row.split(" ").map(Number))
+target.forEach(([a, b]) => {
+  graph[a].push(b)
+  graph[b].push(a)
+})
+let cnt = 0
+
+function bfs(start){
+  const queue = [[start,0]]
+  visited[start] = true
+
+  while(queue.length>0){
+    const [cur,depth] = queue.shift()
+    
+
+    for(let next of graph[cur]){
+      if(!visited[next]){
+        if(depth+1 <= 2 ){
+          cnt +=1
+        }
+        queue.push([next,depth+1])
+        visited[next] = true
+
+      }
+    }
+  }
+}
+
+bfs(1)
+
+console.log(cnt)
