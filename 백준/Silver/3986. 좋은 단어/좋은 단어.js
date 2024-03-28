62d7d0a5f232b84707755d262ac098b82b29d696
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-03-29 02:43:27 +0900

    [Silver IV] Title: 좋은 단어, Time: 244 ms, Memory: 20868 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.js
@@ -0,0 +1,31 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+
+  const n = Number(input[0])
+  const words = input.slice(1)
+
+  let answer = 0
+
+  for(let i=0; i<n;i++){
+    const word = words[i].split('')
+    const stack = []
+
+    while(word.length>0){
+      const letter = word.pop()
+      if(stack.length && stack.at(-1) ===letter){
+        stack.pop()
+        continue
+      }
+      stack.push(letter)
+    }
+
+    if(!stack.length){
+      answer +=1
+    }
+
+  }
+  console.log(answer)
