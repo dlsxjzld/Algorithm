d7814f47a33a64e7813c1fe370dc8d766fde78ef
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-02 23:57:16 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 380 ms, Memory: 33532 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.js
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.js
@@ -4,63 +4,62 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const [n, m] = input[0].split(" ").map(Number)
-const graph = input.slice(1, 1 + n).map((row) => row.split(" "))
-
-const distance = Array.from({ length: n }, () =>
-  Array.from({ length: m }, () => 0),
-)
+const [n, m] = input.shift().split(" ").map(Number)
+const graph = input.map((row) => row.split(" ").map(Number))
 const visited = Array.from({ length: n }, () =>
   Array.from({ length: m }, () => false),
 )
-const start = []
+
+let sx = -1
+let sy = -1
+
+const findStart = () => {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (graph[i][j] === 2) {
+        sx = i
+        sy = j
+        return
+      }
+    }
+  }
+}
+
+findStart()
+visited[sx][sy] = true
+graph[sx][sy] = 0
+
+
+const bfs = () => {
+  const queue = [[sx,sy]]
+  const move = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ]
+
+  while(queue.length>0){
+    const [x,y] = queue.shift()
+    
+    for(let i=0;i<4;i++){
+      const [nx,ny] = [x+move[i][0] , y+move[i][1]]
+      if(nx>=0 && ny>=0 && nx<n && ny<m && !visited[nx][ny] && graph[nx][ny] !==0) {
+        graph[nx][ny] = graph[x][y] + 1
+        queue.push([nx,ny])
+        visited[nx][ny] = true
+      }
+    }
+  }
+}
+bfs()
 
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < m; j++) {
-    if (graph[i][j] === "2") {
-      start.push([i, j])
-      visited[i][j] = true
+    if (graph[i][j] === 1 && !visited[i][j]) {
+      graph[i][j] = -1
     }
   }
 }
 
-function bfs(start) {
-  const queue = [...start]
-
-  const dx = [0, 0, 1, -1]
-  const dy = [1, -1, 0, 0]
-  while (queue.length > 0) {
-    const [x, y] = queue.shift()
-
-    for (let i = 0; i < 4; i++) {
-      const nx = x + dx[i]
-      const ny = y + dy[i]
-
-      if (
-        0 <= nx &&
-        nx < n &&
-        0 <= ny &&
-        ny < m &&
-        graph[nx][ny] === '1' &&
-        !visited[nx][ny]
-      ) {
-        visited[nx][ny] = true
-        distance[nx][ny] = distance[x][y] + 1
-        queue.push([nx, ny])
-      }
-    }
-  }
-
-  for(let i=0;i<n;i++){
-    for(let j=0;j<m;j++){
-      if(distance[i][j] === 0 && graph[i][j] === '1'){
-        distance[i][j] = -1
-      }
-    }
-  }
-}
-bfs(start)
-
-const answer = distance.map(row=>row.join(' '))
-answer.forEach(row => console.log(row))
-
+console.log(graph.map(row=>row.join(' ')).join('\n'))
