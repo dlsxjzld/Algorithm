9e1f8f0dd7b3520528d3bdb959ab8dded64d85e6
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-02 22:53:14 +0900

    [Silver I] Title: 미로 탐색, Time: 204 ms, Memory: 12252 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -5,27 +5,49 @@ const input = require("fs")
   .split("\n")
 
 const [n, m] = input[0].split(" ").map(Number)
+const graph = input.slice(1).map((row) => row.split("").map(Number))
 
-const graph = input.slice(1, 1 + n).map((row) => row.split("").map(Number))
-const visited = Array.from({length:n},()=>Array.from({length:m},()=>false))
-
-const dx = [0, 0, 1, -1]
-const dy = [1, -1, 0, 0]
+class Queue {
+  constructor() {
+    this.queue = []
+    this.front = 0
+    this.rear = 0
+  }
+  enqueue(value) {
+    this.queue[this.rear++] = value
+  }
+  dequeue() {
+    const value = this.queue[this.front]
+    delete this.queue[this.front]
+    this.front += 1
+    return value
+  }
+  size() {
+    return this.rear - this.front
+  }
+}
 
 const bfs = () => {
-  const queue = [[0,0]]
-  visited[0][0] = true
+  const queue = new Queue()
+  queue.enqueue([0, 0])
 
-  while(queue.length >0){
-    const [x,y] = queue.shift()
+  const move = [
+    [0, 1],
+    [0, -1],
+    [-1, 0],
+    [1, 0],
+  ]
 
-    for(let i=0;i<4;i++){
-      const [nx,ny] = [x+dx[i], y+dy[i]]
-      if(0<=nx && nx<n && 0<=ny && ny<m && !visited[nx][ny] && graph[nx][ny] === 1 ){
-        graph[nx][ny] = graph[x][y] +1
-        visited[nx][ny] = true
-        queue.push([nx,ny])
+  while (queue.size() > 0) {
+    const [x, y] = queue.dequeue()
+    for (let i = 0; i < 4; i++) {
+      const [nx, ny] = [x + move[i][0], y + move[i][1]]
+
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m || graph[nx][ny] !== 1) {
+        continue
       }
+      graph[nx][ny] = graph[x][y] + 1
+      queue.enqueue([nx, ny])
     }
   }
 }
