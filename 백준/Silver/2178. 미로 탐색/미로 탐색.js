59b96a8d01c73e843d4d64b7122b5152f23621d9
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-02 22:58:12 +0900

    [Silver I] Title: 미로 탐색, Time: 200 ms, Memory: 12168 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -7,30 +7,8 @@ const input = require("fs")
 const [n, m] = input[0].split(" ").map(Number)
 const graph = input.slice(1).map((row) => row.split("").map(Number))
 
-class Queue {
-  constructor() {
-    this.queue = []
-    this.front = 0
-    this.rear = 0
-  }
-  enqueue(value) {
-    this.queue[this.rear++] = value
-  }
-  dequeue() {
-    const value = this.queue[this.front]
-    delete this.queue[this.front]
-    this.front += 1
-    return value
-  }
-  size() {
-    return this.rear - this.front
-  }
-}
-
 const bfs = () => {
-  const queue = new Queue()
-  queue.enqueue([0, 0])
-
+  const queue = [[0, 0]]
   const move = [
     [0, 1],
     [0, -1],
@@ -38,15 +16,13 @@ const bfs = () => {
     [1, 0],
   ]
 
-  while (queue.size() > 0) {
-    const [x, y] = queue.dequeue()
+  while (queue.length > 0) {
+    const [x, y] = queue.shift()
     for (let i = 0; i < 4; i++) {
       const [nx, ny] = [x + move[i][0], y + move[i][1]]
-
       if (nx >= 0 && ny >= 0 && nx < n && ny < m && graph[nx][ny] === 1) {
-
         graph[nx][ny] = graph[x][y] + 1
-        queue.enqueue([nx, ny])
+        queue.push([nx, ny])
       }
     }
   }
@@ -54,4 +30,4 @@ const bfs = () => {
 
 bfs()
 
-console.log(graph[n-1][m-1])
+console.log(graph[n - 1][m - 1])
