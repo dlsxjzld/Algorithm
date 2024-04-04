be9ab630ecc62c4a1b7b8fa1e644d486d0ac62f2
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-04 17:58:53 +0900

    [Gold V] Title: 토마토, Time: 828 ms, Memory: 168268 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.js
+++ 백준/Gold/7576. 토마토/토마토.js
@@ -4,40 +4,25 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-  class Queue {
-    constructor() {
-        this.queue = [];
-        this.front = 0;
-        this.rear = 0;
-    }
-    enqueue(value) {
-        this.queue[this.rear++] = value;
-    }
-    dequeue() {
-        const returnValue = this.queue[this.front];
-        delete this.queue[this.front];
-        this.front += 1;
-        return returnValue;
-    }
-    size() {
-        return this.rear - this.front;
-    }
-  }
+
+
 const [m, n] = input[0].split(" ").map(Number)
 const graph = input.slice(1).map((row) => row.split(" ").map(Number))
 
-const startPosition = new Queue()
+let unrippen = 0 // 안익은 토마토
+let answer = 0
+
+const startPosition = []
 graph.forEach((row, ridx) =>
   row.forEach((val, cidx) => {
     if (val === 1) {
-      startPosition.enqueue([ridx, cidx])
+      startPosition.push([ridx, cidx,0])
+    }
+    else if(val === 0){
+      unrippen +=1
     }
   }),
 )
-
-let day = 0
-
-
 
 const bfs = (queue) => {
   const move = [
@@ -46,26 +31,34 @@ const bfs = (queue) => {
     [1, 0],
     [-1, 0],
   ]
+  let index = 0
 
-  while (queue.size() > 0) {
-    const [x, y] = queue.dequeue()
+  while (index <queue.length) {
+    const [x, y,day] = queue[index]
+    index +=1
+    answer = day
+
     for (let i = 0; i < 4; i++) {
       const [nx, ny] = [x + move[i][0], y + move[i][1]]
       if (nx < 0 || ny < 0 || nx >= n || ny >= m || graph[nx][ny] !== 0) {
         continue
       }
       graph[nx][ny] = graph[x][y] + 1
-      queue.enqueue([nx,ny])
+      queue.push([nx,ny,graph[nx][ny]])
+      unrippen -= 1
     }
   }
 }
 
-bfs(startPosition)
-const isAlreadyDone = graph.some((row) => row.some((value) => value === 0))
+if(unrippen === 0){
+  console.log(0)
+  return
+}else{
+  bfs(startPosition)
+}
 
-if (isAlreadyDone) {
+if(unrippen >0){
   console.log(-1)
-} else {
-  day = Math.max(...graph.flatMap((row)=>Math.max(...row)))
-  console.log(day === 0 ? 0 : day-1)
+}else{
+  console.log(answer-1)
 }
