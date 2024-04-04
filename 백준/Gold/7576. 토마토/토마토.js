094f3abef344c50830285dd567b428d09bc26b2c
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-04 18:35:17 +0900

    [Gold V] Title: 토마토, Time: 576 ms, Memory: 167008 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.js
+++ 백준/Gold/7576. 토마토/토마토.js
@@ -1,64 +1,56 @@
-const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
+const fs = require("fs");
+const file_path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(file_path).toString().split("\n");
+// your own code here
+const [col, row] = input[0].split(" ").map(Number);
+const box = Array.from({ length: row }, () => Array(col));
 
+const d_row = [-1, 1, 0, 0];
+const d_col = [0, 0, -1, 1];
 
+let unrippen = 0;
+const queue = [];
 
-const [m, n] = input[0].split(" ").map(Number)
-const graph = input.slice(1).map((row) => row.split(" ").map(Number))
+let idx = 0;
+for (let r = 0; r < row; r++) {
+  box[idx++] = input[r + 1].split(" ").map(Number);
+}
 
-let unrippen = 0 // 안익은 토마토
-let answer = 0
+let answer = 0;
 
-const startPosition = []
-graph.forEach((row, ridx) =>
-  row.forEach((val, cidx) => {
-    if (val === 1) {
-      startPosition.push([ridx, cidx,0])
-    }
-    else if(val === 0){
-      unrippen +=1
-    }
-  }),
-)
-
-const bfs = (queue) => {
-  const move = [
-    [0, 1],
-    [0, -1],
-    [1, 0],
-    [-1, 0],
-  ]
-  let index = 0
-
-  while (index <queue.length) {
-    const [x, y,day] = queue[index]
-    index +=1
-    answer = day
-
-    for (let i = 0; i < 4; i++) {
-      const [nx, ny] = [x + move[i][0], y + move[i][1]]
-      if (nx < 0 || ny < 0 || nx >= n || ny >= m || graph[nx][ny] !== 0) {
-        continue
-      }
-      graph[nx][ny] = graph[x][y] + 1
-      queue.push([nx,ny,graph[nx][ny]])
-      unrippen -= 1
+for (let r = 0; r < row; r++) {
+  for (let c = 0; c < col; c++) {
+    if (box[r][c] === 1) {
+      queue.push([r, c, 0]);
+    } else if (box[r][c] === 0) {
+      unrippen++;
     }
   }
 }
 
-if(unrippen === 0){
-  console.log(0)
-  return
-}else{
-  bfs(startPosition)
+if (unrippen === 0) {
+  console.log(0);
+  return;
+} else {
+  let index = 0;
+  while (index < queue.length) {
+    const [r, c, day] = queue[index++];
+    answer = day;
+
+    for (let d = 0; d < d_row.length; d++) {
+      const nr = r + d_row[d];
+      const nc = c + d_col[d];
+      if (nr >= 0 && nr < row && nc >= 0 && nc < col && box[nr][nc] === 0) {
+        box[nr][nc] = 1;
+        unrippen--;
+        queue.push([nr, nc, day + 1]);
+      }
+    }
+  }
 }
 
-if(unrippen >0){
-  console.log(-1)
-}else{
-  console.log(answer-1)
+if (unrippen > 0) {
+  console.log(-1);
+} else {
+  console.log(answer);
 }
