9845bdd97522d10174881330624aaada93c00aee
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-08 10:33:16 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 952 ms, Memory: 101300 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -4,31 +4,30 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const [N, M] = input[0].split(" ").map(Number)
-const graph = Array.from({ length: N + 1 }, () => [])
-const visited = Array.from({ length: N + 1 }, () => false)
-let cnt = 0
+const [n, m] = input[0].split(" ").map(Number)
+const graph = Array.from({ length: n + 1 }, () => [])
+const visited = Array.from({ length: n + 1 }, () => false)
 
-for (let i = 1; i < M + 1; i++) {
+for (let i = 1; i < 1 + m; i++) {
   const [u, v] = input[i].split(" ").map(Number)
   graph[u].push(v)
   graph[v].push(u)
 }
 
-const dfs = (node) => {
-  for (nextNode of graph[node]) {
-    if (!visited[nextNode]) {
-      visited[nextNode] = true
-      dfs(nextNode)
+const dfs = (start) => {
+  if (visited[start]) return 0
+  visited[start] = true
+
+  for (let next of graph[start]) {
+    if (!visited[next]) {
+      dfs(next)
     }
   }
+  return 1
+}
+let answer = 0
+for (let i = 1; i < n + 1; i++) {
+  answer += dfs(i)
 }
 
-for (let i = 1; i < N + 1; i++) {
-  if (!visited[i]) {
-    visited[i] = true
-    dfs(i)
-    cnt += 1
-  }
-}
-console.log(cnt)
+console.log(answer)
