04534e303de08b5326caa3c1f225775196847453
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-11 22:09:40 +0900

    [Bronze II] Title: 커트라인, Time: 132 ms, Memory: 9388 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.js
@@ -0,0 +1,9 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const [n,k] = input[0].split(' ').map(Number)
+const arr = input[1].split(' ').map(Number).sort((a,b)=>b-a)
+console.log(arr[k-1])
