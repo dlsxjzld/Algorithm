b858e74f5b346c2672fba981176356b28b3c7ad8
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-12 14:50:08 +0900

    [Silver II] Title: 점프 점프, Time: 156 ms, Memory: 17232 KB -BaekjoonHub

---
+++ 백준/Silver/14248. 점프 점프/점프 점프.js
@@ -0,0 +1,31 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const n = Number(input[0])
+const distance = input[1].split(" ").map(Number)
+const visited = Array.from({ length: n }, () => false)
+const start = Number(input[2])
+
+let index = 0
+const queue = [start-1]
+visited[start-1] = true
+let cnt = 1
+
+while (queue.length > index) {
+  const curr = queue[index++] 
+
+  for (let nx of [curr + distance[curr], curr - distance[curr]]) {
+    if (nx < 0 || nx > n-1 ) continue
+    
+    if (!visited[nx]) {
+      visited[nx] = true
+      queue.push(nx)
+      cnt += 1
+    }
+  }
+}
+
+console.log(cnt)
