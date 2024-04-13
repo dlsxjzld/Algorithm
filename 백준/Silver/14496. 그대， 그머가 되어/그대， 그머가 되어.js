056fe3da7ad0449a7f035e2d8ddb5bcc39c4a647
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-13 15:23:31 +0900

    [Silver II] Title: 그대, 그머가 되어, Time: 200 ms, Memory: 13596 KB -BaekjoonHub

--- 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.js
+++ 백준/Silver/14496. 그대， 그머가 되어/그대， 그머가 되어.js
@@ -4,43 +4,42 @@ const input = require("fs")
   .trim()
   .split("\n")
 
+const [a, b] = input[0].split(" ").map(Number)
 
-  const [a,b] = input[0].split(' ').map(Number)
+const [n, m] = input[1].split(" ").map(Number)
+const graph = Array.from({ length: n + 1 }, () => [])
+const visited = Array.from({ length: n + 1 }, () => false)
 
-  const [n,m]= input[1].split(' ').map(Number)
-  const graph = Array.from({length:n+1},()=>[])
-  const visited = Array.from({length:n+1},()=>false)
+let answer = Number.MAX_SAFE_INTEGER
 
-  let answer =Number.MAX_SAFE_INTEGER
+for (let i = 2; i < m + 2; i++) {
+  const [x, y] = input[i].split(" ").map(Number)
+  graph[x].push(y)
+  graph[y].push(x)
+}
 
-  for(let i=2;i<m+2;i++){
-    const [x,y] = input[i].split(' ').map(Number)
-    graph[x].push(y)
-    graph[y].push(x)
-  }
+const bfs = (start, end) => {
+  const queue = [[start, 0]]
+  visited[start] = true
+  let index = 0
 
-  const bfs = (start,end)=>{
-    const queue=[[start,0]]
-    visited[start] = true
-    let index = 0
+  while (queue.length > index) {
+    const [curr, dist] = queue[index++]
 
-    while(queue.length>index){
-      const [curr,dist] = queue[index++]
-
-      if(curr === end){
-        answer = Math.min(answer,dist)
-      }
-
-      for(let next of graph[curr]){
-        if(!visited[next]){
-          visited[next] = true
-          queue.push([next,dist+1])
-        }
-      }
+    if (curr === end) {
+      answer = Math.min(answer, dist)
+      break
     }
 
+    for (let next of graph[curr]) {
+      if (!visited[next]) {
+        visited[next] = true
+        queue.push([next, dist + 1])
+      }
+    }
   }
+}
 
-  bfs(a,b)
+bfs(a, b)
+console.log(answer === Number.MAX_SAFE_INTEGER ? -1 : answer)
 
-  console.log(answer === Number.MAX_SAFE_INTEGER ? -1 : answer)
