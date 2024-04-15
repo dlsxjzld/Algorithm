633e455b994b339815f982dd3e9c0b9d2678050d
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-15 17:32:08 +0900

    [Silver I] Title: 숨바꼭질, Time: 288 ms, Memory: 27300 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -0,0 +1,28 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+
+const [n,k] = input[0].split(' ').map(Number)
+const visited = Array(100001).fill(false)
+
+const queue = [[n,0]]
+visited[n] = true
+let index = 0
+
+while(queue.length>index){
+  const [curr,dist] = queue[index++]
+  if(curr === k){
+    console.log(dist)
+    break
+  }
+
+  for(let next of [curr-1,curr+1,curr*2]){
+    if(next>=0 && next <= 100000 && !visited[next]){
+      visited[next] = true
+      queue.push([next,dist+1])
+    }
+  }
+}
