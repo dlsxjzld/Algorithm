913b64d713bccf9f1622e3d78e7eecf93a81c503
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-15 17:33:46 +0900

    [Silver I] Title: 숨바꼭질, Time: 272 ms, Memory: 27184 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -4,25 +4,26 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-
-const [n,k] = input[0].split(' ').map(Number)
+const [n, k] = input[0].split(" ").map(Number)
 const visited = Array(100001).fill(false)
 
-const queue = [[n,0]]
+const queue = [[n, 0]]
 visited[n] = true
 let index = 0
 
-while(queue.length>index){
-  const [curr,dist] = queue[index++]
-  if(curr === k){
+while (queue.length > index) {
+  const [curr, dist] = queue[index++]
+  if (curr === k) {
     console.log(dist)
     break
   }
 
-  for(let next of [curr-1,curr+1,curr*2]){
-    if(next>=0 && next <= 100000 && !visited[next]){
-      visited[next] = true
-      queue.push([next,dist+1])
+  for (let next of [curr - 1, curr + 1, curr * 2]) {
+    if (next < 0 || next > 100000 || visited[next]) {
+      continue
     }
+
+    visited[next] = true
+    queue.push([next, dist + 1])
   }
 }
