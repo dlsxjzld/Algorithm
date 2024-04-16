b8968001c4888d7ac7a0b27c39c37503f920ca2b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-16 22:28:27 +0900

    [Silver I] Title: 숨바꼭질, Time: 228 ms, Memory: 16960 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,29 +1,74 @@
-const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
+const fs = require('fs');
+const PATH =
+  process.platform === 'linux' ? '/dev/stdin' : './baekjon/input.txt';
 
-const [n, k] = input[0].split(" ").map(Number)
-const visited = Array(100001).fill(false)
+const input = fs.readFileSync(PATH).toString().trim().split('\n');
 
-const queue = [[n, 0]]
-visited[n] = true
-let index = 0
-
-while (queue.length > index) {
-  const [curr, dist] = queue[index++]
-  if (curr === k) {
-    console.log(dist)
-    break
+class Queue {
+  constructor() {
+    this.start = null;
+    this.end = null;
+    this.size = 0;
   }
 
-  for (let next of [curr - 1, curr + 1, curr * 2]) {
-    if (next < 0 || next > 100000 || visited[next]) {
-      continue
+  enqueue(val, time) {
+    const node = new Node(val, time);
+    if (this.start === null) {
+      this.start = node;
+      this.end = node;
+    } else {
+      this.end.next = node;
+      this.end = node;
     }
+    this.size++;
+  }
 
-    visited[next] = true
-    queue.push([next, dist + 1])
+  dequeue() {
+    if (this.size === 0) throw new Error('empty');
+    const deleteNode = this.start;
+    this.start = deleteNode.next;
+    deleteNode.next = null;
+    this.size--;
+    if (this.size === 0) this.end = null;
+
+    return deleteNode;
   }
 }
+
+class Node {
+  constructor(val, time) {
+    this.val = val;
+    this.time = time;
+    this.next = null;
+  }
+}
+
+const [n, k] = input[0].split(' ').map(Number);
+
+const arr = new Array(100001).fill(Infinity);
+arr[n] = 0;
+const q = new Queue();
+q.enqueue(n, 0);
+
+while (q.size > 0) {
+  const { val, time } = q.dequeue();
+
+  for (let i of [-1, 1, 2]) {
+    let next;
+    if (i === 1) {
+      next = val + 1;
+    } else if (i === -1) {
+      next = val - 1;
+    } else {
+      next = val * 2;
+    }
+
+    if (next < 0 || next > 100001) continue;
+    if (arr[next] > time + 1) {
+      arr[next] = time + 1;
+      q.enqueue(next, time + 1);
+    }
+  }
+}
+
+console.log(arr[k]);
