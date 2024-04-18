5baf6fb8b6e1e6ccdf8c311e2b43ac71f3e058eb
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-18 12:56:13 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 280 ms, Memory: 16440 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.js
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.js
@@ -13,10 +13,9 @@ const move = [
   [-1, 0],
 ]
 
-const bfs = ({ i: sx, j: sy, graph: _graph, visited: _visited }) => {
+const bfs = ({ i: sx, j: sy, graph: _graph }) => {
   const queue = [[sx, sy]]
 
-  _visited[sx][sy] = true
   let index = 0
 
   while (queue.length > index) {
@@ -29,24 +28,19 @@ const bfs = ({ i: sx, j: sy, graph: _graph, visited: _visited }) => {
         nx >= _graph.length ||
         ny < 0 ||
         ny >= _graph[0].length ||
-        _visited[nx][ny] ||
         _graph[nx][ny] === "."
       ) {
         continue
       }
       queue.push([nx, ny])
-      _visited[nx][ny] = true
+      _graph[nx][ny] = "."
     }
   }
-  return 1
 }
 
 for (let tc = 0; tc < T; tc++) {
   const [h, w] = input.shift().split(" ").map(Number)
   const graph = []
-  const visited = Array.from({ length: h }, () =>
-    Array.from({ length: w }, () => false),
-  )
 
   for (let r = 0; r < h; r++) {
     graph.push(input.shift().split(""))
@@ -54,8 +48,9 @@ for (let tc = 0; tc < T; tc++) {
   let cnt = 0
   for (let i = 0; i < h; i++) {
     for (let j = 0; j < w; j++) {
-      if (graph[i][j] === "#" && !visited[i][j]) {
-        cnt += bfs({ i, j, graph, visited })
+      if (graph[i][j] === "#") {
+        bfs({ i, j, graph })
+        cnt++
       }
     }
   }
