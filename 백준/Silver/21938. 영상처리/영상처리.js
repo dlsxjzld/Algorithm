ffb720c650cf2febc6d2c9211a841aae1a3a07de
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-18 14:04:05 +0900

    [Silver II] Title: 영상처리, Time: 640 ms, Memory: 88112 KB -BaekjoonHub

--- 백준/Silver/21938. 영상처리/영상처리.js
+++ 백준/Silver/21938. 영상처리/영상처리.js
@@ -12,14 +12,14 @@ const rgb = input.slice(1, 1 + n).map((row) => row.split(" ").map(Number))
 
 const average = Number(input[1 + n])
 
-rgb.forEach((row, ridx) => {
+for (let k = 0; k < n; k++) {
   for (let i = 0; i < m; i++) {
-    const pixel =
-      (row[i * 3] + row[i * 3 + 1] + row[i * 3 + 2]) / 3 >= average ? 255 : 0
-    graph[ridx][i] = pixel
+    graph[k][i] =
+      (rgb[k][i * 3] + rgb[k][i * 3 + 1] + rgb[k][i * 3 + 2]) / 3 >= average
+        ? 255
+        : 0
   }
-})
-
+}
 const move = [
   [0, 1],
   [0, -1],
@@ -34,11 +34,10 @@ const bfs = (sx, sy) => {
     const [x, y] = queue[index++]
     for (let i = 0; i < 4; i++) {
       const [nx, ny] = [x + move[i][0], y + move[i][1]]
-      if (nx < 0 || nx >= n || ny < 0 || ny >= m || graph[nx][ny] === 0) {
-        continue
+      if (nx >= 0 && nx < n && ny >= 0 && ny < m && graph[nx][ny] == 255) {
+        graph[nx][ny] = 0
+        queue.push([nx, ny])
       }
-      graph[nx][ny] = 0
-      queue.push([nx, ny])
     }
   }
   return 1
@@ -47,9 +46,9 @@ const bfs = (sx, sy) => {
 let answer = 0
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < m; j++) {
-     
     if (graph[i][j] === 255) {
-      answer += bfs(i, j)
+      bfs(i, j)
+      answer++
     }
   }
 }
