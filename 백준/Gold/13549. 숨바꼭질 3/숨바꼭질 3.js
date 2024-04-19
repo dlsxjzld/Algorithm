116b9803061ba17a91814fd0386c46006dfb8860
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-19 15:03:50 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 208 ms, Memory: 16484 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -1,3 +1,5 @@
+
+// 두번째 코드
 const input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
@@ -18,19 +20,21 @@ const bfs = (start) => {
 
     for (let next of [curr - 1, curr + 1, curr * 2]) {
       if (next < 0 || next > 100000) continue
-      if (graph[next] != -1) {
-        if (next === curr * 2) {
-          if (graph[next] > graph[curr]) {
+      
+      if (graph[next] != -1) { // 이미 방문을 했었다면
+        if (next === curr * 2) { // next가 curr*2 일 때
+        // 방문을 했다면 값이 더 작을 때 갱신해야 하므로 조건문 추가
+          if (graph[next] > graph[curr]) { 
             queue.push(next)
             graph[next] = graph[curr]
           }
-        } else {
-          if (graph[next] > graph[curr]) {
+        } else { // next가 curr-1, curr+1 일 때
+          if (graph[next] > graph[curr] +1) {
             queue.push(next)
             graph[next] = graph[curr] + 1
           }
         }
-      } else {
+      } else { // 방문을 안했다면
         if (next === curr * 2) {
           queue.push(next)
           graph[next] = graph[curr]
