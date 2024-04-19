07523d50cad10adf9beae1f9755d8bc22b3ec3f1
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-19 13:47:00 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 512 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -5,32 +5,29 @@ const input = require("fs")
   .split("\n")
 
 const [n, k] = input[0].split(" ").map(Number)
-const visited = Array.from({length:100100},()=>false)
 
-let answer = Number.MAX_SAFE_INTEGER
-function bfs(start, cnt) {
-  const queue = [[start, cnt]]
+const graph = Array(100001).fill(-1)
+graph[n] = 0
+
+const bfs = (start) => {
+  const queue = [start]
 
   while (queue.length > 0) {
-    const [x, ncnt] = queue.shift()
-    if (x === k) {
-      answer = Math.min(answer, ncnt)
-      return
-    }
+    const curr = queue.shift()
 
-    for (let nx of [2 * x,x - 1, x + 1]) {
-      if (nx <0 || nx>100000 || visited[nx]) {
-        continue
-      }
-      if (nx === 2 * x) {
-        queue.unshift([nx, ncnt])
+    for (let next of [curr - 1, curr + 1, curr * 2]) {
+      if (next < 0 || next > 100000 || graph[next] != -1) continue
+      if (next === curr * 2) {
+        queue.unshift(next)
+        graph[next] = graph[curr]
       } else {
-        queue.push([nx, ncnt + 1])
+        queue.push(next)
+        graph[next] = graph[curr] + 1
       }
-      visited[nx] = true
     }
   }
 }
 
-bfs(n, 0)
-console.log(answer)
+bfs(n)
+
+console.log(graph[k])
