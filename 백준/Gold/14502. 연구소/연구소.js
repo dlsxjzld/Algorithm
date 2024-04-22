b97fe6a95d011e397f546d4d127dd8d155bf4a7f
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-22 15:50:23 +0900

    [Gold IV] Title: 연구소, Time: 752 ms, Memory: 15404 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.js
+++ 백준/Gold/14502. 연구소/연구소.js
@@ -7,12 +7,9 @@ const input = require("fs")
 const [n, m] = input[0].split(" ").map(Number)
 const graph = input.slice(1).map((row) => row.split(" ").map(Number))
 const virusPosition = []
-const move = [
-  [1, 0],
-  [-1, 0],
-  [0, 1],
-  [0, -1],
-]
+const dx = [0, 0, 1, -1]
+const dy = [1, -1, 0, 0]
+
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < m; j++) {
     if (graph[i][j] == 2) {
@@ -31,7 +28,8 @@ const virus = (_graph) => {
     const [x, y] = queue[index++]
 
     for (let i = 0; i < 4; i++) {
-      const [nx, ny] = [x + move[i][0], y + move[i][1]]
+      const nx = x + dx[i]
+      const ny = y + dy[i]
       if (nx >= 0 && ny >= 0 && nx < n && ny < m && newGraph[nx][ny] == 0) {
         newGraph[nx][ny] = 2
         queue.push([nx, ny])
@@ -43,29 +41,31 @@ const virus = (_graph) => {
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
       if (newGraph[i][j] == 0) {
-        safe++
+        safe += 1
       }
     }
   }
   answer = Math.max(answer, safe)
 }
 
-const setWall = (_wall, _graph) => {
+const setWall = (_wall, _graph, prevR, prevC) => {
   if (_wall == 3) {
     virus(_graph)
     return
   }
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
+      if (i < prevR) continue
+      if (i === prevR && j < prevC) continue
       if (_graph[i][j] == 0) {
         _graph[i][j] = 1
-        setWall(_wall + 1, _graph)
+        setWall(_wall + 1, _graph, i, j)
         _graph[i][j] = 0
       }
     }
   }
 }
 
-setWall(0, graph)
+setWall(0, graph, 0, 0)
 
 console.log(answer)
