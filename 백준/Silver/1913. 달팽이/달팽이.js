968ac32d4808137c6ca73dacc497efe5e681684f
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-04-29 14:36:15 +0900

    [Silver III] Title: 달팽이, Time: 436 ms, Memory: 67520 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.js
+++ 백준/Silver/1913. 달팽이/달팽이.js
@@ -4,56 +4,45 @@ const input = require("fs")
   .trim()
   .split("\n")
 
+// 홀수인 자연수
+// 1 ~ n**2
+// n*n
+// 3<= n <= 999
+
 const n = Number(input[0])
 const target = Number(input[1])
-
 const board = Array.from({ length: n }, () =>
-  Array.from({ length: n }, () => 1),
+  Array.from({ length: n }, () => 0),
 )
-
-const start = Math.floor(n / 2)
-let [sx, sy] = [start, start]
-const move = [
+const direction = [
   [-1, 0],
   [0, 1],
   [1, 0],
   [0, -1],
 ] // 위 오 아 왼
+let r = Math.floor(n / 2)
+let c = Math.floor(n / 2)
 
-let num = 1
-let dir = 0
-while (num < n) {
-  if (num !== n - 1) {
-    for (let i = 0; i < 2; i++) {
-      for (let j = 0; j < num; j++) {
-        let [nx, ny] = [sx + move[dir][0], sy + move[dir][1]]
-        board[nx][ny] = board[sx][sy] + 1
-        ;[sx, sy] = [nx, ny]
-      }
-      dir += 1
-      dir %= 4
+board[r][c] = 1
+let directionCnt = 0
+let number = 2
+const targetIndex = { r:r+1, c:c+1 }
+
+for (let i = 2; i < 2 * n + 1; i++) {
+  const step = i == 2 * n ? i / 2 - 1 : Math.floor(i / 2)
+
+  for (let j = 0; j < step; j++) {
+    r += direction[directionCnt][0]
+    c += direction[directionCnt][1]
+    board[r][c] = number
+    if (target === number) {
+      targetIndex.r = r + 1
+      targetIndex.c = c + 1
     }
-  } else {
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < num; j++) {
-        let [nx, ny] = [sx + move[dir][0], sy + move[dir][1]]
-        board[nx][ny] = board[sx][sy] + 1
-        ;[sx, sy] = [nx, ny]
-      }
-      dir += 1
-      dir %= 4
-    }
+    number += 1
   }
-  num += 1
+  directionCnt = (directionCnt + 1) % 4
 }
-let answer = []
-board.forEach((row, ridx) =>
-  row.forEach((val, cidx) => {
-    if (val === target) {
-      answer = [ridx+1,cidx+1]
-    }
-  }),
-)
 
 console.log(board.map((row) => row.join(" ")).join("\n"))
-console.log(answer.join(' '))
+console.log(targetIndex.r, targetIndex.c)
