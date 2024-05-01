785d026de22ae9b72e6123629ecc5017b9dd4d37
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-01 15:00:58 +0900

    [Gold V] Title: 빗물, Time: 200 ms, Memory: 12440 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.js
+++ 백준/Gold/14719. 빗물/빗물.js
@@ -17,21 +17,25 @@ heightList.forEach((height, col) => {
   }
 })
 
-let answer = 0
-let current_wall = { x: -1, y: -1 }
+let answer = 0 // 빗물의 총합
 for (let i = 0; i < n; i++) {
-  let rain = 0
-    current_wall.x = -1
-    current_wall.y = -1
+  // 새로운 행 시작할 때 벽 위치, 빗물 개수 초기화
+  // -1 -1 로 둬서 현재 벽 없음으로 초기화
+  let current_wall = { x: -1, y: -1 } // 행의 벽 위치
+  let rain = 0 // 행의 빗물
+
   for (let j = 0; j < m; j++) {
     if (graph[i][j] == 1) {
+      // 벽 만나면 벽 위치 초기화 및 빗물 더하기
       current_wall.x = i
       current_wall.y = j
       answer += rain
       rain = 0
     } else {
+      // 벽과 벽 사이에 있는 빈 칸만 빗물임
+      // 벽을 한 번은 만났어야 빗물 개수 셀 수 있음
       if (current_wall.x !== -1 && current_wall.y !== -1) {
-        rain += 1 
+        rain += 1 //
       }
     }
   }
