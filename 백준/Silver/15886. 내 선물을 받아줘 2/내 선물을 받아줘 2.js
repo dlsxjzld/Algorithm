d324bfb52e5396504c0b95a2087146169ed3bf9f
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-02 16:29:31 +0900

    [Silver III] Title: 내 선물을 받아줘 2, Time: 132 ms, Memory: 9760 KB -BaekjoonHub

---
+++ 백준/Silver/15886. 내 선물을 받아줘 2/내 선물을 받아줘 2.js
@@ -0,0 +1,18 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const n = Number(input[0])
+let street = input[1]
+while (street.includes("EE")) {
+  street = street.replace("EE", "E")
+}
+while (street.includes("WW")) {
+  street = street.replace("WW", "W")
+}
+while (street.includes("EW")) {
+  street = street.replace("EW", "P")
+}
+console.log(street.length)
