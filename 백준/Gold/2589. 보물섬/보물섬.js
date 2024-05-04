309d198168b8d539893ea1520eb0dd91f0866b2e
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-04 17:45:12 +0900

    [Gold V] Title: 보물섬, Time: 892 ms, Memory: 26880 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.js
+++ 백준/Gold/2589. 보물섬/보물섬.js
@@ -12,38 +12,31 @@ const dx = [0, 0, -1, 1]
 const dy = [-1, 1, 0, 0]
 
 const bfs = (x, y) => {
-  const visited = Array.from({ length: n }, () =>
-    Array.from({ length: m }, () => false),
-  )
+  const visited = Array.from({ length: n }, () => Array(m).fill(false))
   visited[x][y] = true
+
   const queue = [[x, y, 0]]
   let index = 0
 
   while (queue.length > index) {
     const [x, y, dist] = queue[index++]
+    answer = Math.max(answer, dist)
 
     for (let i = 0; i < 4; i++) {
       const nx = x + dx[i]
       const ny = y + dy[i]
-      if (
-        nx < 0 ||
-        ny < 0 ||
-        nx >= n ||
-        ny >= m ||
-        visited[nx][ny] ||
-        graph[nx][ny] == "W"
-      )
-        continue
-      queue.push([nx, ny, dist + 1])
-      answer = Math.max(answer, dist + 1)
-      visited[nx][ny] = true
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue
+      if (!visited[nx][ny] && graph[nx][ny] === "L") {
+        queue.push([nx, ny, dist + 1])
+        visited[nx][ny] = true
+      }
     }
   }
 }
 
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < m; j++) {
-    if (graph[i][j] == "L") {
+    if (graph[i][j] === "L") {
       bfs(i, j)
     }
   }
