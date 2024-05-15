b359e261e4fb64657c666492407263d0bce16a0a
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-15 16:24:10 +0900

    [Gold III] Title: 동방 프로젝트 (Large), Time: 244 ms, Memory: 33536 KB -BaekjoonHub

--- 백준/Gold/14595. 동방 프로젝트 （Large）/동방 프로젝트 （Large）.js
+++ 백준/Gold/14595. 동방 프로젝트 （Large）/동방 프로젝트 （Large）.js
@@ -6,19 +6,45 @@ const input = require("fs")
 
 const n = Number(input[0])
 const m = Number(input[1])
-const destroyWall = input.slice(2).map((row) => row.split(" ").map(Number)) ?? [
-  0, 0,
-]
+const destroyWall = input.slice(2).map((row) => row.split(" ").map(Number))
 
-const visited = Array.from({length: n + 1}, () => false);
-    
-for(const [x, y] of destroyWall) {
-    for(let i = x ; i < y ; i++) {
-        visited[i] = true;
-    }    
+const wallList = Array(n + 1).fill(false)
+
+if (m === 0) {
+  console.log(n)
+  return
 }
 
-const wall = visited.slice(1).filter(visit => visit === false).length;
+// 내림차순 정렬
+destroyWall.sort((a, b) => {
+    if(a[0] === b[0]){
+        return b[1]-a[1]
+    }
+    return b[0] - a[0]
+})
+
+const check = [destroyWall.pop()]
 
 
-console.log(wall)
+while (destroyWall.length > 0) {
+  const [start, end] = check.pop()
+  const [start2, end2] = destroyWall.pop()
+
+  if (start2 <= end) {
+      check.push([start, Math.max(end, end2)])
+    }
+    else{
+      check.push([start,end])
+      check.push([start2,end2])
+    }
+  
+}
+
+for (let [start, end] of check) {
+    
+  for(let i=start; i<end;i++){
+    wallList[i] = true
+  }
+}
+
+console.log(wallList.filter((val) => val === false).length - 1)
