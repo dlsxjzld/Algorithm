d1fa7459356cfd5c694990cde7f0697e369dc15b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-20 17:29:48 +0900

    [Silver III] Title: 선분 위의 점, Time: 484 ms, Memory: 53604 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.js
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.js
@@ -8,23 +8,9 @@ const [n, m] = input[0].split(" ").map(Number)
 
 const line = input[1].split(" ").map(Number).sort((a,b)=>a-b)
 
+
 const answer = []
 
-const findEndIndex = (target) => {
-  // 끝점은 target이 line[mid]보다 크거나 같으면 mid
-  let start = 0
-  let end = n - 1
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2)
-
-    if (target < line[mid]) {
-      end = mid - 1
-    } else {
-      start = mid + 1
-    }
-  }
-  return end
-}
 const findStartIndex = (target) => {
   // 시작점은 target이 line[mid]보다 작거나 같으면 mid
   let start = 0
@@ -39,15 +25,33 @@ const findStartIndex = (target) => {
       end = mid - 1
     }
   }
-  return start
+  return end
+}
+
+const findEndIndex = (target) => {
+  // 끝점은 target이 line[mid]보다 크거나 같으면 mid
+  let start = 0
+  let end = n - 1
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
+
+    if (target >= line[mid]) {
+      start = mid+1
+    } else {
+      end = mid - 1
+    }
+  }
+  return start-1
 }
 
 input.slice(2).forEach((row) => {
   const [s, e] = row.split(" ").map(Number)
   const eIdx = findEndIndex(e)
   const sIdx = findStartIndex(s)
+  
 
-  answer.push(eIdx - sIdx + 1)
+  answer.push(eIdx - sIdx)
 })
 
 console.log(answer.join("\n"))
