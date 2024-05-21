3921cc00161f77c7246696b4814d0d15a386432c
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-21 14:06:58 +0900

    [Gold V] Title: 하노이 탑, Time: 604 ms, Memory: 156736 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.js
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.js
@@ -10,17 +10,16 @@ const answer = []
 
 const hanoi = (from, to, thru, disc) => {
   if (disc === 1) {
-    answer.push([from, to])
-    return
+    return answer.push(`${from} ${to}`)
   }
 
   hanoi(from, thru, to, disc - 1)
-  answer.push([from, to])
+  answer.push(`${from} ${to}`)
   hanoi(thru, to, from, disc - 1)
 }
 
-console.log(((BigInt(2 ** n) - 1n).toString().replace('n','')))
+console.log((BigInt(2 ** n) - 1n).toString())
 if (n <= 20) {
   hanoi(1, 3, 2, n)
-  console.log(answer.map((row) => row.join(" ")).join("\n"))
+  console.log(answer.join("\n"))
 }
