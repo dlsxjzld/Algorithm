91356e67d0f8dcb6a5b236e234a4f206cd05197d
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-23 14:30:20 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 204 ms, Memory: 16936 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
@@ -4,20 +4,11 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const n = Number(input[0])
-const m = Number(input[1])
+const n = +(input[0])
+const m = +(input[1])
 const position = input[2].split(" ").map(Number)
 
-// 굴다리 0 ~ n
-// 가로등은 모두 높이가 같아야 함
-// 가로등의 높이가 h라면 왼쪽으로 h 오른쪽으로 h만큼 비춘다
-
-// 1<= 높이 <= n
-
-// position 에서 -높이 +높이
-
 for (let h = 1; ; h++){
-
   if (position[0] - h > 0)
     continue;
   if (position[m - 1] + h < n)
