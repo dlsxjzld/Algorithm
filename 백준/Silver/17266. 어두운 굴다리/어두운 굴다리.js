e6658286c46580eb3064d7af526e7919efab9460
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-23 14:28:02 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 208 ms, Memory: 16832 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
@@ -15,62 +15,24 @@ const position = input[2].split(" ").map(Number)
 // 1<= 높이 <= n
 
 // position 에서 -높이 +높이
-const checkHeight = (height) => {
-  if (m === 1) {
-    if (position[0] - height > 0) {
-      return false
-    }
-    if (position[0] + height < n) {
-      return false
+
+for (let h = 1; ; h++){
+
+  if (position[0] - h > 0)
+    continue;
+  if (position[m - 1] + h < n)
+    continue;
+
+  let isHeightOkay = true;
+  for (let i = 0; i < m; i++){
+    let right = position[i] + h, leftNext = position[i + 1] - h;
+    if (right < leftNext) {
+      isHeightOkay = false;
+      break;
     }
   }
-
-  for (let i = 0; i < m - 1; i++) {
-    const currPos = position[i]
-    const nextPos = position[i + 1]
-    if (i === 0) {
-      // 첫 가로등이라면
-      if (currPos - height > 0) {
-        // 왼쪽으로 비췄을 때  0보다 크면 어둡기 때문에 이 높이는 안됨
-        return false
-      }
-    }
-    if (i === m - 2) {
-      // 마지막 가로등이라면
-      if (nextPos + height < n) {
-        // 오른쪽으로 비췄을 때  n보다 작으면 어둡기 때문에 이 높이는 안됨
-        return false
-      }
-    }
-
-    if (nextPos - height > currPos + height) {
-      // 다음 가로등과 현재 가로등 사이가 어두우면 안됨
-      return false
-    }
+  if (isHeightOkay) {
+    console.log(h);
+    break;
   }
-  return true
 }
-
-const solution = () => {
-  let start = 1
-  let end = n
-  let mid = null
-  let height = Infinity
-
-  while (start <= end) {
-    mid = Math.floor((start + end) / 2)
-    if (checkHeight(mid)) {
-      
-      // 이 높이가 된다면? 높이 낮춰야함
-      height = Math.min(height, mid)
-      end = mid - 1
-    } else {
-      // 이 높이가 안된다면? 높이 올려야함
-      start = mid + 1
-    }
-  }
-
-  return height
-}
-
-console.log(solution())
