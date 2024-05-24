95c3cbd1edf0a65db616a69ed7b0d7c2c4c73cbb
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-24 16:53:07 +0900

    [Silver I] Title: 봄버맨, Time: 292 ms, Memory: 12624 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.js
+++ 백준/Silver/16918. 봄버맨/봄버맨.js
@@ -10,57 +10,61 @@ const bomb_graph = Array.from({ length: r }, () =>
   Array.from({ length: c }, () => 0),
 )
 
-// 초기 폭탄 터지는 시간 저장
-for (let i = 0; i < r; i++) {
-  for (let j = 0; j < c; j++) {
-    if (graph[i][j] === "O") {
-      bomb_graph[i][j] = 3
+graph.forEach((row, r) =>
+  row.forEach((val, c) => {
+    if (graph[r][c] === "O") {
+      bomb_graph[r][c] = 3
+    }
+  }),
+)
+const dx = [0, 0, 1, -1]
+const dy = [1, -1, 0, 0]
+
+const goBomb = () => {
+  for (let i = 0; i < r; i++) {
+    for (let j = 0; j < c; j++) {
+      if (graph[i][j] === "O" && bomb_graph[i][j] === time) {
+        graph[i][j] = "."
+        for (let k = 0; k < 4; k++) {
+          const nx = i + dx[k]
+          const ny = j + dy[k]
+
+          if (
+            nx < 0 ||
+            ny < 0 ||
+            nx >= r ||
+            ny >= c ||
+            bomb_graph[nx][ny] === time
+          ) {
+            continue
+          }
+          graph[nx][ny] = "."
+          bomb_graph[nx][ny] = 0
+        }
+      }
     }
   }
 }
 
-const move = [
-  [0, 1],
-  [0, -1],
-  [1, 0],
-  [-1, 0],
-]
+const setBomb = () => {
+  for (let i = 0; i < r; i++) {
+    for (let j = 0; j < c; j++) {
+      if (graph[i][j] === ".") {
+        graph[i][j] = "O"
+        bomb_graph[i][j] = time + 3
+      }
+    }
+  }
+}
 
 let time = 1
 while (time <= n) {
   if (time % 2) {
-    for (let i = 0; i < r; i++) {
-      for (let j = 0; j < c; j++) {
-        if (graph[i][j] === "O" && bomb_graph[i][j] === time) {
-          graph[i][j] = "."
-          bomb_graph[i][j] = 0
-          for (let k = 0; k < 4; k++) {
-            const [nx, ny] = [i + move[k][0], j + move[k][1]]
-            if (
-              nx < 0 ||
-              ny < 0 ||
-              nx >= r ||
-              ny >= c ||
-              bomb_graph[nx][ny] === time
-            )
-              continue
-            graph[nx][ny] = "."
-            bomb_graph[nx][ny] = 0
-          }
-        }
-      }
-    }
+    goBomb(graph)
   } else {
-    for (let i = 0; i < r; i++) {
-      for (let j = 0; j < c; j++) {
-        if (graph[i][j] === ".") {
-          bomb_graph[i][j] = time + 3
-          graph[i][j] = "O"
-        }
-      }
-    }
+    setBomb(graph)
   }
-  time += 1
+  time++
 }
 
 console.log(graph.map((row) => row.join("")).join("\n"))
