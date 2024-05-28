d638e16e768401f81d2cbafd7399dbac6a301c74
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-28 16:01:04 +0900

    [Silver I] Title: 회전 초밥, Time: 196 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.js
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.js
@@ -11,8 +11,6 @@ const input = require("fs")
 const [n, d, k, c] = input[0].split(" ").map(Number)
 const food = input.slice(1).map(Number)
 
-let cnt = -Infinity // 초밥 가짓수의 최댓값
-
 let counting = {}
 
 // 0부터 k-1까지 구하기
@@ -25,25 +23,30 @@ for (let i = 0; i < k; i++) {
 }
 
 counting[c] = counting[c] === undefined ? 1 : counting[c] + 1 // 쿠폰
-cnt = Object.keys(counting).length
+let answer = Object.keys(counting).length // 초밥 가짓수의 최댓값
 
 let start = 0
 let dish = k
+let cnt = answer
 while (start < n) {
   let end = dish % n
 
   counting[food[start]] -= 1
   if (counting[food[start]] === 0) {
     delete counting[food[start]]
+    cnt -= 1
   }
 
   if (counting[food[end]] === undefined) {
     counting[food[end]] = 1
+    cnt += 1
   } else {
     counting[food[end]] += 1
   }
-  cnt = Math.max(cnt, Object.keys(counting).length)
+
+  answer = Math.max(answer, cnt)
+
   start++
   dish++
 }
-console.log(cnt)
+console.log(answer)
