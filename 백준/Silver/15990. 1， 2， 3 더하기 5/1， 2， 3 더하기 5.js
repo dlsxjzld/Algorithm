00dddfaa18ffd6263b02d9fb09bda91b839838ce
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-30 17:22:49 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 316 ms, Memory: 26836 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.js
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.js
@@ -18,12 +18,16 @@ dp[3][2] = 1
 dp[3][3] = 1
 
 for (let i = 4; i < MAX + 1; i++) {
-  dp[i][1] = (dp[i - 1][2] + dp[i - 1][3])%DIVIDE
-  dp[i][2] = (dp[i - 2][1] + dp[i - 2][3])%DIVIDE
-  dp[i][3] = (dp[i - 3][1] + dp[i - 3][2])%DIVIDE
+  dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % DIVIDE
+  dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % DIVIDE
+  dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % DIVIDE
 }
+
+const answer = []
 
 for (let tc = 1; tc < T + 1; tc++) {
   const num = Number(input[tc])
-  console.log((dp[num][1] + dp[num][2] + dp[num][3])%DIVIDE)
+  answer.push((dp[num][1] + dp[num][2] + dp[num][3]) % DIVIDE)
 }
+
+console.log(answer.join("\n"))
