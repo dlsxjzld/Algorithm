79b2bb326d2282acba7dc5af717f73ec7d3690e8
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-30 16:17:58 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 220 ms, Memory: 26768 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.js
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.js
@@ -11,12 +11,14 @@ const dp = Array(MAX + 1).fill(0)
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
-
+const answer = []
 for (let i = 4; i <= MAX; i++) {
   dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % DIVIDE
 }
 
 for (let tc = 1; tc < T + 1; tc++) {
   const target = Number(input[tc])
-  console.log(dp[target])
+  answer.push(dp[target])
 }
+
+console.log(answer.join('\n'))
