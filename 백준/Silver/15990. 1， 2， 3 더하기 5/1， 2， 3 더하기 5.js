c4b10c99da4b0a6571df1ba614f1bc9f39a42309
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-05-30 17:21:04 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 476 ms, Memory: 30780 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.js
@@ -0,0 +1,29 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const T = Number(input[0])
+const MAX = 100_000
+const DIVIDE = 1_000_000_009
+const dp = Array.from({ length: MAX + 1 }, () =>
+  Array.from({ length: 4 }, () => 0),
+)
+
+dp[1][1] = 1
+dp[2][2] = 1
+dp[3][1] = 1
+dp[3][2] = 1
+dp[3][3] = 1
+
+for (let i = 4; i < MAX + 1; i++) {
+  dp[i][1] = (dp[i - 1][2] + dp[i - 1][3])%DIVIDE
+  dp[i][2] = (dp[i - 2][1] + dp[i - 2][3])%DIVIDE
+  dp[i][3] = (dp[i - 3][1] + dp[i - 3][2])%DIVIDE
+}
+
+for (let tc = 1; tc < T + 1; tc++) {
+  const num = Number(input[tc])
+  console.log((dp[num][1] + dp[num][2] + dp[num][3])%DIVIDE)
+}
