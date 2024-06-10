37f3e6bdee808dd4f47ad7b9f95d7d0d5a04816b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-10 15:04:54 +0900

    [Silver II] Title: 텔레포트 정거장, Time: 1252 ms, Memory: 137168 KB -BaekjoonHub

--- 백준/Silver/18232. 텔레포트 정거장/텔레포트 정거장.js
+++ 백준/Silver/18232. 텔레포트 정거장/텔레포트 정거장.js
@@ -8,26 +8,47 @@ const [N, M] = input[0].split(" ").map(Number)
 const [S, E] = input[1].split(" ").map(Number)
 const graph = Array.from({ length: N + 1 }, () => [])
 const visited = Array.from({ length: N + 1 }, () => 0)
+
 input.slice(2).forEach((row) => {
   const [x, y] = row.split(" ").map(Number)
   graph[x].push(y)
   graph[y].push(x)
 })
 
-const move = (start) => {
-  const queue = [[start, 0]]
-  let index = 0
-  while (queue.length > index) {
-    const [current, time] = queue[index++]
+// for (let i = 1; i <= N; i++) {
+//   graph[i].sort((a, b) => b - a)
+// }
 
-    for (let nx of [current + 1, current - 1 , ...graph[current]]) {
-      if (nx < 1 || nx > N || visited[nx]) continue
-      
-      visited[nx] = time+1
-      queue.push([nx, time + 1])
+const bfs = () => {
+  const queue = [S]
+  let index = 0
+  visited[S] = 1
+
+  while (queue.length > index) {
+    const curr = queue[index++]
+    for (let next of [curr + 1, curr - 1]) {
+      if (next < 1 || next > N) continue
+      if (visited[next]) continue
+      queue.push(next)
+      visited[next] = visited[curr] + 1
+    }
+    for (let next of graph[curr]) {
+      if (next < 1 || next > N) continue
+      if (visited[next]) continue
+      queue.push(next)
+      visited[next] = visited[curr] + 1
     }
   }
 }
-move(S)
+bfs()
+console.log(visited[E] - 1)
 
-console.log(visited[E])
+// 1부터 N까지의 각 점
+// 현재 위치가 X면 X+1, X-1, X에 위치한 텔레포트와 연결된 지점으로 이동 가능
+// 각 행동은 1초 소모
+
+// 텔레포트는 양방향!
+
+// 주예는 S, 방주는 E
+// S -> E
+// S < E || S > E
