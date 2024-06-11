1d8335bf189b56645470a5e7a3dbeee8e89939b3
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-11 15:51:57 +0900

    [Silver I] Title: 완전 이진 트리, Time: 116 ms, Memory: 9824 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.js
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.js
@@ -1,82 +1,27 @@
-const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const N = Number(input[0]);
+const num = input[1].split(' ').map(Number);
+const answer = [];
 
-class Node {
-  constructor(val) {
-    this.val = val
-    this.left = null
-    this.right = null
-  }
+for (let i = 0; i < N; i++) {
+   answer.push([]);
 }
 
-class BinaryTree {
-  constructor() {
-    this.root = null
-  }
+function makeTree(arr, level) {
+   if (arr.length === 1) {
+      answer[level].push(arr[0]);
+      return;
+   }
+   const center = Math.floor(arr.length / 2);
+   const leftArr = arr.slice(0, center);
+   const rightArr = arr.slice(center + 1, arr.length);
 
-  insert(newNode) {
-    if (this.root === null) {
-      this.root = newNode
-      return
-    }
-  }
+   answer[level].push(arr[center]);
+   
+   makeTree(leftArr, level + 1);
+   makeTree(rightArr, level + 1);
+   return;
 }
 
-const K = Number(input[0])
-const builidings = input[1].split(" ").map(Number)
-const visited = Array.from({ length: 2 ** K }, () => false)
-
-const bt = new BinaryTree()
-
-const divide = (arr) => {
-  const Length = arr.length
-  const middle = Math.floor(Length / 2)
-
-  const newNode = new Node(arr[middle])
-  if (Length === 1) return newNode
-
-  newNode.left = divide(arr.slice(0, middle))
-  newNode.right = divide(arr.slice(middle + 1))
-
-  return newNode
-}
-
-bt.insert(divide(builidings))
-
-let answer = ""
-let currentNode = bt.root
-const queue = [currentNode]
-let index = 0
-answer += currentNode.val +'\n'
-let cnt = 1
-let level = 2
-
-
-while (queue.length > index) {
-  const currentNode = queue[index++]
-
-  if (!visited[currentNode.val]) {
-    visited[currentNode.val] = true
-
-    if (currentNode.left !== null) {
-      queue.push(currentNode.left)
-      answer += currentNode.left.val + " "
-      cnt +=1
-    }
-    if (currentNode.right !== null) {
-      queue.push(currentNode.right)
-      answer += currentNode.right.val + " "
-      cnt +=1
-    }
-    if(cnt === (2**level)-1 ){
-        
-        answer +='\n'
-        level +=1
-    }
-  }
-}
-
-console.log(answer)
+makeTree(num, 0);
+console.log(answer.map(v => v.join(' ')).join('\n'));
