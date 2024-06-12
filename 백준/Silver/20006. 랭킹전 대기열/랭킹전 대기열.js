e380ade5daec2e8bf144a854123cb369889ec700
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-12 20:48:58 +0900

    [Silver II] Title: 랭킹전 대기열, Time: 140 ms, Memory: 10708 KB -BaekjoonHub

--- 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.js
+++ 백준/Silver/20006. 랭킹전 대기열/랭킹전 대기열.js
@@ -5,37 +5,38 @@ const input = require("fs")
   .split("\n")
 
 const [p, m] = input[0].split(" ").map(Number)
-const roomList = []
+const players = input.slice(1).map((row) => row.split(" "))
 
-for (let i = 1; i < 1 + p; i++) {
-  const [level, nickname] = input[i].split(" ")
-  if (roomList.length === 0) {
-    roomList.push([[Number(level), nickname]])
-    continue
-  }
-  let isNoRoom = true
-  for (let room of roomList) {
-    if (
-      room.length < m &&
-      room[0][0] - 10 <= Number(level) &&
-      room[0][0] + 10 >= Number(level)
-    ) {
-      room.push([Number(level), nickname])
-      isNoRoom = false
-      break
+const rooms = []
+
+for (let [level, nickname] of players) {
+  if (rooms.length === 0) {
+    rooms.push([[Number(level), nickname]])
+  } else {
+    let noRoom = true
+    const myLevel = Number(level)
+    for (let room of rooms) {
+      const LEVEL = room[0][0]
+
+      if (room.length < m && myLevel >= LEVEL - 10 && myLevel <= LEVEL + 10) {
+        room.push([Number(level), nickname])
+        noRoom = false
+        break
+      }
     }
-  }
-  if (isNoRoom) {
-    roomList.push([[Number(level), nickname]])
+    if (noRoom) {
+      rooms.push([[Number(level), nickname]])
+    }
   }
 }
 
-for (let room of roomList) {
+for (let room of rooms) {
   room.sort((a, b) => (a[1] > b[1] ? 1 : -1))
 }
 
 const answer = []
-for (let room of roomList) {
+
+for (let room of rooms) {
   if (room.length === m) {
     answer.push("Started!")
   } else {
