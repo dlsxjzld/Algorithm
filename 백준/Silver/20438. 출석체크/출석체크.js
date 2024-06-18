6f6fa5bbc8300228265650950ec7222c6176b7ab
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-18 15:25:13 +0900

    [Silver II] Title: 출석체크, Time: 1568 ms, Memory: 35516 KB -BaekjoonHub

---
+++ 백준/Silver/20438. 출석체크/출석체크.js
@@ -0,0 +1,43 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const [n, k, q, m] = input[0].split(" ").map(Number)
+const arrK = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b)
+const arrQ = input[2]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b)
+  .filter((val) => !arrK.includes(val))
+const arrM = input.slice(3).map((row) => row.split(" ").map(Number))
+
+const students = Array(5001).fill(false)
+
+arrQ.forEach((val) => {
+  for (let i = 3; i <= 5000; i++) {
+    if (!students[i] && i % val === 0) {
+      students[i] = true
+    }
+  }
+})
+
+arrK.forEach((val) => {
+  for (let i = 3; i <= 5000; i++) {
+    if (students[val]) {
+      students[val] = false
+    }
+  }
+})
+
+const answer = []
+
+arrM.forEach(([s, e]) => {
+  answer.push(students.slice(s, e + 1).filter((val) => val === false).length)
+})
+
+console.log(answer.join("\n"))
