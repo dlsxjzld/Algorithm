855cc5eaff590f4cec09b8626036ec73a4e75b23
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-19 17:12:31 +0900

    [Silver I] Title: 단어 맞추기, Time: 120 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/9081. 단어 맞추기/단어 맞추기.js
+++ 백준/Silver/9081. 단어 맞추기/단어 맞추기.js
@@ -4,36 +4,43 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-
 const T = Number(input[0])
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+const answer = []
 
 for (let tc = 1; tc < T + 1; tc++) {
   const word = input[tc].split("")
-  let index = []
-  for (let i = word.length - 1; i >= 1; i--) {
-    if (word[i - 1] < word[i]) {
-      index.push(i - 1)
-      break
-    }
-  }
-  for (let i = word.length - 1; i > index[0]; i--) {
-    if (word[i] > word[index[0]]) {
-      index.push(i)
-      break
-    }
-  }
+  const convertedWord = word.map((val) => alphabet.indexOf(val))
 
-  const newWord = [...word]
-  let tmp = newWord[index[0]]
-  newWord[index[0]]  = word[index[1]]
-  newWord[index[1]] = tmp
-  
-  if(index.length >1){
-    const result = (newWord.slice(0,index[0]+1).concat(newWord.slice(index[0]+1).sort())).join('')
-    console.log(result)
+  let x = null
+  let y = null
+  for (let i = convertedWord.length - 1; i > 0; i--) {
+    x = i - 1
+    const char = convertedWord[x]
+    for (let j = convertedWord.length - 1; j >= i; j--) {
+      if (convertedWord[j] > char) {
+        y = j
+        break
+      }
+    }
+    if (y !== null) {
+      const temp = convertedWord[y]
+      convertedWord[y] = char
+      convertedWord[x] = temp
+      break
+    }
+  }
+  if (y !== null) {
+    answer.push(
+      convertedWord
+        .slice(0, x+1)
+        .concat(convertedWord.slice(x+1).sort((a, b) => a - b))
+        .map((val) => alphabet[val])
+        .join(""),
+    )
   }else{
-    console.log(word.join(''))
+    answer.push(word.join(''))
   }
 }
-
-
+console.log(answer.join("\n"))
