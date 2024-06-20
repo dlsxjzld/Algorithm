b9df855418ba0489d6a53810d6e11e56182f0271
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-20 16:59:20 +0900

    [Gold V] Title: A와 B 2, Time: 104 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.js
+++ 백준/Gold/12919. A와 B 2/A와 B 2.js
@@ -5,29 +5,33 @@ const input = require("fs")
   .split("\n")
 
 const s = input[0]
-const t = input[1]
+const t = input[1].split("")
+
+// 문자열의 뒤에 A를 추가
+// 문자열의 뒤에 B를 추가하고 문자열을 뒤집기
 let answer = 0
-
-const dfs = (str)=>{
-  if(str === s){
-    answer = 1
+const dfs = (target, curr) => {
+  if (target.length === curr.length) {
+    if (curr.join("") === target) {
+      answer = 1
+    }
     return
   }
-  if(str.length == 0){
-    return
+  if (curr[curr.length - 1] === "A") {
+    const next = [...curr]
+    next.pop()
+    dfs(target, next)
+    const reversedNext = [...curr].reverse()
+    if (reversedNext[reversedNext.length - 1] === "B") {
+      reversedNext.pop()
+      dfs(target, reversedNext)
+    }
+  } else if (curr[0] === "B") {
+    const reversedNext = [...curr].reverse()
+    reversedNext.pop()
+    dfs(target, reversedNext)
   }
-
-  if(str[str.length-1] === 'A'){
-    dfs(str.slice(0,str.length-1))
-  }
-  
-  if(str[0] === 'B'){
-    const newStr = str
-    dfs(newStr.split('').reverse().join('').slice(0,newStr.length-1))
-  }
-
 }
 
-dfs(t)
-
+dfs(s, t)
 console.log(answer)
