493705883e0fea310ce90f68495dac069ef879fe
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-20 13:31:16 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 236 ms, Memory: 43552 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.js
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.js
@@ -4,35 +4,45 @@ const input = require("fs")
   .trim()
   .split("\n")
 
+// 수빈이 n
+// 동생 k
+// n<k || n>k || n==k
+
+// x-1, x+1, 2*x
+// 1초 걸림
+
+// 가장 빠른 시간은 몇 초?
+// 가장 빠른 시간으로 찾는 방법이 몇 가지?
+
 const [n, k] = input[0].split(" ").map(Number)
 
-const queue = [n]
-let index = 0
-const distance = Array.from({ length: 100001 }, () => 0)
+const solution = (n, k) => {
+  const MAX = 100_001
+  const visited = Array(MAX).fill(0)
+  const queue = [n]
+  let index = 0
+  visited[n] = 1
 
+  let cnt = 0
 
+  while (queue.length > index) {
+    const curr = queue[index++]
 
+    if (curr === k) {
+      cnt += 1
+      continue
+    }
 
-let cnt = 0
-distance[n] = 0
-while (queue.length > index) {
-  const curr = queue[index++]
-
-  if(curr === k){
-    cnt +=1
-    continue
-  }
-
-  for (let next of [2 * curr, curr - 1, curr + 1]) {
-    if (next >= 0 && next <= 100000 && next !== n) {
-      if (distance[next] === 0 || distance[next] === distance[curr] + 1) {
-        distance[next] = distance[curr] + 1
+    for (let next of [curr + 1, curr - 1, curr * 2]) {
+      if (next < 0 || next >= MAX) continue
+      if (visited[next] === 0 || visited[next] === visited[curr] + 1) {
+        visited[next] = visited[curr] + 1
         queue.push(next)
-      } 
+      }
     }
   }
+  return [visited[k]-1, cnt]
 }
-console.log(distance[k])
-console.log(cnt)
-// 5 10 9 18 17
-// 5 4 8 16 17
+
+const answer = solution(n,k)
+console.log(answer.join("\n"))
