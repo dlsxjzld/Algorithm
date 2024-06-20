66e0507c8f6c24495947ed513417bfce2d693b93
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-20 20:37:43 +0900

    [Silver II] Title: 지구 온난화, Time: 100 ms, Memory: 9384 KB -BaekjoonHub

--- 백준/Silver/5212. 지구 온난화/지구 온난화.js
+++ 백준/Silver/5212. 지구 온난화/지구 온난화.js
@@ -6,110 +6,70 @@ const input = require("fs")
 
 const [r, c] = input[0].split(" ").map(Number)
 
-const graph = Array.from({ length: r + 2 }, () =>
-  Array.from({ length: c + 2 }, () => "."),
-)
-const INPUT = input.slice(1).map((row) => row.split(""))
+const graph = input.slice(1).map((row) => row.split(""))
 
-for (let i = 1; i <= r; i++) {
-  for (let j = 1; j <= c; j++) {
-    graph[i][j] = INPUT[i - 1][j - 1]
-  }
-}
-
-
-
-const landToSeaPosition = []
 const move = [
   [0, 1],
   [0, -1],
   [1, 0],
   [-1, 0],
 ]
-
-for (let i = 1; i <= r; i++) {
-  for (let j = 1; j <= c; j++) {
-    let cnt = 0
-    for (let k = 0; k < 4; k++) {
-      if (graph[i + move[k][0]][j + move[k][1]] === ".") {
-        cnt += 1
-      }
-    }
-    if (cnt >= 3) {
-      landToSeaPosition.push([i, j])
-    }
-  }
-}
-
-for (let [x, y] of landToSeaPosition) {
-  graph[x][y] = "."
-}
-
-const row = []
-const col = []
-// 1행부터
-for (let i = 1; i <= r; i++) {
-  if (!graph[i].includes("X")) {
-    row.push(i)
-  } else {
-    break
-  }
-}
-
-// 1열부터
-for (let i = 1; i <= c; i++) {
+const checkSea = (x, y) => {
+  // X 주변 X개수가 1이하면 다 바다로 변함
   let cnt = 0
-  for (let j = 1; j <= r; j++) {
-    if (graph[j][i] === ".") {
-      cnt++
-    }
-  }
-  if (cnt === r) {
-    col.push(i)
-  } else {
-    break
-  }
-}
 
-// r행부터
-for (let i = r; i >= 1; i--) {
-  if (!graph[i].includes("X")) {
-    row.push(i)
-  } else {
-    break
-  }
-}
-
-// c열부터
-for (let i = c; i >= 1; i--) {
-  let cnt = 0
-  for (let j = 1; j <= r; j++) {
-    if (graph[j][i] === ".") {
-      cnt++
-    }
-  }
-  if (cnt === r) {
-    col.push(i)
-  } else {
-    break
-  }
-}
-row.sort((a, b) => a - b)
-col.sort((a, b) => a - b)
-
-const answer =[]
-
-for (let i = 1; i <= r; i++) {
-    const temp = []
-    if(row.includes(i)) continue
-  for (let j = 1; j <= c; j++) {
-      
-    if (col.includes(j)) {
+  for (let i = 0; i < 4; i++) {
+    const [nx, ny] = [x + move[i][0], y + move[i][1]]
+    if (nx < 0 || ny < 0 || nx >= r || ny >= c) {
       continue
     }
-    temp.push(graph[i][j])
+    if (graph[nx][ny] === "X") {
+      cnt += 1
+    }
   }
-  answer.push(temp)
+  if (cnt <= 1) return true
+  return false
 }
 
+const pos = []
+
+graph.forEach((row, r) => {
+  row.forEach((val, c) => {
+    if (val === "X") {
+      const isGoingSea = checkSea(r, c)
+      // 해당하는 position x,y 메모
+      if (isGoingSea) {
+        pos.push([r, c])
+      }
+    }
+  })
+})
+
+for (let [x, y] of pos) {
+  // 바다로 변화시키기
+  graph[x][y] = "."
+}
+// X인지 확인하면서 MAX_Row,MAX_Col,MIN_Row,MIN_Col 위치 갱신?
+let max_r = -Infinity
+let max_c = -Infinity
+let min_r = Infinity
+let min_c = Infinity
+
+graph.forEach((row, r) => {
+  row.forEach((val, c) => {
+    if (val === "X") {
+      max_r = Math.max(max_r, r)
+      max_c = Math.max(max_c, c)
+      min_r = Math.min(min_r, r)
+      min_c = Math.min(min_c, c)
+    }
+  })
+})
+
+const answer = []
+for (let i = 0; i < r; i++) {
+  if (i >= min_r && i <= max_r) {
+    answer.push([...graph[i].slice(min_c, max_c + 1)])
+  }
+}
 console.log(answer.map((row) => row.join("")).join("\n"))
