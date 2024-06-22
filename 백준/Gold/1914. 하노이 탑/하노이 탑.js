43d0932a6f821cc522aa37983203beeb6e18b91f
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-22 19:15:07 +0900

    [Gold V] Title: 하노이 탑, Time: 584 ms, Memory: 155040 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.js
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.js
@@ -4,22 +4,23 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const stick = 3
 const n = Number(input[0])
+
+// n개 있으면 1->3 1개 옮기고 n-1개 1->2
+//
+// n-1개 있으면 2->3 1개 옮기고 n-2개 2->1
+// n-2개 있으면 1->3 1개 옮기고 n-3개 1->2
 const answer = []
 
-const hanoi = (from, to, thru, disc) => {
-  if (disc === 1) {
-    return answer.push(`${from} ${to}`)
-  }
-
-  hanoi(from, thru, to, disc - 1)
+const hanoi = (from, thru, to, num) => {
+  if (num === 0) return
+  hanoi(from, to, thru, num - 1)
   answer.push(`${from} ${to}`)
-  hanoi(thru, to, from, disc - 1)
+  hanoi(thru, from, to, num - 1)
 }
 
-console.log((BigInt(2 ** n) - 1n).toString())
-if (n <= 20) {
-  hanoi(1, 3, 2, n)
-  console.log(answer.join("\n"))
+console.log((BigInt(2**n) - 1n).toString())
+if(n<=20){
+  hanoi(1, 2, 3, n)
+  console.log(answer.join('\n'))
 }
