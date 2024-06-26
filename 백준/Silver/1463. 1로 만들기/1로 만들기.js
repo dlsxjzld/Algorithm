a01f67a3e3cbaf4c099e603d14d9368f5d213bbc
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-06-26 19:44:42 +0900

    [Silver III] Title: 1로 만들기, Time: 228 ms, Memory: 25840 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -2,22 +2,23 @@ const input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split("\n");
+  .split("\n")
 
-let n = Number(input[0]);
-const MAX = Number.MAX_SAFE_INTEGER;
-const dp = Array.from({ length: n + 1 }, () => MAX);
-dp[n] = 0;
+let N = Number(input[0])
 
-while (n > 1) {
-  if (n % 3 === 0) {
-    dp[n / 3] = Math.min(dp[n] + 1, dp[n / 3]);
+const dp = Array(N + 1).fill(Infinity)
+dp[N] = 0
+
+while (N >= 1) {
+  if (N % 3 === 0) {
+    dp[N / 3] = Math.min(dp[N / 3], dp[N] + 1)
   }
-  if (n % 2 === 0) {
-    dp[n / 2] = Math.min(dp[n] + 1, dp[n / 2]);
+
+  if (N % 2 === 0) {
+    dp[N / 2] = Math.min(dp[N / 2], dp[N] + 1)
   }
-  dp[n - 1] = Math.min(dp[n] + 1, dp[n - 1]);
-  n -= 1;
+  dp[N - 1] = Math.min(dp[N - 1], dp[N] + 1)
+  N--
 }
 
-console.log(dp[1]);
+console.log(dp[1])
