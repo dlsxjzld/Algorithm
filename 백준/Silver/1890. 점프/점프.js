ecb0dcb7bf0ba224e4d7b7097b3475c9534811cc
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-07-03 18:57:34 +0900

    [Silver I] Title: 점프, Time: 92 ms, Memory: 9392 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.js
+++ 백준/Silver/1890. 점프/점프.js
@@ -1,28 +1,27 @@
-const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const N = Number(input[0])
-const graph = input.slice(1).map((row) => row.split(" ").map(Number))
-const dp = Array.from({ length: N }, () => Array.from({ length: N }, () => 0n))
-
-const move = [
-  [0, 1],
-  [1, 0],
-] // 동, 남
-
-dp[0][0] = 1n
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    if(i===N-1 && j===N-1) continue
-    const canMove = graph[i][j]
-    for (let k = 0; k < 2; k++) {
-      const [nx, ny] = [i + move[k][0] * canMove, j + move[k][1] * canMove]
-      if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue
-      dp[nx][ny] += dp[i][j]
+let n = Number(input[0]);
+let arr = new Array(n).fill(new Array(n));
+for (let i = 0; i < arr.length; i++) {
+  arr[i] = input[i + 1].split(" ").map(Number);
+}
+let dp = new Array(n);
+for (let i = 0; i < dp.length; i++) {
+  dp[i] = new Array(n).fill(BigInt(0));
+}
+dp[0][0] = BigInt(1);
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    let num = arr[i][j];
+    if (num === 0) continue;
+    if (i + arr[i][j] < n) {
+      dp[i + arr[i][j]][j] += dp[i][j];
+    }
+    if (j + arr[i][j] < n) {
+      dp[i][j + arr[i][j]] += dp[i][j];
     }
   }
 }
-console.log(dp[N-1][N-1].toString())
+console.log(dp[n - 1][n - 1].toString());
