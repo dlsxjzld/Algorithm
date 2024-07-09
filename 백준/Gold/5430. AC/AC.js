43d0ea42c6464d14c5075603b35e8dfaef957014
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-07-09 19:20:26 +0900

    [Gold V] Title: AC, Time: 340 ms, Memory: 53092 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.js
+++ 백준/Gold/5430. AC/AC.js
@@ -2,41 +2,48 @@ const input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split("\n");
+  .split("\n")
 
-input.shift();
+const T = Number(input[0])
+const answer = []
+for (let tc = 0; tc < T; tc++) {
+  const p = input[tc * 3 + 1].split("") // 1 4 7
+  const n = Number(input[tc * 3 + 2])
+  const arr =
+    input[tc * 3 + 3].length === 2
+      ? []
+      : input[tc * 3 + 3]
+          .slice(1, input[tc * 3 + 3].length - 1)
+          .split(",")
+          .map(Number)
 
-const Delete = (arr, isR) => {
-  if (isR) {
-    arr.pop();
-    return arr;
-  }
+  let start = 0
+  let end = n
 
-  if (!isR) {
-    arr.shift();
-    return arr;
-  }
-};
-
-for (let i = 0; i < input.length; i += 3) {
-  const command = input[i].split("");
-  let array = JSON.parse(input[i + 2]);
-  let isReverse = false;
-  let isError = false;
-
-  for (let j = 0; j < command.length; j++) {
-    if (command[j] === "R") {
-      isReverse = !isReverse;
-    }
-    if (command[j] === "D") {
-      if (array.length === 0) {
-        isError = true;
+  let reversed = false
+  for (let action of p) {
+    if (action === "R") {
+      reversed = !reversed
+    } else {
+      if (reversed) {
+        // end
+        end -= 1
+      } else {
+        start += 1
       }
-      array = Delete(array, isReverse);
     }
   }
-
-  console.log(
-    isError ? "error" : JSON.stringify(isReverse ? array.reverse() : array)
-  );
+  if (end < start) {
+    answer.push("error")
+  } else if (end == start) {
+    answer.push("[]")
+  } else {
+    if (reversed) {
+      answer.push("[" + arr.slice(start, end).reverse().join(",") + "]")
+    } else {
+      answer.push("[" + arr.slice(start, end).join(",") + "]")
+    }
+  }
 }
+
+console.log(answer.join("\n"))
