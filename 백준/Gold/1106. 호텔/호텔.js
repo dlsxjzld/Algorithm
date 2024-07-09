f12b1b1aa4fc4b0e469a4ac78d7ae26fd24e0eb7
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-07-09 21:50:01 +0900

    [Gold IV] Title: 호텔, Time: 128 ms, Memory: 10796 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.js
+++ 백준/Gold/1106. 호텔/호텔.js
@@ -1,29 +1,22 @@
-const input = require('fs')
-  .readFileSync('/dev/stdin')
+const input = require("fs")
+  .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split('\n')
+  .split("\n")
 
-const [c, n] = input[0].split(' ').map(Number)
-const cities = input.slice(1, 1 + n).map((row) => row.split(' ').map(Number))
-const sortedCities = cities.sort((a, b) => a[1] - b[1])
-const dp = Array.from({ length: c+1 }, () => Infinity)
-
-// c 명 늘리기
-// n 개의 도시
+const [c, n] = input[0].split(" ").map(Number)
+const dp = Array.from({ length: c + 1 }, () => Infinity)
 dp[0] = 0
-// 각 도시 : [비용 , 고객의 수]
-for (let [cost, customer] of sortedCities) {
-  if (dp[customer] > cost) {
-    dp[customer] = cost
-  }
-  for (let idx = 1; idx < c + 1; idx++) {
-    if (idx < customer) {
-      dp[idx] = Math.min(dp[idx], cost)
-    } else {
-      dp[idx] = Math.min(dp[idx], dp[idx - customer] + dp[customer])
-    }
-  }
-}
+for (let tc = 1; tc < 1 + n; tc++) {
+  const [cost, customer] = input[tc].split(" ").map(Number)
 
+  for (let i = 1; i <= c; i++) {
+   if(i<=customer){
+       dp[i] = Math.min(dp[i],cost)
+   }else{
+       dp[i] = Math.min(dp[i],dp[i - customer] + dp[customer])
+   }
+  }
+  
+}
 console.log(dp[c])
