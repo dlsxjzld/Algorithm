b45d8bfea4a0628f63156bc6135cee65ef38120a
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-07-10 15:21:21 +0900

    [Silver III] Title: 선분 위의 점, Time: 476 ms, Memory: 67368 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.js
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.js
@@ -6,52 +6,55 @@ const input = require("fs")
 
 const [n, m] = input[0].split(" ").map(Number)
 
-const line = input[1].split(" ").map(Number).sort((a,b)=>a-b)
+const dots = input[1].split(" ").map(Number)
+const lines = input.slice(2).map((row) => row.split(" ").map(Number))
+dots.sort((a,b)=>a-b)
+// 0 1  2  3  4
+// 1 3 10 20 30
 
+// 1 10 -> 0 2
+// 끝은 mid가 target보다 작거나 같은 값 중에 제일 큰 값
+// 시작은 mid가 target보다 크거나 같은 값 중에 제일 작은 값
 
-const answer = []
+// 2 15 -> 3포함 10포함  1 2
+// 4 8 -> 2 1
 
-const findStartIndex = (target) => {
-  // 시작점은 target이 line[mid]보다 작거나 같으면 mid
-  let start = 0
-  let end = n - 1
+// 0 2
+// 3 4
+// 1 4
+// 1 2
+// 2 1
 
+const findStartIndex = (start, end, target) => {
   while (start <= end) {
     let mid = Math.floor((start + end) / 2)
-
-    if (target > line[mid]) {
-      start = mid + 1
+    if (dots[mid] >= target) {
+      end = mid-1
     } else {
-      end = mid - 1
+      start = mid + 1
     }
   }
-  return end
+  return end+1
 }
 
-const findEndIndex = (target) => {
-  // 끝점은 target이 line[mid]보다 크거나 같으면 mid
-  let start = 0
-  let end = n - 1
-
+const findEndIndex = (start, end, target) => {
   while (start <= end) {
     let mid = Math.floor((start + end) / 2)
-
-    if (target >= line[mid]) {
-      start = mid+1
+    if (dots[mid] > target) {
+      end = mid-1
     } else {
-      end = mid - 1
+      start = mid +1
     }
   }
   return start-1
 }
+const answer = []
+for (let [s, e] of lines) {
 
-input.slice(2).forEach((row) => {
-  const [s, e] = row.split(" ").map(Number)
-  const eIdx = findEndIndex(e)
-  const sIdx = findStartIndex(s)
-  
+  const start = findStartIndex(0, n-1, s)
+  const end = findEndIndex(0, n-1, e)
 
-  answer.push(eIdx - sIdx)
-})
+  answer.push(end -start+1>0 ? end - start + 1 : 0)
+}
 
-console.log(answer.join("\n"))
+console.log(answer.join('\n'))
