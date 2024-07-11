2a2518c7148215df504d26a6afa0104636a2b074
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-07-11 13:19:47 +0900

    [Gold V] Title: 숫자고르기, Time: 100 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.js
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.js
@@ -5,28 +5,31 @@ const input = require("fs")
   .split("\n")
 
 const n = Number(input[0])
-
 const secondLine = [0].concat(input.slice(1).map(Number))
+// 사이클이 되면 된다
 
-const visited = Array(n + 1).fill(false)
-let answer = []
+const visited = Array.from({ length: n + 1 }, () => false)
+const answer = []
+
+// dfs (백트래킹)
+// 사이클 판단
+// 사이클 되면 answer에 값 추가
 
 const dfs = (start, target) => {
   if (!visited[start]) {
     visited[start] = true
     dfs(secondLine[start], target)
     visited[start] = false
-  }else{
-      if (start === target) {
-        answer.push(target)
-      }
+  } else {
+    if (start === target) {
+      answer.push(target)
+    }
   }
-
 }
 
 for (let i = 1; i <= n; i++) {
   dfs(i, i)
 }
-answer.sort((a, b) => a - b)
+
 console.log(answer.length)
-console.log(answer.join("\n"))
+console.log(answer.join('\n'))
