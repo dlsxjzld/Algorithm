cac296a6f048a896f382b3c3c7f4a73a2a645b9b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-07-12 22:21:44 +0900

    [Silver II] Title: 키로거, Time: 644 ms, Memory: 217020 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.js
@@ -0,0 +1,32 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const T = Number(input[0])
+const answer = []
+for (let tc = 1; tc <= T; tc++) {
+  const keyLogger = input[tc].split("")
+  const left = []
+  const right = []
+
+  for (let key of keyLogger) {
+    if (key === "<") {
+      if (left.length > 0) {
+        right.push(left.pop())
+      }
+    } else if (key === ">") {
+      if (right.length > 0) {
+        left.push(right.pop())
+      }
+    } else if (key === "-") {
+      left.pop()
+    } else {
+      left.push(key)
+    }
+  }
+  answer.push(left.concat(right.reverse()).join(""))
+}
+
+console.log(answer.join("\n"))
