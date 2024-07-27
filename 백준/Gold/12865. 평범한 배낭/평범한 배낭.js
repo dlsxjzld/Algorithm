b54eb359987c7f6ce59398c501a8cc2cf265ef3d
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-07-27 14:52:17 +0900

    [Gold V] Title: 평범한 배낭, Time: 252 ms, Memory: 12064 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -5,19 +5,14 @@ const input = require("fs")
   .split("\n")
 
 const [n, k] = input[0].split(" ").map(Number)
-const dp = Array.from({ length: n + 1 }, () =>
-  Array.from({ length: k + 1 }, () => 0),
-)
+
+const dp = Array.from({ length: k + 1 }, () => 0)
+
 for (let i = 1; i <= n; i++) {
   const [w, v] = input[i].split(" ").map(Number)
-
-  for (let j = 1; j <= k; j++) {
-    if (j - w >= 0) {
-      dp[i][j] = Math.max(dp[i - 1][j - w] + v, dp[i - 1][j])
-    } else {
-      dp[i][j] = dp[i - 1][j]
-    }
+  for (let weight = k; weight >= w; weight--) {
+    dp[weight] = Math.max(dp[weight], dp[weight - w] + v)
   }
 }
 
-console.log(dp[n][k])
+console.log(dp[k])
