d3d36ed973e828096ad764c76ba6dc2ea6ac290c
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-08-26 13:48:28 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 128.76 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=javascript#) 
 
 ### 성능 요약
 
-메모리: 37.7 MB, 시간: 115.72 ms
+메모리: 37.7 MB, 시간: 128.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 26일 13:41:12
+2024년 08월 26일 13:48:26
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,16 +1,17 @@
-function convert (n,k){
-    if(k === 10) return n.toString()
-    let tmp =''
-    while(n > k){
-        let rest = n%k
-        tmp = rest+tmp
-        n= Math.floor(n/k)
-    }
-    tmp = n + tmp
-    return tmp
-}
+// function convert (n,k){
+//     if(k === 10) return n.toString()
+//     let tmp =''
+//     while(n > k){
+//         let rest = n%k
+//         tmp = rest+tmp
+//         n= Math.floor(n/k)
+//     }
+//     tmp = n + tmp
+//     return tmp
+// }
 
 function isPrime (n){
+    // n = parseInt(n,10)
     if(n<=1) return false
     const limit = Math.floor(Math.sqrt(n))+1
     for(let i=2;i<limit;i++){
@@ -18,15 +19,15 @@ function isPrime (n){
             return false
         }
     }
-    
     return true
 }
 
 function solution(n, k) {
     var answer = -1;
-    const convertedN = convert(n,k)
-
-    const numbers = convertedN.split('0').filter(Boolean)
+    // const convertedN = convert(n,k)
+    // const numbers = convertedN.split('0').filter(Boolean)
+    
+    const numbers = n.toString(k).split('0').filter(Boolean)
     answer = numbers.filter((number)=>isPrime(number)).length
 
     return answer;
