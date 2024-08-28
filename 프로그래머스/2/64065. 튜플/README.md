1c446dcbb3dcf300d57632ea05f0d800dd74a890
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-08-28 21:55:07 +0900

    [level 2] Title: 튜플, Time: 49.98 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 43.1 MB, 시간: 45.09 ms
+메모리: 43.2 MB, 시간: 49.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 21일 10:47:35
+2024년 08월 28일 21:55:04
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,15 +1,9 @@
+function solution(s) {
+    var answer = s.slice(2,-2).split('},{')
+    .map((str)=>str.split(',').map(Number))
+    .sort((a,b)=>a.length - b.length)
+    .reduce((prev,cur)=> [...prev,...cur.filter((value)=>!prev.includes(value))],[])
+    
 
-
-
-const tupleFrom = (str) =>
-  str.slice(2, -2).split('},{')
-    .map((it) => toNumbers(it))
-    .sort(accendingByLength)
-    .reduce((acc, cur) =>
-      [...acc, ...cur.filter((it) => !acc.includes(it))], []);
-
-const toNumbers = (str) => str.split(',').map(it => Number(it));
-
-const accendingByLength = (arr1, arr2) => arr1.length - arr2.length;
-
-const solution = (s) => tupleFrom(s);
+    return answer;
+}
