953331ebe9a6466c0bbfdae8e963a8507ef6b862
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-09-04 11:17:06 +0900

    [level 2] Title: 우박수열 정적분, Time: 3.83 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.js
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.js
@@ -1,41 +1,18 @@
-function getPosition(y){
-    const position = []
-    let x = 0
-    position.push([x,y])
-    while(y>1){
-        if(y%2) y = y*3 +1
-        else y = y/2
-        x +=1
-        position.push([x,y])
-    }
-    return position
-}
-
-function getArea(a,b,position){
-    const n = position.length-1
-    const start = a
-    const end = n+b
-    if(start>end) return -1
-    let sum =0
-    
-    for(let x=start;x<end;x++){
-
-        const [x1,y1] = position[x]
-        const [x2,y2] = position[x+1]
-
-        sum += (x2-x1)*Math.min(y1,y2) + (x2-x1) * Math.abs(y1-y2) / 2
-    }
-    return sum
-}
-
 function solution(k, ranges) {
-    var answer = [];
-    const position = getPosition(k)
-
-    
-    for(const [a,b] of ranges){
-        answer.push(getArea(a,b,position))
+  const integral = [0];
+  while (k !== 1) {
+      // console.log('-1',integral.at(-1))
+    if (k % 2) {
+      integral.push((k + k * 3 + 1) / 2 + integral.at(-1));
+      k = k * 3 + 1;
+    } else {
+      integral.push((k * 3) / 4 + integral.at(-1));
+      k /= 2;
     }
-    
-    return answer;
+  }
+    // console.log(integral)
+  return ranges.map(([s, e]) => {
+    if (integral.length - 1 + e < s) return -1;
+    return integral.at(e-1) - integral[s];
+  });
 }
