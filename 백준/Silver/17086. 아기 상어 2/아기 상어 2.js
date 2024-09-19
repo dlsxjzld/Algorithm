8cf5ee2ec7ce32038bf450ee154933cf2c2d7a7c
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-09-19 18:39:11 +0900

    [Silver II] Title: 아기 상어 2, Time: 176 ms, Memory: 12604 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.js
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.js
@@ -1,69 +1,52 @@
-class Queue {
-  constructor() {
-    this.queue = [];
-    this.front = 0;
-    this.rear = 0;
-  }
-  enqueue(value) {
-    this.queue[this.rear++] = value;
-  }
-  dequeue() {
-    const returnValue = this.queue[this.front];
-    delete this.queue[this.front];
-    this.front += 1;
-    return returnValue;
-  }
-  size() {
-    return this.rear - this.front;
-  }
-}
-const input = require("fs")
+const input = 
+  require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split("\n");
+  .split("\n")
 
-const [n, m] = input[0].split(" ").map(Number);
-const graph = input.slice(1, n + 1).map((row) => row.split(" ").map(Number));
+const [n, m] = input[0].split(" ").map(Number)
+const board = input.slice(1).map((row) => row.split(" ").map(Number))
 const visited = Array.from({ length: n }, () =>
-  Array.from({ length: m }, () => false)
-);
-const babySharks = new Queue();
-
-for (let ridx = 0; ridx < n; ridx += 1) {
-  for (let cidx = 0; cidx < m; cidx += 1) {
-    if (graph[ridx][cidx] === 1) {
-      babySharks.enqueue([ridx, cidx]);
-      visited[ridx][cidx] = true;
+  Array.from({ length: m }, () => false),
+)
+const move = [
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+]
+const sharks = []
+board.forEach((row, r) =>
+  row.forEach((val, c) => {
+    if (val === 1) {
+      sharks.push([r, c])
+      visited[r][c] = true
     }
-  }
-}
-const dx = [0, 1, 1, 1, 0, -1, -1, -1]; // 동 동남 남 남서 서 서북 북 북동
-const dy = [1, 1, 0, -1, -1, -1, 0, 1];
+  }),
+)
 
-function bfs() {
-  while (babySharks.size() > 0) {
-    const [x, y] = babySharks.dequeue();
+const bfs = (sharks, board, visited) => {
+  const queue = [...sharks]
+  let index = 0
+  while (queue.length > index) {
+    const [x, y] = queue[index++]
 
     for (let i = 0; i < 8; i++) {
-      const nx = x + dx[i];
-      const ny = y + dy[i];
-      if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny] === true)
-        continue;
-      if (visited[nx][ny] === false) {
-        visited[nx][ny] = true;
-        graph[nx][ny] = graph[x][y] + 1;
-        babySharks.enqueue([nx, ny]);
-      } else if ((visited[nx][ny] = true)) {
-        if (graph[nx][ny] > graph[x][y] + 1) {
-          graph[nx][ny] = graph[x][y] + 1;
-        }
-      }
+      const [nx, ny] = [x + move[i][0], y + move[i][1]]
+
+      if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny]) continue
+      queue.push([nx, ny])
+      visited[nx][ny] = true
+      board[nx][ny] = board[x][y] + 1
     }
   }
 }
+bfs(sharks, board, visited)
 
-bfs();
-
-const answer = Math.max(...graph.map((row) => Math.max(...row)));
-console.log(answer - 1);
+const answer = Math.max(...board.flatMap((row) => row)) - 1
+console.log(answer)
