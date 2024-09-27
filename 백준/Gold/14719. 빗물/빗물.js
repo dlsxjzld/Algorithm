439dfc48124478fbb4860f97c2781c6f1d423cfd
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-09-27 21:46:53 +0900

    [Gold V] Title: 빗물, Time: 96 ms, Memory: 11264 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.js
+++ 백준/Gold/14719. 빗물/빗물.js
@@ -4,41 +4,24 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const [n, m] = input[0].split(" ").map(Number)
+const [h, w] = input[0].split(" ").map(Number)
+const heights = input[1].split(" ").map(Number)
 
-const heightList = input[1].split(" ").map(Number)
-const graph = Array.from({ length: n }, () =>
-  Array.from({ length: m }, () => 0),
-)
+/**
+ * 고이는 빗물의 총량
+ * 고이려면 블록과 블록 사이에 공간이 있어야함
+ * 0번째 인덱스와 w-1번째 인덱스는 고일 수 없음 (마지막 벽들이므로)
+ * 현재 인덱스 기준으로 가장 큰 왼쪽 벽, 가장 큰 오른쪽 벽 찾아야함.
+ * 찾았으면 그 중에 더 낮은 높이 벽만큼 물이 찰 수 있음
+ */
+let total = 0 // 빗물의 양
 
-heightList.forEach((height, col) => {
-  for (let i = 0; i < height; i++) {
-    graph[i][col] = 1
-  }
-})
-
-let answer = 0 // 빗물의 총합
-for (let i = 0; i < n; i++) {
-  // 새로운 행 시작할 때 벽 위치, 빗물 개수 초기화
-  // -1 -1 로 둬서 현재 벽 없음으로 초기화
-  let current_wall = { x: -1, y: -1 } // 행의 벽 위치
-  let rain = 0 // 행의 빗물
-
-  for (let j = 0; j < m; j++) {
-    if (graph[i][j] == 1) {
-      // 벽 만나면 벽 위치 초기화 및 빗물 더하기
-      current_wall.x = i
-      current_wall.y = j
-      answer += rain
-      rain = 0
-    } else {
-      // 벽과 벽 사이에 있는 빈 칸만 빗물임
-      // 벽을 한 번은 만났어야 빗물 개수 셀 수 있음
-      if (current_wall.x !== -1 && current_wall.y !== -1) {
-        rain += 1 //
-      }
-    }
-  }
+for (let i = 1; i < w - 1; i++) {
+  let current = heights[i]
+  let left = Math.max(...heights.slice(0, i))
+  let right = Math.max(...heights.slice(i + 1))
+  let compare = Math.min(left, right)
+  total += compare > current ? compare - current : 0
 }
 
-console.log(answer)
+console.log(total)
