fdcbea1eb1bc52294245b4883bea6660c4996dd1
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-09-29 16:19:29 +0900

    [Silver II] Title: DNA 비밀번호, Time: 240 ms, Memory: 33748 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.js
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.js
@@ -13,39 +13,27 @@ const currentChar = {
   G: 0,
   T: 0,
 }
-let answer = 0
-
-for (let i = 0; i < p; i++) {
-  if (currentChar[dna[i]] != undefined) {
-    currentChar[dna[i]] += 1
-  }
-}
-if (
-  currentChar["A"] >= a &&
-  currentChar["C"] >= c &&
-  currentChar["G"] >= g &&
-  currentChar["T"] >= t
-) {
-  answer += 1
-}
-
-for (let i = 1; i < s - p + 1; i++) {
-  if (currentChar[dna[i - 1]] != undefined) {
-    currentChar[dna[i - 1]] -= 1
-    currentChar[dna[i - 1]] =
-      currentChar[dna[i - 1]] < 0 ? 0 : currentChar[dna[i - 1]]
-  }
-  if (currentChar[dna[i + p - 1]] != undefined) {
-    currentChar[dna[i + p - 1]] += 1
-  }
+const check = () => {
   if (
     currentChar["A"] >= a &&
     currentChar["C"] >= c &&
     currentChar["G"] >= g &&
     currentChar["T"] >= t
   ) {
-    answer += 1
+    return 1
   }
+  return 0
+}
+let answer = 0
+for (let i = 0; i < p; i++) {
+  currentChar[dna[i]] += 1
+}
+answer += check()
+
+for (let i = 1; i < s - p + 1; i++) {
+  currentChar[dna[i - 1]] -= 1
+  currentChar[dna[i + p - 1]] += 1
+  answer += check()
 }
 
 console.log(answer)
