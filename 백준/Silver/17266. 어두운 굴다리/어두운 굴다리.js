e556c7d6678c57b317517f4ab95d83f0348d0e9e
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-01 20:52:37 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 196 ms, Memory: 24852 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.js
@@ -4,26 +4,43 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const n = +(input[0])
-const m = +(input[1])
-const position = input[2].split(" ").map(Number)
+const n = Number(input[0]) // 0 ~ n
+const m = Number(input[1])
+const x = [0, ...input[2].split(" ").map(Number), n]
+// 이분탐색으로 높이를 구하고 이 높이가 모든 굴다리르 비출 수 있는지 확인해야함
+// 높이의 최댓값은 n
 
-for (let h = 1; ; h++){
-  if (position[0] - h > 0)
-    continue;
-  if (position[m - 1] + h < n)
-    continue;
-
-  let isHeightOkay = true;
-  for (let i = 0; i < m; i++){
-    let right = position[i] + h, leftNext = position[i + 1] - h;
-    if (right < leftNext) {
-      isHeightOkay = false;
-      break;
+// 높이가 안되는 경우
+// x[1] - 0 > height
+// x[n-1] - x[n-2] > height
+// (x[2] - x[1])/2 > height , (x[3] - x[2])/2 > height...
+const canLight = (height) => {
+  if (x[1]-x[0] > height) {
+    return false
+  }
+  if (x[x.length-1] - x[x.length-2] > height) {
+    return false
+  }
+  for (let i = 1; i <= m; i++) {
+    if ((x[i + 1] - x[i]) / 2 > height) {
+      return false
     }
   }
-  if (isHeightOkay) {
-    console.log(h);
-    break;
+  return true
+}
+
+let start = 0
+let end = n
+let answer = n
+while (start < end) {
+  let mid = Math.floor((start + end) / 2)
+
+  if (canLight(mid)) {
+    end = mid
+    answer = mid
+  } else {
+    start = mid + 1
   }
 }
+
+console.log(answer)
