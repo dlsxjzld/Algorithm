ee475f16b3349a97e35d970940fbb4ccb64062ac
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-02 19:06:49 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 100 ms, Memory: 9400 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.js
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.js
@@ -1,54 +1,49 @@
-const input = require('fs')
-  .readFileSync('/dev/stdin')
+const input = require("fs")
+  .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split('\n');
+  .split("\n")
 
-// 1. 지구 -> 달로 가는 경우 우주선이 움직일 수 있는 방향은 아래와 같다.
-// 2. 우주선은 전에 움직인 방향으로 움직일 수 없다. 즉, 같은 방향으로 두번 연속으로 움직일 수 없다.
-// 3. 연료를 최대한 아끼며 지구의 어느위치에서든 출발하여 달의 어느위치든 착륙하는 것
-const MAX = Number.MAX_SAFE_INTEGER;
-const [n, m] = input[0].split(' ').map(Number);
-const distance = [
-    Array.from({ length: m }, () => 0),
-  ...input.slice(1, 1 + n).map((row) => row.split(' ').map(Number)),
-];
-
+const [n, m] = input[0].split(" ").map(Number)
+const values = [Array.from({ length: m }, () => 0)].concat(
+  input.slice(1).map((row) => row.split(" ").map(Number)),
+)
 
 const dp = Array.from({ length: n + 1 }, () =>
-  Array.from({ length: m }, () => Array.from({ length: 3 }, () => MAX))
-);
-
-const direction = [
-  [-1, 1],
-  [-1, 0],
-  [-1, -1],
-]; // 남서, 남, 남동
+  Array.from({ length: m }, () => Array.from({ length: 3 }, () => Infinity)),
+)
 
 for (let i = 0; i < m; i++) {
   for (let j = 0; j < 3; j++) {
-    dp[0][i][j] = 0;
+    dp[0][i][j] = 0
   }
 }
+const dir = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+]
 
-for (let x = 1; x < n + 1; x++) {
-  for (let y = 0; y < m; y++) {
-    for (let idx = 0; idx < 3; idx++) {
-      const prevX = x + direction[idx][0];
-      const prevY = y + direction[idx][1];
+for (let r = 1; r <= n; r++) {
+  for (let c = 0; c < m; c++) {
+    for (let direction = 0; direction < 3; direction++) {
+      const [pr, pc] = [r - dir[direction][0], c - dir[direction][1]]
 
-      if (prevY < 0 || prevY >= m) {
-        dp[x][y][idx] = MAX;
-        continue;
+      if (pr < 0 || pc < 0 || pr > n || pc >= m) continue
+      if (c === 0 && direction === 0) {
+        continue
+      } else if (c === m - 1 && direction === 2) {
+        continue
+      } else {
+        dp[r][c][direction] =
+          values[r][c] +
+          Math.min(
+            dp[pr][pc][(direction + 1) % 3],
+            dp[pr][pc][(direction + 2) % 3],
+          )
       }
-      dp[x][y][idx] =
-        distance[x][y] +
-        Math.min(
-          dp[prevX][prevY][(idx + 1) % 3],
-          dp[prevX][prevY][(idx + 2) % 3]
-        );
     }
   }
 }
 
-console.log(Math.min(...dp[n].flat()));
+console.log(Math.min(... dp[n].flat()))
