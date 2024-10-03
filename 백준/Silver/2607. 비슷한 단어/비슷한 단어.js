4b79a2df67baeca54018f49792bc2e6b5f99251b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-03 16:15:10 +0900

    [Silver II] Title: 비슷한 단어, Time: 92 ms, Memory: 9412 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.js
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.js
@@ -4,62 +4,23 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const count = (str) => {
-  const cnt = {}
-  for (let char of str) {
-    if (!cnt[char]) {
-      cnt[char] = 0
-    }
-    cnt[char] += 1
-  }
-  return cnt
-}
 
 const n = Number(input[0])
+const original = input[1]
 let answer = 0
 
-const [original, ...rest] = input.slice(1)
-const [originalCnt, ...restCnt] = [original, ...rest].map((val) => count(val))
+const target = input.slice(2)
 
-for (let i = 0; i < n-1; i++) {
-  const target = rest[i]
-  const tmpOriginalCnt = { ...originalCnt }
-  const targetCnt = restCnt[i]
-  if (original.length > target.length) {
-    // original에서 빼거나
-    for (let key of target) {
-      tmpOriginalCnt[key] -= 1
-      if (tmpOriginalCnt[key] == 0) {
-        delete tmpOriginalCnt[key]
-      }
+for(let i=0;i<n-1;i++){
+  let originalCnt = original
+  for(let char of original){
+    if(target[i].includes(char)){
+      target[i] = target[i].replace(char,'')
+      originalCnt = originalCnt.replace(char,'')
     }
-    if (Object.values(tmpOriginalCnt).reduce((a, b) => a + b) === 1) {
-      answer += 1
-    }
-  } else if (original.length < target.length) {
-    // original에서 더하거나
-    for (let key of original) {
-      targetCnt[key] -= 1
-      if (targetCnt[key] == 0) {
-        delete targetCnt[key]
-      }
-    }
-    if (Object.values(targetCnt).reduce((a, b) => a + b) === 1) {
-      answer += 1
-    }
-  } else {
-    // original에서 변경
-    for (let key of target) {
-      if(tmpOriginalCnt[key]){  
-        tmpOriginalCnt[key] -= 1
-          if (tmpOriginalCnt[key] == 0) {
-            delete tmpOriginalCnt[key]
-          }
-      }
-    }
-    if (Object.values(tmpOriginalCnt).length === 0 || Object.values(tmpOriginalCnt).reduce((a,b)=>a+b) === 1  ) {
-      answer += 1
-    }
+  }
+  if(originalCnt.length<=1 && target[i].length<=1){
+    answer +=1
   }
 }
 console.log(answer)
