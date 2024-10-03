4e37c9eee6d92eb6172270aa9faab7477108f3a4
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-03 15:56:54 +0900

    [Silver II] Title: 비슷한 단어, Time: 96 ms, Memory: 9732 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.js
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.js
@@ -1,36 +1,65 @@
 const input = require("fs")
-    .readFileSync("/dev/stdin")
-    .toString()
-    .trim()
-    .split("\n");
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
 
-const num = input[0];
-input.splice(0, 1);
-const target = input[0];
-input.splice(0, 1);
-const words = input;
+const count = (str) => {
+  const cnt = {}
+  for (let char of str) {
+    if (!cnt[char]) {
+      cnt[char] = 0
+    }
+    cnt[char] += 1
+  }
+  return cnt
+}
 
-// target : 첫 단어
-// words : 비슷한 단어인지 확인할 단어들
-const replacedWord = words.map((word) => {
-    // targetWord : 첫 단어
-    let targetWord = target.split("").join("");
+const n = Number(input[0])
+let answer = 0
 
-    target.split("").forEach((char) => {
-        // 단어가 첫 단어의 문자들을 갖고 있는지 확인
-        if (word.includes(char)) {
-            word = word.replace(char, "");
-            // 단어에서 첫 단어와 겹치는 문자들 지우기
-            targetWord = targetWord.replace(char, "");
-            // 첫 단어에서 겹치는 문자들 지우기
-        }
-    });
-    // 단어의 남은 문자의 길이 , 첫 단어에서 겹치는 문자 지우고 남은 길이
-    return [word.length, targetWord.length];
-});
+const [original, ...rest] = input.slice(1)
+const [originalCnt, ...restCnt] = [original, ...rest].map((val) => count(val))
 
-console.log(
-    replacedWord.filter(
-        ([wordCnt, targetCnt]) => wordCnt <= 1 && targetCnt <= 1
-    ).length
-);
+for (let i = 0; i < n-1; i++) {
+  const target = rest[i]
+  const tmpOriginalCnt = { ...originalCnt }
+  const targetCnt = restCnt[i]
+  if (original.length > target.length) {
+    // original에서 빼거나
+    for (let key of target) {
+      tmpOriginalCnt[key] -= 1
+      if (tmpOriginalCnt[key] == 0) {
+        delete tmpOriginalCnt[key]
+      }
+    }
+    if (Object.values(tmpOriginalCnt).reduce((a, b) => a + b) === 1) {
+      answer += 1
+    }
+  } else if (original.length < target.length) {
+    // original에서 더하거나
+    for (let key of original) {
+      targetCnt[key] -= 1
+      if (targetCnt[key] == 0) {
+        delete targetCnt[key]
+      }
+    }
+    if (Object.values(targetCnt).reduce((a, b) => a + b) === 1) {
+      answer += 1
+    }
+  } else {
+    // original에서 변경
+    for (let key of target) {
+      if(tmpOriginalCnt[key]){  
+        tmpOriginalCnt[key] -= 1
+          if (tmpOriginalCnt[key] == 0) {
+            delete tmpOriginalCnt[key]
+          }
+      }
+    }
+    if (Object.values(tmpOriginalCnt).length === 0 || Object.values(tmpOriginalCnt).reduce((a,b)=>a+b) === 1  ) {
+      answer += 1
+    }
+  }
+}
+console.log(answer)
