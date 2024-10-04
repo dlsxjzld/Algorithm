0c62d3ae2c2dd60dfc2628b6d23103e556c8a3fb
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-04 21:27:09 +0900

    [Silver II] Title: KCPC, Time: 140 ms, Memory: 33164 KB -BaekjoonHub

--- 백준/Silver/3758. KCPC/KCPC.py
+++ 백준/Silver/3758. KCPC/KCPC.py
@@ -2,15 +2,6 @@ import sys
 
 input = sys.stdin.readline
 
-# id, 문제 번호, 점수 -> 시간 순서대로 저장
-# 한 문제에 대한 풀이를 여러 번 제출 가능 -> 그 중 최고 점수가 최종 점수
-# 풀이를 한번도 제출하지 않으면 최종 점수는 0
-
-# 팀의 최종 점수는 각 문제에 대해 받은 점수의 총합, 순위는 당신 팀보다 높은 점수를 받은 팀의 수 + 1
-# 점수가 동일하면
-# 1. 풀이를 제출한 횟수가 적은 팀의 순위가 높다
-# 2. 제출 횟수도 같다면, 마지막 제출 시간이 더 빠른 팀의 순위가 높다.
-
 T = int(input())
 
 def getTeamInfo (teamId,problemCnt):
@@ -24,10 +15,7 @@ def getTeamInfo (teamId,problemCnt):
 
 answer =[]
 for tc in range(0,T):
-  # 팀의 개수, 문제의 개수, 당신 팀의 ID, 로그 엔트리의 개수
   n,k,yourId,m = map(int,input().split(' '))
-  
-  # log -> [1,1,30] -> 팀 ID, 문제 번호, 획득 점수
   logs = [list(map(int,input().split(' '))) for _ in range(0,m)]
   teams = [getTeamInfo(i,k) for i in range(1,n+1)]
 
