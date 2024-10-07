1f0da96187a474ae8a2b11f9943f062c3c814fdd
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-07 21:30:59 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 360 ms, Memory: 70328 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.js
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.js
@@ -4,51 +4,43 @@ const input = require("fs")
   .trim()
   .split("\n")
 
+// 10000 이하 WEAK
+// 10,000 초과 100,000 이하 NORMAL
+// 100,000 초과 1,000,000 이하 STRONG
+
+// 칭호의 개수 N 1<= N <= 10^5
+// 칭호를 출력해야 하는 캐릭터들의 개수 M 1<= M <= 10^5
+
 const [n, m] = input[0].split(" ").map(Number)
 
-const rankingForMedal = {}
-
-// 점수 : 칭호 객체 생성
-input.slice(1, 1 + n).forEach((row) => {
-  const [medal, score] = row.split(" ")
-  if (rankingForMedal[score] == null) {
-    rankingForMedal[score] = medal
-  }
+const medals = input.slice(1, 1 + n).map((row) => {
+  const [name, value] = row.split(" ")
+  return [Number(value), name]
 })
-
-// 점수 값들만 가진 ranking 배열 만들기
-const ranking = Object.keys(rankingForMedal).map((value) => Number(value))
-
-const userScoreList = input.slice(1 + n).map((val) => Number(val))
-
+const values = input.slice(1 + n).map(Number)
 const answer = []
 
-const checkScore = (targetScore) => {
-  let start = 0
-  let end = ranking.length - 1
+const binarySearch = (s, e, array, target) => {
+  let answer = null
 
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2)
-    
-    if (targetScore <= ranking[mid]) {
-      if (mid !== 0) {
-        if (ranking[mid - 1] < targetScore) {
-          answer.push(rankingForMedal[ranking[mid]])
-          break
-        }
-      } else {
-        answer.push(rankingForMedal[ranking[0]])
-        break
-      }
-      end = mid - 1
-    } else if (targetScore >= ranking[mid]) {
-      start = mid + 1
+  while (s <= e) {
+    let mid = Math.floor((s + e) / 2)
+
+    if (array[mid][0] >= target) {
+      answer = array[mid][1]
+      e = mid - 1
+    } else if (array[mid][0] < target) {
+      s = mid + 1
     }
   }
+  return answer
 }
 
-for (let userScore of userScoreList) {
-  checkScore(userScore)
+for (let target of values) {
+  let s = 0
+  let e = n - 1
+
+  answer.push(binarySearch(s, e, medals, target))
 }
 
 console.log(answer.join("\n"))
