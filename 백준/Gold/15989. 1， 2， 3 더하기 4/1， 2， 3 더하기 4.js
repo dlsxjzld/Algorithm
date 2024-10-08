3fd83b952f87952e00cf4693e57c4c4fb4831ef4
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-08 21:17:14 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 192 ms, Memory: 13000 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.js
@@ -0,0 +1,27 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const t = Number(input[0])
+
+const numbers = input.slice(1).map(Number)
+const dp = Array.from({ length: 10001 }, () =>
+  Array.from({ length: 4 }, () => 0),
+)
+dp[1][1] = 1
+dp[2][1] = 1
+dp[2][2] = 1
+dp[3][1] = 1
+dp[3][2] = 1
+dp[3][3] = 1
+
+for (let i = 4; i < 10001; i++) {
+  dp[i][1] = dp[i - 1][1]
+  dp[i][2] = dp[i - 2][2] + dp[i-2][1]
+  dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3]
+}
+for(let num of numbers){
+  console.log(dp[num][1] + dp[num][2] + dp[num][3])
+}
