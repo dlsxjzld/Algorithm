12533bec1f3af7f15f16d557a9b0478231422f69
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-08 17:19:08 +0900

    [Silver II] Title: 주식, Time: 2228 ms, Memory: 426492 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.js
+++ 백준/Silver/11501. 주식/주식.js
@@ -1,26 +1,23 @@
-const input = require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
-const t = Number(input[0])
-const answer = []
-let index = 1
-for (let tc = 0; tc < t; tc++) {
-  const n = Number(input[index++])
-  const prices = input[index++].split(" ").map(Number)
-  let maxPrice = 0
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+main();
 
-  let total = 0
-
-  for (let i = n - 1; i >= 0; i--) {
-    if (maxPrice < prices[i]) {
-      maxPrice = prices[i]
-    } else if (maxPrice >= prices[i]) {
-      total += maxPrice - prices[i]
+function main(){
+    let answer ="";
+    for(let i = 2; i < input.length; i += 2) {
+        answer += calc(i) + "\n";
     }
-  }
-  answer.push(total)
+    console.log(answer);
 }
 
-console.log(answer.join("\n"))
+function calc(i){
+    let stockFlow = input[i].trim().split(' ').map(e => parseInt(e));
+    let profit = 0;
+    let maxPrice = 0;
+    for(let j = stockFlow.length - 1; j >= 0; j--) {
+        if (stockFlow[j] >= maxPrice) {
+            maxPrice = stockFlow[j];
+        }
+        profit += maxPrice - stockFlow[j];
+    }
+    return profit;
+}
