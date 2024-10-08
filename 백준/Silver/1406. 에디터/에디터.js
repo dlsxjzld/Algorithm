b95f390ed57020802fc8e75d46cd7ba999cee95c
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-08 17:32:50 +0900

    [Silver II] Title: 에디터, Time: 324 ms, Memory: 82696 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.js
+++ 백준/Silver/1406. 에디터/에디터.js
@@ -1,12 +1,12 @@
 const filePath = process.platform === 'linux' ? 
 '/dev/stdin' : 'example.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
-let rightStack = [];
+const rightStack = [];
 let l = input.splice(0, 1);
 const str = l[0].split('');
 
 
-const comand = +input[0];
+const comand = Number(input[0])
 
 for (let i = 1; i <=comand; i++){
     switch (input[i][0]) { 
