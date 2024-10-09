1fecf77d6f57efc63a35659e8cbaa498d847d8b1
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-09 14:58:04 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 96 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.js
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.js
@@ -4,28 +4,23 @@ const input = require("fs")
   .trim()
   .split("\n")
 const [n, k] = input[0].split(" ").map(Number)
+const result = []
+const array = []
 
-const num = [1, 2, 3]
-const solution = (target) => {
-  const numbers = []
-  const getNum = (target, array) => {
-    const total = array.reduce((a, b) => a + b, 0)
-    if (total > target) {
-      return
-    }
-    if (total == target) {
-      numbers.push(array.join(""))
-    }
-    for (let i = 0; i < 3; i++) {
-      array.push(num[i])
-      getNum(target, array)
-      array.pop()
-    }
+const solution = (total) => {
+  if (total > n) {
+    return
   }
-  getNum(target, [])
-  numbers.sort((a, b) => (a < b ? -1 : 1))
-  return numbers
+  if (total == n) {
+    result.push([...array])
+    return
+  }
+  for (let i = 1; i < 4; i++) {
+    array.push(i)
+    solution(total + i)
+    array.pop()
+  }
 }
 
-const answer = solution(n).map((val)=>val.split('').join('+'))[k-1] ?? -1
-console.log(answer)
+solution(0)
+console.log(result.length >=k ? result[k-1].join('+') : -1)
