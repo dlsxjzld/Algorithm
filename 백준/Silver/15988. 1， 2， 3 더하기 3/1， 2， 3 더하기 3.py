5bca5004a59352ec23d2728b6fa3ed7d922b83d0
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-09 15:24:08 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 320 ms, Memory: 71128 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -1,9 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-dp = [1,2,4,7]
+dp = [0,1,2,4,7]
 for tc in range(int(input())):
     n = int(input())
-    for i in range(len(dp), n):
+    for i in range(len(dp), n+1):
         dp.append((dp[-3] + dp[-2] + dp[-1]) % 1000000009)
-    print(dp[n-1])
+    print(dp[n])
