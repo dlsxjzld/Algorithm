9923e58008d8be52adda09ae8af4a7df784319c8
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-10 11:42:23 +0900

    [Silver II] Title: N번째 큰 수, Time: 984 ms, Memory: 19268 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.js
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.js
@@ -1,109 +1,87 @@
-const readline = require("readline")
+  const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    class MinHeap {
+        constructor() {
+            this.heap = [null];
 
-class MinHeap {
-  constructor() {
-    this.heap = []
-  }
-
-  enqueue(value) {
-    this.heap.push(value)
-    this.bubbleUp()
-  }
-
-  bubbleUp() {
-    let currentIdx = this.heap.length - 1
-
-    while (currentIdx > 0) {
-      let parentIdx = Math.floor((currentIdx - 1) / 2)
-      const current = this.heap[currentIdx]
-      const parent = this.heap[parentIdx]
-
-      if (current < parent) {
-        this.heap[parentIdx] = current
-        this.heap[currentIdx] = parent
-        currentIdx = parentIdx
-      } else {
-        break
-      }
-    }
-  }
-
-  size() {
-    return this.heap.length
-  }
-
-  dequeue() {
-    const Length = this.size()
-    if (Length === 0) return 0
-    if (Length === 1) return this.heap.pop()
-
-    const Min = this.heap[0]
-    const End = this.heap.pop()
-    this.heap[0] = End
-
-    let currentIdx = 0
-
-    while (true) {
-      let leftIdx = currentIdx * 2 + 1
-      let rightIdx = currentIdx * 2 + 2
-      let current = this.heap[currentIdx]
-      let leftChild = null
-      let rightChild = null
-      let swap = null
-      if (leftIdx < this.heap.length) {
-        leftChild = this.heap[leftIdx]
-        if (leftChild < current) {
-          swap = leftIdx
         }
-      }
-      if (rightIdx < this.heap.length) {
-        rightChild = this.heap[rightIdx]
-        if (
-          (swap === null && rightChild < current) ||
-          (swap !== null && rightChild < leftChild)
-        ) {
-          swap = rightIdx
+
+        Push(item) {
+            let cur = this.heap.length;
+            while (cur > 1) {
+                let parent = Math.floor(cur / 2);
+                if (item < this.heap[parent]) {
+                    this.heap[cur] = this.heap[parent];
+                    cur = parent;
+                } else break;
+            }
+            this.heap[cur] = item;
         }
-      }
-      if (swap === null) break
-      this.heap[currentIdx] = this.heap[swap]
-      this.heap[swap] = current
-      currentIdx = swap
+
+        remove() {
+            const removeItem = this.heap[1];
+            if (this.heap.length > 2) {
+                this.heap[1] = this.heap[this.heap.length - 1];
+                this.heap.pop();
+                let cur = 1;
+                let leftChild = cur * 2;
+                let rightChild = cur * 2 + 1;
+                while (this.heap[leftChild]) {
+                    let CompareChild = leftChild;
+                    if (this.heap[rightChild]) {
+                        if (this.heap[leftChild] > this.heap[rightChild]) {
+                            CompareChild = rightChild;
+                        }
+                    }
+                    if (this.heap[CompareChild] < this.heap[cur]) {
+                        [this.heap[CompareChild], this.heap[cur]] = [this.heap[cur], this.heap[CompareChild]];
+                        cur = CompareChild;
+                    }else break;
+                    leftChild = cur * 2;
+                    rightChild = cur * 2 + 1;
+                }
+
+            }else if (this.heap.length === 2) {
+                this.heap.pop();
+            } else {
+                return 0;
+            }
+            return removeItem;
+        }
+
+        getSize() {
+            return this.heap.length - 1;
+        }
+
     }
 
-    return Min
-  }
-}
+    const minHeap = new MinHeap();
 
-const minHeap = new MinHeap()
-
-let n = -1
-let end = -1
-let answer = null
-rl.on("line", function (line) {
-  // 입력의 첫번째 줄인 행렬의 사이즈를 나타내는 값 저장
-  if (n === -1) {
-    n = parseInt(line)
-    end = n
-    return
-  }
-  // 기존 for문과 동일
-  // minHeap에 행렬 Push 후 길이에 따라 minHeap에서 값 제거
-  line.split(" ").forEach((value) => {
-    minHeap.enqueue(parseInt(value))
-    if (minHeap.heap.length > end) minHeap.dequeue()
-  })
-  // 한줄이 끝날 때마다 N 감소
-  n--
-
-  // 만약 모든 줄 다 봤으면 종료
-  if (n === 0) rl.close()
-}).on("close", function () {
-  console.log(minHeap.dequeue())
-  process.exit()
-})
+	// 다른 코드를 참조하여 작성
+	// 입력 받은 줄의 수를 세기 위한 변수
+    let N = -1;
+    rl.on("line", function (line) {
+      	// 입력의 첫번째 줄인 행렬의 사이즈를 나타내는 값 저장
+        if (N === -1) {
+            N = parseInt(line);
+            n = N;
+            return;
+        }
+      	// 기존 for문과 동일 
+      	// minHeap에 행렬 Push 후 길이에 따라 minHeap에서 값 제거
+        line.split(' ').forEach((value) => {
+            minHeap.Push(parseInt(value));
+            if(minHeap.getSize() > n) minHeap.remove();
+        });
+      	// 한줄이 끝날 때마다 N 감소
+        N--;
+      	// 만약 모든 줄 다 봤으면 종료
+        if (N === 0) rl.close();
+    }).on("close", function () {
+        console.log(minHeap.remove());
+        process.exit();
+    });
