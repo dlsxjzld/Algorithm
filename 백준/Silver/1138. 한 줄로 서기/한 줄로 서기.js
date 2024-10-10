56d132afa29069708b1d96f53906c88abb8562e8
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-10 12:57:23 +0900

    [Silver II] Title: 한 줄로 서기, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.js
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.js
@@ -3,23 +3,18 @@ const input = require("fs")
   .toString()
   .trim()
   .split("\n")
+
+// 사람들은 자기보다 큰 사람이 왼쪽에 몇 명 있었는지만을 기억한다.
+// N명의 사람이 있고, 사람들의 키는 1부터 N까지 모두 다르다.
 const n = Number(input[0])
-
-const memory = input[1].split(" ").map(Number).reverse()
-
-let answer = []
-for (let i = 0; i < n; i++) {
-  const x = n - i
-  if (x === n) {
-    answer.push( x.toString())
-  } else {
-    if (memory[i] > 0) {
-      const before = answer.slice(0, memory[i]) 
-      const after = answer.slice(memory[i])
-      answer = [...before, x.toString() ,...after]
-    } else {
-      answer.unshift(x.toString())
-    }
+const height = input[1].split(" ").map(Number)
+let str = [n.toString()]
+for (let i = n - 2; i >= 0; i--) {
+  if(height[i] == 0){
+    str = [(i+1).toString(), ...str]
+  }else{
+    str = [...str.slice(0,height[i]), (i+1).toString(), ...str.slice(height[i])]
   }
 }
-console.log(answer.join(' '))
+
+console.log(str.join(' '))
