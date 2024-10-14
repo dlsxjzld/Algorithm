9261c8202051997b97e04595df3053ae44e54f56
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-14 18:59:19 +0900

    [Silver I] Title: 숨바꼭질, Time: 180 ms, Memory: 16268 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,74 +1,27 @@
-const fs = require('fs');
-const PATH =
-  process.platform === 'linux' ? '/dev/stdin' : './baekjon/input.txt';
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
 
-const input = fs.readFileSync(PATH).toString().trim().split('\n');
+const [n, k] = input[0].split(" ").map(Number)
+const MAX = 100_000
+const dp = Array.from({ length: MAX + 1 }, () => -1)
 
-class Queue {
-  constructor() {
-    this.start = null;
-    this.end = null;
-    this.size = 0;
-  }
+const queue = [n]
+dp[n] = 0
+let index = 0
 
-  enqueue(val, time) {
-    const node = new Node(val, time);
-    if (this.start === null) {
-      this.start = node;
-      this.end = node;
-    } else {
-      this.end.next = node;
-      this.end = node;
-    }
-    this.size++;
-  }
+while (queue.length > index) {
+  const cur = queue[index++]
+  if (cur == k) continue
 
-  dequeue() {
-    if (this.size === 0) throw new Error('empty');
-    const deleteNode = this.start;
-    this.start = deleteNode.next;
-    deleteNode.next = null;
-    this.size--;
-    if (this.size === 0) this.end = null;
-
-    return deleteNode;
-  }
-}
-
-class Node {
-  constructor(val, time) {
-    this.val = val;
-    this.time = time;
-    this.next = null;
-  }
-}
-
-const [n, k] = input[0].split(' ').map(Number);
-
-const arr = new Array(100001).fill(Infinity);
-arr[n] = 0;
-const q = new Queue();
-q.enqueue(n, 0);
-
-while (q.size > 0) {
-  const { val, time } = q.dequeue();
-
-  for (let i of [-1, 1, 2]) {
-    let next;
-    if (i === 1) {
-      next = val + 1;
-    } else if (i === -1) {
-      next = val - 1;
-    } else {
-      next = val * 2;
-    }
-
-    if (next < 0 || next > 100001) continue;
-    if (arr[next] > time + 1) {
-      arr[next] = time + 1;
-      q.enqueue(next, time + 1);
+  for (let i of [cur - 1, cur + 1, cur * 2]) {
+    if (i >= 0 && i <= MAX && dp[i] == -1) {
+      dp[i] = dp[cur] + 1
+      queue.push(i)
     }
   }
 }
 
-console.log(arr[k]);
+console.log(dp[k])
