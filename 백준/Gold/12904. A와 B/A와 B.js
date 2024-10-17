e69eb708b6004d4a91da095fc82be5a8aae04b9c
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-17 16:32:33 +0900

    [Gold V] Title: A와 B, Time: 104 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.js
+++ 백준/Gold/12904. A와 B/A와 B.js
@@ -4,17 +4,24 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const S = input[0]
+/**
+ *
+ * 문자열의 뒤에 A를 추가한다.
+ * 문자열을 뒤집고 뒤에 B를 추가한다.
+ */
+
+const S = input[0].split("")
 const T = input[1].split("")
 
-while (S.length !== T.length) {
-  if (T[T.length - 1] === "A") {
+
+while(T.length !== S.length){
+
+  if(T.at(-1) == 'A'){
     T.pop()
-  } else {
+  }
+  else if(T.at(-1) == 'B'){
     T.pop()
     T.reverse()
   }
 }
-
-
-  console.log(S === T.join("") ?  1 : 0)
+console.log(Number(T.join('') == S.join('')))
