78df6250d153e197b5dc6207381bc8720d1922b1
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-17 14:54:33 +0900

    [Silver I] Title: 회전 초밥, Time: 164 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.js
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.js
@@ -4,49 +4,38 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-// 연속해서 k개 먹기 -> 추가 1접시
-// 초밥 가짓수의 최댓값 구하기
-// 1~n 넘어가면 n으로 나눈 나머지 가 인덱스
+const [N, d, k, c] = input[0].split(" ").map(Number)
+const sushi = input.slice(1).map(Number)
+const kind = {}
+let answer = Number.MIN_SAFE_INTEGER
+let cnt = 0
 
-const [n, d, k, c] = input[0].split(" ").map(Number)
-const food = input.slice(1).map(Number)
-
-let counting = {}
-
-// 0부터 k-1까지 구하기
 for (let i = 0; i < k; i++) {
-  if (counting[food[i]] === undefined) {
-    counting[food[i]] = 1
-  } else {
-    counting[food[i]] += 1
+  if (!kind[sushi[i]]) {
+    kind[sushi[i]] = 0
+    cnt += 1
   }
+  kind[sushi[i]] += 1
 }
+if (!kind[c]) {
+  kind[c] = 0
+  cnt += 1
+}
+kind[c] += 1
+answer = Math.max(answer, cnt)
 
-counting[c] = counting[c] === undefined ? 1 : counting[c] + 1 // 쿠폰
-let answer = Object.keys(counting).length // 초밥 가짓수의 최댓값
-
-let start = 0
-let dish = k
-let cnt = answer
-while (start < n) {
-  let end = dish % n
-
-  counting[food[start]] -= 1
-  if (counting[food[start]] === 0) {
-    delete counting[food[start]]
+for (let i = 1; i < N; i++) {
+  kind[sushi[i - 1]] -= 1
+  if (kind[sushi[i - 1]] == 0) {
     cnt -= 1
   }
-
-  if (counting[food[end]] === undefined) {
-    counting[food[end]] = 1
+  if (!kind[sushi[(i + k - 1) % N]]) {
+    kind[sushi[(i + k - 1) % N]] = 0
     cnt += 1
-  } else {
-    counting[food[end]] += 1
   }
+  kind[sushi[(i + k - 1) % N]] += 1
 
   answer = Math.max(answer, cnt)
-
-  start++
-  dish++
 }
+
 console.log(answer)
