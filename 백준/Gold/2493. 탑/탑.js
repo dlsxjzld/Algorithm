170879b4601d5f03d5c30281d02132156fdc6d35
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-18 21:58:02 +0900

    [Gold V] Title: 탑, Time: 600 ms, Memory: 157096 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.js
+++ 백준/Gold/2493. 탑/탑.js
@@ -1,33 +1,42 @@
-const input = require("fs")
-  .readFileSync("/dev/stdin")
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const [N, towers] = fs
+  .readFileSync(filePath, 'utf8')
   .toString()
   .trim()
-  .split("\n")
+  .split('\n')
+  .map((item) => item.split(' ').map(Number));
 
+const solution = (N, towers) => {
+  let answer = Array.from({length:N},() => 0);
+  const stack = [];
 
-  const N = Number(input[0])
-  const towers = input[1].split(' ').map(Number)
-const check = [...towers]
-  const heights = {}
-  const start = towers.pop()
-  const compare = [start]
-  heights[start] = 0
+  for (let i = 0; i < N; i++) {
+    const stackNode = {
+      idx: i + 1,
+      value: towers[i],
+    };
 
-  while(towers.length>0){
-    heights[towers[towers.length-1]] = 0
-    while(compare.length>0){
-      if(compare.at(-1) <= towers.at(-1)){
-          const cur = compare.pop()
-          heights[cur] = towers.length
-      }else{
-          break
-      }
+    // stack이 비어있다면 노드 추가
+    if (!stack.length) {
+      stack.push(stackNode);
+      continue;
     }
-    compare.push(towers.pop())
-  }
 
-let char = []
-for(let i=0;i<N;i++){
-    char.push(heights[check[i]].toString())
-}
-console.log(char.join(' '))
+    // top 노드의 값이 추가하려는 stackNode의 값보다 커질때까지 pop
+    // stack이 비었다면 while문 탈출(stack이 비었을때 while문의 조건이 돌면 에러발생)
+    while (stack[stack.length - 1].value < stackNode.value) {
+      stack.pop();
+      if (!stack.length) break;
+    }
+
+    // 새로운 노드를 넣기 전에 답안 배열에 top 노드의 인덱스값 할당, top 노드가 없다면 0을 할당
+    answer[i] = stack[stack.length - 1]?.idx || 0;
+    stack.push(stackNode);
+  }
+  return answer;
+};
+
+const answer = solution(...N, towers).join(' ');
+console.log(answer);
