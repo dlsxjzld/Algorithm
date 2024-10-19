83e93ea0186cffed7291f6b9e73b9333c84d523a
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-19 16:27:11 +0900

    [Silver III] Title: 선분 위의 점, Time: 456 ms, Memory: 66976 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.js
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.js
@@ -4,57 +4,63 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const [n, m] = input[0].split(" ").map(Number)
+// 일차원 좌표상의 점 N개와 선분 M개가 주어진다.
+// 이때, 각각의 선분 위에 입력으로 주어진 점이 몇 개 있는지 구
+
+// 1 ≤ N, M ≤ 100,000
+// 모든 좌표는 1,000,000,000보다 작거나 같은 자연수
+const [N, M] = input[0].split(" ").map(Number)
 
 const dots = input[1].split(" ").map(Number)
+dots.sort((a, b) => a - b)
+
 const lines = input.slice(2).map((row) => row.split(" ").map(Number))
-dots.sort((a,b)=>a-b)
-// 0 1  2  3  4
-// 1 3 10 20 30
-
-// 1 10 -> 0 2
-// 끝은 mid가 target보다 작거나 같은 값 중에 제일 큰 값
-// 시작은 mid가 target보다 크거나 같은 값 중에 제일 작은 값
-
-// 2 15 -> 3포함 10포함  1 2
-// 4 8 -> 2 1
-
-// 0 2
-// 3 4
-// 1 4
-// 1 2
-// 2 1
-
-const findStartIndex = (start, end, target) => {
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2)
-    if (dots[mid] >= target) {
-      end = mid-1
-    } else {
-      start = mid + 1
-    }
-  }
-  return end+1
-}
-
-const findEndIndex = (start, end, target) => {
-  while (start <= end) {
-    let mid = Math.floor((start + end) / 2)
-    if (dots[mid] > target) {
-      end = mid-1
-    } else {
-      start = mid +1
-    }
-  }
-  return start-1
-}
 const answer = []
-for (let [s, e] of lines) {
 
-  const start = findStartIndex(0, n-1, s)
-  const end = findEndIndex(0, n-1, e)
-
-  answer.push(end -start+1>0 ? end - start + 1 : 0)
+const findStartIndex = (dots, lineValue) => {
+  let start = 0
+  let end = dots.length - 1
+  let answer = null
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
+    if (dots[mid] > lineValue) {
+      // 우리가 찾는 값: lineValue 보다 딱 하나 더 작은 dots의 인덱스
+      answer = mid - 1
+      end = mid - 1
+    } else if (dots[mid] < lineValue) {
+      start = mid + 1
+    } else {
+      // 같으면
+      answer = mid - 1
+      break
+    }
+  }
+  return answer
 }
+const findEndIndex = (dots, lineValue) => {
+  let start = 0
+  let end = dots.length - 1
+  let answer = null
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
+    if (dots[mid] > lineValue) {
+      end = mid - 1
+    } else if (dots[mid] < lineValue) {
+      // 우리가 찾는 값: lineValue 보다 딱 하나 더 큰 dots의 인덱스
+      answer = mid + 1
+      start = mid + 1
+    } else {
+      // 같으면
+      answer = mid + 1
+      break
+    }
+  }
+  return answer
+}
+for (let [start, end] of lines) {
+  const sIdx = findStartIndex(dots, start)
+  const eIdx = findEndIndex(dots, end)
 
+  answer.push((sIdx == null || eIdx == null) ? 0 :  eIdx - sIdx - 1)
+}
 console.log(answer.join('\n'))
