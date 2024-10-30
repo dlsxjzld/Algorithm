2fc7915ba568131bf8e5a3c24f308ce2534a00b2
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-30 14:34:26 +0900

    [Gold V] Title: 동전 2, Time: 160 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.js
+++ 백준/Gold/2294. 동전 2/동전 2.js
@@ -11,11 +11,8 @@ const dp = Array.from({ length: k + 1 }, () => Number.MAX_SAFE_INTEGER)
 dp[0] = 0
 
 for (let i = 0; i < n; i++) {
-  dp[coins[i]] = 1
-
   for (let j = coins[i]; j <= k; j++) {
-     dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1)
+    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1)
   }
-
 }
 console.log(dp[k] === Number.MAX_SAFE_INTEGER ? -1 : dp[k])
