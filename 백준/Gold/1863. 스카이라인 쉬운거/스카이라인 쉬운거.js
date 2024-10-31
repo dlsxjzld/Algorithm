dc5e6799a8155ee31a8bf959dcb39fc0c5fa3fc7
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-10-31 17:09:26 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 168 ms, Memory: 12464 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.js
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.js
@@ -6,43 +6,27 @@ const input = require("fs")
 
 const n = Number(input[0])
 
-const positions = input.slice(1).map((val) => val.split(" ").map(Number))
+const positions = [
+  0,
+  ...input.slice(1).map((val) => Number(val.split(" ")[1])),
+  0,
+]
 
 let answer = 0
 
-let y = []
+let y = [0]
 
-for (let [nx, ny] of positions) {
-  if (y.length == 0 && ny != 0) {
-    y.push(ny)
-  } else {
-    if (ny == 0) {
-      while (y.length > 0) {
-        y.pop()
-        answer += 1
-      }
-    } else {
-      if (y.at(-1) > ny) {
-        while (true) {
-          const cur = y.at(-1)
-          if (cur > ny) {
-            y.pop()
-            answer += 1
-          } else if (cur < ny || y.length == 0) {
-            y.push(ny)
-          } else {
-            break
-          }
-        }
-      } else if (y.at(-1) < ny) {
-        y.push(ny)
-      }
-    }
+for (let i=1;i<positions.length;i++) {
+  const cur = positions[i]
+  
+  while(y.at(-1) > cur){
+    y.pop()
+    answer +=1
+  }
+
+  if(y.at(-1) < cur){
+    y.push(cur)
   }
 }
 
-while (y.length > 0) {
-  y.pop()
-  answer += 1
-}
 console.log(answer)
