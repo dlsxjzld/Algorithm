8295499d6bb69c90f5832c8e2724680ca78dc850
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-11-01 20:13:55 +0900

    [Gold V] Title: 문자열 게임 2, Time: 232 ms, Memory: 17328 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.js
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.js
@@ -10,28 +10,37 @@ const answer = []
 for (let tc = 0; tc < T; tc++) {
   const W = input[tc * 2 + 1]
   const K = Number(input[tc * 2 + 2])
-  const alpha = {}
-  let min = Infinity
-  let max = -Infinity
+  let MAX = Number.MIN_SAFE_INTEGER
+  let MIN = Number.MAX_SAFE_INTEGER
 
-  for(let i =0;i<W.length;i++){
+  // k개 포함된 문자열
+  // 1. K개 포함된 알파벳만 구하기
+  // 2. 그 중에서 가장 긴 문자열 길이 구하기
+  // 시작과 끝이 같은 알파벳이여야함
+
+  const alphabet = new Map()
+
+  for (let i = 0; i < W.length; i++) {
     const char = W[i]
-    if(!alpha[char]){
-      alpha[char] = []
+    if (!alphabet.has(char)) {
+      alphabet.set(char, [])
     }
-    alpha[char].push(i)
+
+    alphabet.get(char).push(i)
   }
 
-  const greaterThanK = Object.entries(alpha).filter(([key,value])=>value.length>=K)
+  const candidate = Array.from(alphabet).filter(([_, values]) => values.length >= K)
 
-  for(const [key,value] of greaterThanK){
-    
-    for(let i=0;i<=value.length-K;i++){
-      min = Math.min(min, value[i+K-1] - value[i]+1)
-      max = Math.max(max, value[i+K-1] - value[i]+1)
+  for (let [char, values] of candidate) {
+    for (let i = 0; i <= values.length - K; i += 1) {
+      MAX = Math.max(MAX, values[i + K - 1] - values[i] + 1)
+      MIN = Math.min(MIN, values[i + K - 1] - values[i] + 1)
     }
   }
-  if(min == Infinity) answer.push('-1')
-    else answer.push(`${min} ${max}`)
+  if (MIN == Number.MAX_SAFE_INTEGER) {
+    answer.push("-1")
+  } else {
+    answer.push(`${MIN} ${MAX}`)
+  }
 }
-console.log(answer.join('\n'))
+console.log(answer.join("\n"))
