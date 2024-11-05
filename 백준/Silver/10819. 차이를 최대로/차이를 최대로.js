b31e298ac2bc161d312b8cabcb957c8365efba8e
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-11-05 11:12:43 +0900

    [Silver II] Title: 차이를 최대로, Time: 228 ms, Memory: 12292 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
@@ -5,12 +5,18 @@ const input = require("fs")
   .split("\n")
 const n = Number(input[0])
 const arr = input[1].split(" ").map(Number)
-const answer = []
+
 const visited = Array.from({ length: n }, () => false)
+let MAX = Number.MIN_SAFE_INTEGER
 
 const dfs = (array, visited, targetLength) => {
   if (array.length === targetLength) {
-    answer.push(array)
+
+    let sum = 0
+    for(let i =0;i<array.length-1;i++){
+      sum += Math.abs(array[i] - array[i+1])
+    }
+    MAX = Math.max(MAX,sum)
     return
   }
 
@@ -22,18 +28,7 @@ const dfs = (array, visited, targetLength) => {
     }
   }
 }
-const calculate = (answer)=>{
-  let MAX = Number.MIN_SAFE_INTEGER
-  for(let array of answer){
 
-    let sum = 0
-    for(let i =0;i<array.length-1;i++){
-      sum += Math.abs(array[i] - array[i+1])
-    }
-    MAX = Math.max(MAX,sum)
-  }
-  return MAX
-}
 
 for (let i = 0; i < n; i++) {
   if (!visited[i]) {
@@ -42,4 +37,4 @@ for (let i = 0; i < n; i++) {
     visited[i] = false
   }
 }
-console.log(calculate(answer))
+console.log(MAX)
