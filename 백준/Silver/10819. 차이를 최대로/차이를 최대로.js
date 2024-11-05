fa015c3da1dc75b49b15d0fbdf4df2f2a1b1e279
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-11-05 11:05:26 +0900

    [Silver II] Title: 차이를 최대로, Time: 272 ms, Memory: 37616 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
@@ -3,33 +3,43 @@ const input = require("fs")
   .toString()
   .trim()
   .split("\n")
+const n = Number(input[0])
+const arr = input[1].split(" ").map(Number)
+const answer = []
+const visited = Array.from({ length: n }, () => false)
 
-const N = Number(input[0])
-const A = input[1].split(" ").map(Number)
-
-const getPermutations = (array, targetNumber) => {
-  if (targetNumber === 1) {
-    return array.map((val) => [val])
+const dfs = (array, visited, targetLength) => {
+  if (array.length === targetLength) {
+    answer.push(array)
+    return
   }
-  const result = []
-  array.forEach((fixed, index, origin) => {
-    const rest = [...origin.slice(0, index), ...origin.slice(index + 1)]
 
-    const permutation = getPermutations(rest, targetNumber - 1)
-    const target = permutation.map((val) => [fixed, ...val])
-    result.push(...target)
-  })
-  return result
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      visited[i] = true
+      dfs([...array, arr[i]], visited, targetLength)
+      visited[i] = false
+    }
+  }
+}
+const calculate = (answer)=>{
+  let MAX = Number.MIN_SAFE_INTEGER
+  for(let array of answer){
+
+    let sum = 0
+    for(let i =0;i<array.length-1;i++){
+      sum += Math.abs(array[i] - array[i+1])
+    }
+    MAX = Math.max(MAX,sum)
+  }
+  return MAX
 }
 
-const permutations = getPermutations(A, N)
-let MAX = Number.MIN_SAFE_INTEGER
-
-for (let array of permutations) {
-  let sum = 0
-  for (let i = 0; i < N - 1; i++) {
-    sum += Math.abs(array[i] - array[i + 1])
+for (let i = 0; i < n; i++) {
+  if (!visited[i]) {
+    visited[i] = true
+    dfs([arr[i]], visited, n)
+    visited[i] = false
   }
-  MAX = Math.max(MAX, sum)
 }
-console.log(MAX)
+console.log(calculate(answer))
