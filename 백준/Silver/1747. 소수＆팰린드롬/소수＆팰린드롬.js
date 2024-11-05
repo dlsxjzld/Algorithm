104e624b3ffa7bacabbe2ed572d5c968f0d1d0b0
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-11-05 13:52:37 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 440 ms, Memory: 12236 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.js
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.js
@@ -4,63 +4,39 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const MAX = 1003002
 const n = Number(input[0])
-const check = Array.from({ length: MAX }, () => false)
 
-// n보다 크거나 같고, 소수이면서 팰린드롬인 수 중에서, 가장 작은 수
-
-function isPrime(number) {
-  const maxIdx = Math.floor(Math.sqrt(number)) + 1
-
-  for (let i = 2; i < maxIdx; i++) {
-    if (number % i === 0) {
+const checkPrime = (num) => {
+  if(num == 1) return false
+  const sqrt = Math.floor(num ** 0.5)
+  for (i = 2; i <= sqrt; i += 1) {
+    if (num % i == 0) {
       return false
     }
   }
-
   return true
 }
+let start = n
+const checkPaline = (num) => {
+  const conv = num.toString()
+  const mid = Math.floor(conv.length / 2)
 
-function isPalindrom(number) {
-  const convertedNumber = number.toString()
-  const len = convertedNumber.length
-  if (len <= 1) {
-    return true
+  if (conv.length <= 2) {
+    return conv[0] == conv.at(-1)
   }
-  if(len % 2 === 0){
-      
-  const left = convertedNumber.slice(0, len / 2)
-  const right = convertedNumber.slice(len / 2, len + 1)
-  const convertedRight = Array.from(right).reverse().join('')
-  if (left === convertedRight) {
-      
-    return true
-  } else {
-    return false
+  for (let i = 0; i < mid; i++) {
+    if (conv[i] !== conv[conv.length - 1 - i]) {
+      return false
+    }
   }
-  }
-  if(len % 2 === 1){
-      const left = convertedNumber.slice(0, len / 2+1)
-  const right = convertedNumber.slice(len / 2, len + 1)
-  const convertedRight = Array.from(right).reverse().join('')
-  if (left === convertedRight) {
-      
-    return true
-  } else {
-    return false
-  }
-  }
-      
+  return true
 }
-
-for (let start = 2; start <= MAX; start++) {
-  check[start] = isPrime(start)
-}
-
-for (let start = n; start <= MAX; start++) {
-  if (check[start] && isPalindrom(start)) {
-    console.log(start)
-    break
+while (true) {
+  if (checkPrime(start)) {
+    if (checkPaline(start)) {
+      console.log(start)
+      break
+    }
   }
+  start += 1
 }
