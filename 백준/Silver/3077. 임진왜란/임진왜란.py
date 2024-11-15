1fb43adeabcda1b089fe486378abf42dbfd1de67
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-11-15 18:48:16 +0900

    [Silver III] Title: 임진왜란, Time: 384 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3077. 임진왜란/임진왜란.py
+++ 백준/Silver/3077. 임진왜란/임진왜란.py
@@ -1,16 +1,20 @@
 import sys
+
 input = sys.stdin.readline
 
-N = int(input().strip())
-my = input().strip().split()
-real = input().strip().split()
+def solution(N, answer, stu):
+    total = N * (N - 1) // 2
+    book = {}
+    for idx in range(N):
+        book[stu[idx]] = idx
+    ans = 0
+    for i in range(N):
+        for j in range(i + 1, N):
+            if book[answer[i]] < book[answer[j]]:
+                ans += 1
+    return f'{ans}/{total}'
 
-check = {value: index for index, value in enumerate(real)}
-
-cnt = 0
-for i in range(N):
-    for j in range(i + 1, N):
-        if check[my[i]] < check[my[j]]:
-            cnt += 1
-
-print(f"{cnt}/{(N * (N - 1)) // 2}")
+N = int(input())
+answer = input().split()
+stu = input().split()
+print(solution(N, answer, stu))
