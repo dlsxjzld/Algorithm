c29a856cd1a6619f2856e96283c8d10c6453955a
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-11-28 12:12:35 +0900

    [Silver IV] Title: Maximum Subarray, Time: 160 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.js
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.js
@@ -5,6 +5,7 @@ const input = require("fs")
   .split("\n")
 
 const t = Number(input[0])
+const answer = []
 for (let tc = 0; tc < t; tc += 1) {
   const n = Number(input[tc * 2 + 1])
   const numbers = input[tc * 2 + 2].split(" ").map(Number)
@@ -15,5 +16,6 @@ for (let tc = 0; tc < t; tc += 1) {
       dp[i] += dp[i - 1]
     }
   }
-  console.log(Math.max(...dp))
+  answer.push(Math.max(...dp))
 }
+console.log(answer.join('\n'))
