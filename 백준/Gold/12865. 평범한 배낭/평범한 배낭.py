3ed5127404cb373d54549d1712202c444f9e9025
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-11-29 14:06:26 +0900

    [Gold V] Title: 평범한 배낭, Time: 3028 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n,k = map(int,input().split(' '))
+dp = [0] * (k+1)
+
+for i in range(n):
+    w,v = map(int,input().split(' '))
+    for j in range(k,w-1,-1):
+        dp[j] = max(dp[j],dp[j-w]+v)
+
+print(max(dp))
