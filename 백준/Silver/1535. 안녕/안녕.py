b64b21d0148305ecbc10fdb5af034d35eb63e7ca
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-11-30 17:45:26 +0900

    [Silver II] Title: 안녕, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -4,18 +4,17 @@ input = sys.stdin.readline
 n = int(input())
 L = list(map(int,input().split()))
 J = list(map(int,input().split()))
-dp  = [0 for i in range(0,101)]
+dp  = [[0 for i in range(0,101)] for _ in range(n+1)]
 
-# 인사하거나 안하거나
 
-# dp[i] = i만큼의 체력이 있을 때 인사해서 얻은 기쁨의 양
+for i in range(1,n+1):
+  lost = L[i-1]
+  joy = J[i-1]
 
-for i in range(0,n):
+  for hp in range(1,101):
+    if(hp <=lost):
+      dp[i][hp] = dp[i-1][hp]
+    else:
+      dp[i][hp] = max(dp[i-1][hp], dp[i-1][hp-lost]+joy)
 
-  lost = L[i]
-  joy = J[i]
-  for hp in range(100,0,-1):
-    if(hp > lost):
-      dp[hp] = max(dp[hp-lost] + joy, dp[hp])
-
-print(max(dp))
+print(max(map(max,dp)))
