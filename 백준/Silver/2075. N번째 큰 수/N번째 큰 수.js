bbf4da8a4d7bf189dc048bf9e64a85c83645d6e4
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-12-03 14:37:55 +0900

    [Silver III] Title: N번째 큰 수, Time: 888 ms, Memory: 19576 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.js
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.js
@@ -1,98 +1,110 @@
-// N×N의 표에 수 N2개 채워져 있다.
-// 채워진 수에는 한 가지 특징이 있는데, 모든 수는 자신의 한 칸 위에 있는 수보다 크다는 것이다.
-
-const readLine = require("readline")
-const rl = readLine.createInterface({
+const readline = require("readline")
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 })
+
+// 최소힙으로 구현하고 계속 넣다가 N개 남을 때까지 다 빼기?
 class MinHeap {
   constructor() {
-    this.array = []
+    this.heap = []
   }
-  push(value) {
-    this.array.push(value)
 
-    if (this.array.length >= 2) {
-      let currentIndex = this.array.length - 1
-      let parentIndex = Math.floor((currentIndex - 1) / 2)
-      while (parentIndex >= 0) {
-        if (this.array[parentIndex] > this.array[currentIndex]) {
-          let tmp = this.array[parentIndex]
-          this.array[parentIndex] = this.array[currentIndex]
-          this.array[currentIndex] = tmp
-          currentIndex = parentIndex
-          parentIndex = Math.floor((currentIndex - 1) / 2)
-        } else {
-          break
-        }
+  push(value) {
+    if (this.heap.length === 0) {
+      this.heap.push(value)
+      return
+    }
+    this.heap.push(value)
+    this.bubbleUp()
+  }
+  bubbleUp() {
+    let currentIndex = this.heap.length - 1
+    let parentIndex = Math.floor((currentIndex - 1) / 2)
+    while (parentIndex >= 0) {
+      if (this.heap[parentIndex] > this.heap[currentIndex]) {
+        const tmp = this.heap[parentIndex]
+        this.heap[parentIndex] = this.heap[currentIndex]
+        this.heap[currentIndex] = tmp
+        currentIndex = parentIndex
+        parentIndex = Math.floor((currentIndex - 1) / 2)
+      } else {
+        break
       }
     }
   }
+
   pop() {
-    if (this.array.length == 0) return null
-    if (this.array.length == 1) return this.array.pop()
-    const returnValue = this.array[0]
-    const endValue = this.array.pop()
-    this.array[0] = endValue
+    if (this.heap.length === 0) return null
+    if (this.heap.length === 1) {
+      return this.heap.pop()
+    }
+    const returnValue = this.heap[0]
+    const current = this.heap.pop()
+    this.heap[0] = current
+
+    this.bubbleDown()
+
+    return returnValue
+  }
+
+  bubbleDown() {
     let currentIndex = 0
-    while (currentIndex <= this.array.length - 1) {
+    while (true) {
       let leftIndex = currentIndex * 2 + 1
       let rightIndex = currentIndex * 2 + 2
       let swap = null
-
-      if (this.array.length - 1 >= leftIndex) {
-        if (this.array[leftIndex] < this.array[currentIndex]) {
+      if (leftIndex < this.heap.length) {
+        if (this.heap[leftIndex] < this.heap[currentIndex]) {
           swap = leftIndex
         }
       }
-      if (this.array.length - 1 >= rightIndex) {
+      if (rightIndex < this.heap.length) {
         if (
-          (swap !== null && this.array[leftIndex] > this.array[rightIndex]) ||
-          (swap == null && this.array[rightIndex] < this.array[currentIndex])
+          (swap !== null && this.heap[rightIndex] < this.heap[leftIndex]) ||
+          (swap === null && this.heap[rightIndex] < this.heap[currentIndex])
         ) {
           swap = rightIndex
         }
       }
-
-      if (swap == null) {
+      if (swap === null) {
         break
       }
-      let tmp = this.array[currentIndex]
-      this.array[currentIndex] = this.array[swap]
-      this.array[swap] = tmp
+      const tmp = this.heap[currentIndex]
+      this.heap[currentIndex] = this.heap[swap]
+      this.heap[swap] = tmp
       currentIndex = swap
     }
-    return returnValue
   }
-  getSize() {
-    return this.array.length
+
+  getLength() {
+    return this.heap.length
   }
 }
-const minHeap = new MinHeap()
 
 let index = -1
-let n = 0
-rl.on("line", function (line) {
-  if (index == -1) {
-    n = Number(line)
+let n = null
+const minHeap = new MinHeap()
+rl.on("line", (input) => {
+  if (index === -1) {
+    n = Number(input)
     index = n
     return
   }
-  const nums = line.split(" ")
-
-  for (let num of nums) {
-    minHeap.push(parseInt(num))
-    if (minHeap.getSize() > n) {
+  const numbers = input.split(" ")
+  for (let num of numbers) {
+    minHeap.push(Number(num))
+    if (minHeap.getLength() > n) {
       minHeap.pop()
     }
   }
-
-  index--
-  if (index == 0) {
+  index -= 1
+  if (index === 0) {
     rl.close()
   }
-}).on("close", function () {
-  console.log(minHeap.pop())
+})
+
+rl.on("close", () => {
+  console.log(minHeap.heap[0])
   process.exit()
 })
