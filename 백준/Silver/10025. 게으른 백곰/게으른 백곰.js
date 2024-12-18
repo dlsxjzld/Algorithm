67420c87456a5e9f80e5fd3258bf6005cfa6a166
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-12-18 11:55:32 +0900

    [Silver III] Title: 게으른 백곰, Time: 360 ms, Memory: 57088 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.js
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.js
@@ -6,11 +6,6 @@ const input = require("fs")
 
 let [n, k] = input[0].split(" ").map(Number)
 
-// 총 N(1 ≤ N ≤ 100000)개의 얼음 양동이들
-// xi(0 ≤ xi ≤ 1,000,000)좌표마다 놓여 있고
-// 각 양동이 안에는 gi(1 ≤ gi ≤ 10,000)씩의 얼음이 들어 있다.
-// 일단 앨버트가 자리를 잡으면 그로부터 좌우로 K(1 ≤ K ≤ 2,000,000) 만큼 떨어진 양동이까지 닿을 수 있다.
-
 const ices = Array.from({ length: 1_000_001 }, () => 0)
 for (let i = 1; i <= n; i += 1) {
   const [g, x] = input[i].split(" ").map(Number)
@@ -19,7 +14,9 @@ for (let i = 1; i <= n; i += 1) {
 
 let answer = 0
 if (k >= 500_000) {
-  answer = ices.reduce((a, b) => a + b, 0)
+    for(let i=0;i<=1_000_000;i+=1){
+        answer += ices[i]
+    }
 } else {
   let start = 0
 
