8067c3c45a4c87a38342e3496d548e49a3c361bd
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-12-18 11:54:30 +0900

    [Silver III] Title: 게으른 백곰, Time: 392 ms, Memory: 56300 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.js
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.js
@@ -5,27 +5,35 @@ const input = require("fs")
   .split("\n")
 
 let [n, k] = input[0].split(" ").map(Number)
+
+// 총 N(1 ≤ N ≤ 100000)개의 얼음 양동이들
+// xi(0 ≤ xi ≤ 1,000,000)좌표마다 놓여 있고
+// 각 양동이 안에는 gi(1 ≤ gi ≤ 10,000)씩의 얼음이 들어 있다.
+// 일단 앨버트가 자리를 잡으면 그로부터 좌우로 K(1 ≤ K ≤ 2,000,000) 만큼 떨어진 양동이까지 닿을 수 있다.
+
 const ices = Array.from({ length: 1_000_001 }, () => 0)
-input
-  .slice(1)
-  .map((row) => row.split(" ").map(Number))
-  .forEach(([ice, index]) => (ices[index] = ice))
+for (let i = 1; i <= n; i += 1) {
+  const [g, x] = input[i].split(" ").map(Number)
+  ices[x] = g
+}
 
-const max = 1_000_000
- k = k >=5_00_001 ?5_00_000 : k
-let current = ices
-  .slice(0)
-  .filter((_,position) => position < k*2+1)
-  .reduce((a, b) => a + b)
-let answer = current
+let answer = 0
+if (k >= 500_000) {
+  answer = ices.reduce((a, b) => a + b, 0)
+} else {
+  let start = 0
 
+  for (let i = 0; i <= 2 * k; i += 1) {
+    start += ices[i]
+  }
+  answer = Math.max(answer, start)
 
+  for (let i = 1; i <= 1_000_000 - 2 * k; i += 1) {
+    start -= ices[i - 1]
+    start += ices[i + 2 * k]
+    answer = Math.max(answer, start)
+  }
 
-for (let i = 1; i <= max - (k*2+1)+1; i++) {
-  current = current - ices[i - 1] + ices[i + k*2]
-
-
-  answer = Math.max(answer, current)
 }
 
 console.log(answer)
