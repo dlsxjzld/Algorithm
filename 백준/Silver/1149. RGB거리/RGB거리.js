7c7c1a51fa11284624d3e46a80a52b0108ff1fb6
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-12-24 14:21:45 +0900

    [Silver I] Title: RGB거리, Time: 104 ms, Memory: 10068 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.js
+++ 백준/Silver/1149. RGB거리/RGB거리.js
@@ -1,23 +1,22 @@
-const input = require('fs')
-  .readFileSync('/dev/stdin')
+const input = require("fs")
+  .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split('\n');
+  .split("\n")
 
-const n = Number(input[0]);
-const street = input
-  .slice(1, 1 + n)
-  .map((colors) => colors.split(' ').map(Number));
-const dp = Array.from({ length: n }, () => Array.from({ length: 3 }, () => 0));
+const N = Number(input[0])
+const houses = input.slice(1).map((row) => row.split(" ").map(Number))
 
-for (let i = 0; i < 3; i++) {
-  dp[0][i] = street[0][i];
+const dp = Array.from({length:N},()=>Array.from({length:3},()=>Infinity))
+
+dp[0][0] = houses[0][0]
+dp[0][1] = houses[0][1]
+dp[0][2] = houses[0][2]
+
+for(let i=1;i<N;i+=1){
+  dp[i][0] = Math.min(dp[i][0],houses[i][0] + Math.min(dp[i-1][1],dp[i-1][2]))
+  dp[i][1] = Math.min(dp[i][1],houses[i][1] + Math.min(dp[i-1][0],dp[i-1][2]))
+  dp[i][2] = Math.min(dp[i][2],houses[i][2] + Math.min(dp[i-1][0],dp[i-1][1]))
 }
 
-for (let i = 1; i < n; i++) {
-  for (let j = 0; j < 3; j++) {
-    dp[i][j] = street[i][j] + Math.min(dp[i-1][(j + 1) % 3], dp[i-1][(j + 2) % 3]);
-  }
-}
-
-console.log(Math.min(...dp[n-1]))
+console.log(Math.min(...dp[N-1]))
