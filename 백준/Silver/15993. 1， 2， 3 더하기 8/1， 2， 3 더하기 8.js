897c13d4fd40076d75ac1673264b5d22c5b2b959
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2024-12-30 16:08:35 +0900

    [Silver I] Title: 1, 2, 3 더하기 8, Time: 228 ms, Memory: 26176 KB -BaekjoonHub

--- 백준/Silver/15993. 1， 2， 3 더하기 8/1， 2， 3 더하기 8.js
+++ 백준/Silver/15993. 1， 2， 3 더하기 8/1， 2， 3 더하기 8.js
@@ -7,17 +7,19 @@ const input = require("fs")
 const T = Number(input[0])
 
 const dp = Array.from({ length: 100_001 }, () => [0, 0])
-dp[1][1] = 1
-dp[2][1] = 1
+dp[1][0] = 1
 dp[2][0] = 1
-dp[3][0] = 2
+dp[2][1] = 1
 dp[3][1] = 2
+dp[3][0] = 2
 for (let i = 4; i <= 100_000; i += 1) {
-  dp[i][0] = (dp[i - 1][1] + dp[i - 2][1] + dp[i - 3][1]) % 1_000_000_009
   dp[i][1] = (dp[i - 1][0] + dp[i - 2][0] + dp[i - 3][0]) % 1_000_000_009
+  dp[i][0] = (dp[i - 1][1] + dp[i - 2][1] + dp[i - 3][1]) % 1_000_000_009
 }
-
+const answer = []
 for(let i=1;i<=T;i+=1){
   const n = Number(input[i])
-  console.log(dp[n][1], dp[n][0])
+  
+  answer.push(dp[n].join(' '))
 }
+console.log(answer.join('\n'))
