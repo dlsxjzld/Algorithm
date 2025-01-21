60a4ef5950e0674c953d79cca33e7c75e742f501
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-01-21 13:46:19 +0900

    [Silver III] Title: 원상 복구 (small), Time: 928 ms, Memory: 119592 KB -BaekjoonHub

---
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.js
@@ -0,0 +1,38 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const [n, k] = input[0].split(" ").map(Number)
+const S = input[1].split(" ").map(Number)
+const D = input[2].split(" ").map(Number)
+
+// P 카드를 D방식으로 K번 섞은 게 S
+
+// Di번째 카드를 i번째로 가져오기
+
+// D: [4 3 1 2 5]
+// 현재 S의 1번째는 4번째로 보내기
+// 현재 S의 2번째는 3번째로 보내기
+// 현재 S의 3번째는 1번째로 보내기
+// 현재 S의 4번째는 2번째로 보내기
+// 현재 S의 5번째는 5번째로 보내기
+
+const getP = (S, D, count) => {
+  if (count === k) {
+    return S
+  }
+  const newArr = Array.from({ length: n })
+
+  for (let i = 0; i < n; i += 1) {
+    const cur = S[i]
+    const where = D[i]-1
+    newArr[where] = cur
+  }
+
+  return getP(newArr, D, count + 1)
+}
+
+const answer = getP(S, D, 0)
+console.log(answer.join(" "))
