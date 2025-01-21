91c48839bdc75c81a449d2e0d7b7e08fa96f5df6
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-01-21 13:49:51 +0900

    [Silver III] Title: 원상 복구 (small), Time: 252 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.js
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.js
@@ -5,7 +5,7 @@ const input = require("fs")
   .split("\n")
 
 const [n, k] = input[0].split(" ").map(Number)
-const S = input[1].split(" ").map(Number)
+let S = input[1].split(" ").map(Number)
 const D = input[2].split(" ").map(Number)
 
 // P 카드를 D방식으로 K번 섞은 게 S
@@ -19,20 +19,14 @@ const D = input[2].split(" ").map(Number)
 // 현재 S의 4번째는 2번째로 보내기
 // 현재 S의 5번째는 5번째로 보내기
 
-const getP = (S, D, count) => {
-  if (count === k) {
-    return S
-  }
-  const newArr = Array.from({ length: n })
-
-  for (let i = 0; i < n; i += 1) {
-    const cur = S[i]
-    const where = D[i]-1
+let newArr = Array.from({ length: n })
+for (let i = 0; i < k; i += 1) {
+  for (let j = 0; j < n; j += 1) {
+    const cur = S[j]
+    const where = D[j] - 1
     newArr[where] = cur
   }
-
-  return getP(newArr, D, count + 1)
+  S = [...newArr]
 }
 
-const answer = getP(S, D, 0)
-console.log(answer.join(" "))
+console.log(S.join(" "))
