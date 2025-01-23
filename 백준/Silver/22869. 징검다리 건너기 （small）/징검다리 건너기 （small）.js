aa2b97390011dadac0f2cce2a7a404b4380be1d1
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-01-23 13:53:59 +0900

    [Silver I] Title: 징검다리 건너기 (small), Time: 192 ms, Memory: 9932 KB -BaekjoonHub

--- 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.js
+++ 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.js
@@ -10,15 +10,8 @@ const A = input[1].split(" ").map(Number)
 const dp = Array.from({ length: n }, () => false)
 dp[0] = true
 
-for (let j = 1; j < n; j += 1) {
-  const cost = j * (1 + Math.abs(A[0] - A[j]))
-  if (cost > k) {
-    continue
-  }
-  dp[j] = true
-}
 
-for (let i = 1; i < n - 1; i += 1) {
+for (let i = 0; i < n - 1; i += 1) {
   if (!dp[i]) {
     continue
   }
