53310c0342d31ac44fbaddd78221d7e669c7df81
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-02-03 20:01:14 +0900

    [Gold IV] Title: 호텔, Time: 100 ms, Memory: 10504 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.js
+++ 백준/Gold/1106. 호텔/호텔.js
@@ -5,18 +5,22 @@ const input = require("fs")
   .split("\n")
 
 const [c, n] = input[0].split(" ").map(Number)
-const dp = Array.from({ length: c + 1 }, () => Infinity)
-dp[0] = 0
-for (let tc = 1; tc < 1 + n; tc++) {
-  const [cost, customer] = input[tc].split(" ").map(Number)
+const lines = input.slice(1).map((row) => row.split(" ").map(Number))
 
-  for (let i = 1; i <= c; i++) {
-   if(i<=customer){
-       dp[i] = Math.min(dp[i],cost)
-   }else{
-       dp[i] = Math.min(dp[i],dp[i - customer] + dp[customer])
-   }
+const dp = Array.from({ length: c + 1 }, () => Infinity)
+
+for (let line of lines) {
+  const [cost, customer] = line
+
+  dp[customer] = Math.min(dp[customer], cost)
+
+  for (let i = 1; i <= c; i += 1) {
+    if (i <= customer) {
+      dp[i] = Math.min(dp[i], cost)
+    } else {
+      dp[i] = Math.min(dp[i - customer] + cost, dp[i])
+    }
   }
-  
 }
+
 console.log(dp[c])
