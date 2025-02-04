e999728f7af50b7986c1d5898e99b649070ed602
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-02-04 17:28:19 +0900

    [Gold V] Title: LCS, Time: 328 ms, Memory: 66036 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.js
+++ 백준/Gold/9251. LCS/LCS.js
@@ -10,15 +10,16 @@ const dp = Array.from({ length: row.length + 1 }, () =>
   Array.from({ length: col.length + 1 }, () => 0),
 )
 
-
 for(let i=1;i<=row.length;i+=1){
   for(let j=1;j<=col.length;j+=1){
     if(row[i-1] === col[j-1]){
-      dp[i][j] = dp[i-1][j-1] +1
+      dp[i][j] = dp[i-1][j-1]+1
     }else{
-      dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+      dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1])
     }
   }
 }
 
-console.log(Math.max(...dp.flat()))
+const answer = []
+dp.forEach((row)=> answer.push(...row))
+console.log(Math.max(...answer))
