bd681e1a37b5a0f2971b2c2c07ff9519f298b99b
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-03-06 20:28:31 +0900

    [Gold V] Title: 수 고르기, Time: 404 ms, Memory: 23068 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.js
@@ -0,0 +1,32 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+
+const [n, m] = input[0].split(" ").map(Number)
+const numbers = input.slice(1).sort((a, b) => a - b)
+let start = 0
+let end = 1
+let answer = numbers[n - 1] - numbers[0]
+if (m === 0) {
+  console.log(0)
+  return
+}
+
+while (start <= n - 1) {
+  const diff = Math.abs(numbers[end] - numbers[start])
+  if (diff >= m) {
+    if (answer > diff) {
+      answer = diff
+    }
+    start += 1
+  } else {
+    if (n - 1 > end) {
+      end += 1
+    } else {
+      start += 1
+    }
+  }
+}
+console.log(answer)
