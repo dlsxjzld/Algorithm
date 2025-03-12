452a4c06c6f2c6648017d2678fdfc0032c34fa23
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-03-12 14:45:34 +0900

    [Gold IV] Title: 좋다, Time: 292 ms, Memory: 14808 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.js
+++ 백준/Gold/1253. 좋다/좋다.js
@@ -5,34 +5,39 @@ const input = require("fs")
   .split("\n")
 
 const N = Number(input[0])
-const A = input[1].split(" ").map(Number)
+const A = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b)
 
-A.sort((a, b) => a - b)
-
-const binarySearch = (target, rest) => {
+const search = (target, rest) => {
   let start = 0
-  let end = rest.length-1
+  let end = rest.length - 1
 
+  let mid = -1
   while (start < end) {
-    let mid = rest[start] + rest[end]
+    const left = rest[start]
+    const right = rest[end]
 
-    if (target === mid) {
+    mid = left + right
+
+    if (mid === target) {
       return true
-    } else if (target < mid) {
-      end = end - 1
-    } else if (target > mid) {
-      start = start + 1
+    } else if (mid > target) {
+      end -= 1
+    } else {
+      // mid < target
+      start += 1
     }
   }
-  return false
 }
-let num = 0
-for (let i = 0; i < N; i++) {
-  const target = A[i]
-  const rest = [...A.slice(0, i), ...A.slice(i + 1)]
 
-  if(binarySearch(target,rest)){
-    num += 1
+let answer =0
+for(let i=0;i<N;i+=1){
+  const target = A[i]
+  const rest = [...A.slice(0,i), ...A.slice(i+1)]
+  if(search(target,rest)){
+    answer +=1
   }
 }
-console.log(num)
+console.log(answer)
