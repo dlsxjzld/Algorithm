3a046ba550fd87f3b34b5ce15068cdeae472a3c5
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-03-20 19:02:10 +0900

    [Silver IV] Title: 주몽, Time: 160 ms, Memory: 10980 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.js
+++ 백준/Silver/1940. 주몽/주몽.js
@@ -4,30 +4,28 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const n = Number(input[0])
-const m = Number(input[1])
+const N = Number(input[0])
+const M = Number(input[1])
 const numbers = input[2]
   .split(" ")
   .map(Number)
   .sort((a, b) => a - b)
 
-const solution = (arr, n, m) => {
-  let start = 0
-  let end = n - 1
-  let cnt = 0
+let left = 0
+let right = N - 1
+let answer = 0
 
-  while (start < end) {
-    let ssum = arr[start] + arr[end]
-    if (ssum < m) {
-      start += 1
-    } else {
-      end -= 1
-      if (ssum === m) {
-        cnt += 1
-      }
-    }
+let cur = numbers[left] + numbers[right]
+while (left < right) {
+  if (cur === M) {
+    answer += 1
   }
-  return cnt
-}
 
-console.log(solution(numbers, n, m))
+  if (cur < M) {
+    left += 1
+  } else {
+    right -= 1
+  }
+    cur = numbers[left] + numbers[right]
+}
+console.log(answer)
