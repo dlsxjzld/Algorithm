bccde9942095f65cf3418d5a903b07db8f327f94
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-05-31 17:58:12 +0900

    [Silver I] Title: 징검다리 건너기 (small), Time: 188 ms, Memory: 9900 KB -BaekjoonHub

--- 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.js
+++ 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.js
@@ -5,23 +5,30 @@ const input = require("fs")
   .split("\n")
 
 const [n, k] = input[0].split(" ").map(Number)
-const A = input[1].split(" ").map(Number)
+const arr = input[1].split(" ").map(Number)
 
 const dp = Array.from({ length: n }, () => false)
+
+// 가장 왼쪽 돌에서 출발
 dp[0] = true
 
+// dp[i] i번째 돌이 되는지
 
-for (let i = 0; i < n - 1; i += 1) {
+for (let i = 0; i < n; i += 1) {
   if (!dp[i]) {
     continue
   }
+
   for (let j = i + 1; j < n; j += 1) {
-    const cost = (j - i) * (1 + Math.abs(A[i] - A[j]))
-    if (cost > k) {
+    const str = (j - i) * (1 + Math.abs(arr[i] - arr[j]))
+    if (str > k) {
       continue
+    } else {
+      dp[j] = true
     }
-    dp[j] = true
   }
 }
 
-console.log(dp[n - 1] ? "YES" : "NO")
+const answer = dp[n - 1]
+
+console.log(answer ? "YES" : "NO")
