1bfa2237a178def083fa7392abf3848b7267a7c3
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-05 17:55:10 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 35.97 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 파일명 정렬 - 17686 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686?language=javascript#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17686#) 
 
 ### 성능 요약
 
-메모리: 37 MB, 시간: 11.15 ms
+메모리: 38.2 MB, 시간: 35.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 14:38:40
+2025년 06월 05일 17:55:07
 
 ### 문제 설명
 
--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,66 +1,84 @@
+function checkNotNumberType (char){
+    if(char.trim() === ''){
+        return true
+    }
+    const convertedChar = Number(char)
+    return isNaN(convertedChar)
+}
+
+
 function solution(files) {
-    var answer = [];
-    const sorted_files = []
     
-    for(const file of files){
-        const sp_file = file.split('')
-        let idx = 0
-        let head = ''
+    const splitedFiles = []
+    // HEAD, NUMBER, TAIL 로 나누기
+    for(let file of files){
+        let head =''
         let number = ''
-        let tail = ''
-        
-        for(const [i,str] of sp_file.entries()){
-            let num = Number(str)
-            if(str === ' ' || isNaN(num)){ // 변환한 값이 문자면
-                head += str
-            }else{ // 변환한 값이 숫자면 -> head는 끝
-                idx = i
+        let tail =''
+        let currentIndex = -1
+        const fileName = file.split('')
+
+        // head
+        for(let [index,letter] of fileName.entries()){
+            const isNotNumber = checkNotNumberType(letter)
+            if(isNotNumber) {
+                head += letter
+            }else{
+                currentIndex = index
                 break
             }
         }
-
-        let cnt = 0 // 숫자의 개수
-        
-        for(let i=idx;i<sp_file.length;i++){
-            let num = Number(sp_file[i])
-            if(!isNaN(num)){ // 숫자면 추가
-                cnt +=1
-                if(cnt>5){
-                    tail = sp_file.join('').slice(i,file.length)
+        // number
+        for(let [index,letter] of fileName.entries()){
+            if(index < currentIndex){
+                continue
+            }
+            const isNotNumber = checkNotNumberType(letter)
+            if(isNotNumber) {
+                currentIndex = index
+                break
+            }else{
+                if(number.length === 5){
+                    currentIndex = index
                     break
                 }
-                number += sp_file[i]
-            }else{
-                tail = sp_file.join('').slice(i,file.length)
-                break
+                number += letter
+                currentIndex = index
             }
         }
-        sorted_files.push([head,number,tail])
+        // tail
+        if(currentIndex !== fileName.length-1){
+            tail = fileName.slice(currentIndex).join('')    
+        }
+        splitedFiles.push([head,number,tail])
     }
 
-    answer = sorted_files.sort((a,b)=>{
-        let aHead = a[0].toLowerCase()
-        let bHead = b[0].toLowerCase()
-        let aNumber = Number(a[1])
-        let bNumber = Number(b[1])
-        if(aHead < bHead){
+    // 숫자를 반영한 정렬 기능
+    const answer = splitedFiles.sort((a,b)=>{
+        const [aHead,aNumber,aTail] = a
+        const [bHead,bNumber,bTail] = b
+        // HEAD 부분을 기준으로 사전 순으로 정렬
+        // 문자열 비교 시 대소문자 구분을 하지 않는다
+
+        if(aHead.toLowerCase() < bHead.toLowerCase()){
             return -1
-        }
-        if(aHead>bHead){
+        }else if(aHead.toLowerCase() > bHead.toLowerCase() ){
             return 1
-        }else{
-            if(aNumber<bNumber){
+        }
+        else {
+            // HEAD 부분이 대소문자 차이 외에는 같을 경우, NUMBER의 숫자 순으로 정렬
+            if(Number(aNumber) < Number(bNumber)){
                 return -1
-            }else if(aNumber>bNumber){
+            }else if(Number(aNumber) > Number(bNumber)){
                 return 1
             }else{
+                // HEAD 부분과, NUMBER의 숫자도 같을 경우, 원래 입력에 주어진 순서를 유지
                 return 0
             }
         }
-        return 0
-        
-    }).map(arr=>arr.join(''))
+    })
+    return answer.map((val)=>val.join(''))
 
     
-    return answer;
+
 }
