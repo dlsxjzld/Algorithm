ba97d3fb95dc497c3e2cbca0db3de2195b08ee39
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-06-19 11:41:10 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 48.19 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,17 +1,5 @@
 function solution(arr)
 {
-    var answer = [];
-    for(let i=0;i<=arr.length-1;i+=1){
-        const cur = arr[i]
-        if(!answer.length){
-            answer.push(cur)
-            continue
-        }
-        const last = answer[answer.length-1]
-        if(last !== cur){
-            answer.push(cur)
-        }
-    }
+    return arr.filter((val,index) => val != arr[index+1]);
     
-    return answer;
 }
