666614fe459cfb1f69ffdd4da34ec8cf889332bf
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-07-17 19:53:45 +0900

    [level 3] Title: 등굣길, Time: 1.70 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,21 +1,44 @@
+function solution(m, n, puddles) {
+    var answer = 0;
+    const dp = Array.from({length:n},()=>Array.from({length:m},()=>0))
+    
+    puddles.forEach(([c,r])=>{
+        dp[r-1][c-1] = -1
+    })
+    
+    const MAX = 1_000_000_007
+    
+    
+    dp[n-1][m-1] = 1
+    
+    for(let i=n-1;i>=1;i-=1){
+        if(dp[i-1][m-1] !== -1){
+            dp[i-1][m-1] = dp[i][m-1]
+        }else{
+            dp[i-1][m-1] = 0
+        }
+    }
+    
+    for(let j=m-1;j>=1;j-=1){
+        if(dp[n-1][j-1] !== -1){
+            dp[n-1][j-1] = dp[n-1][j] 
+        }else{
+            dp[n-1][j-1] = 0
+        }
+    }
+    
 
-function solution(M, N, puddles) {
-    const T = Array(M).fill(null).map(() => [])
-    const chk = new Set(puddles.map(([x,y]) => `${x-1}:${y-1}`));
-    for (let i = 0; i < M ; i++) {
-        for (let j = 0 ; j < N ; j++) {
-            if (chk.has(`${i}:${j}`)) {
-                T[i][j] = 0;
-            } else if (i === 0 && j === 0) {
-                T[i][j] = 1;
-            } else if (i === 0) {
-                T[i][j] = T[i][j-1];
-            } else if (j === 0) {
-                T[i][j] = T[i-1][j];
-            } else {
-                T[i][j] = (T[i-1][j] + T[i][j-1]) % 1000000007;
+    
+    for(let i=n-2;i>=0;i-=1){
+        for(let j=m-2;j>=0;j-=1){
+            if(dp[i][j] === -1){
+                dp[i][j] = 0
+            }else{
+                dp[i][j] = (dp[i+1][j]%MAX) + (dp[i][j+1]%MAX)
             }
         }
     }
-    return T[M-1][N-1];
+
+    answer = dp[0][0] % MAX
+    return answer;
 }
