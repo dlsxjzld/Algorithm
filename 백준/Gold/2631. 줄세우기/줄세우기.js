31600fbc7a06958beca4e3e1303dbadb8f0fa6c5
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-08-03 16:19:52 +0900

    [Gold IV] Title: 줄세우기, Time: 148 ms, Memory: 9616 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.js
+++ 백준/Gold/2631. 줄세우기/줄세우기.js
@@ -4,16 +4,22 @@ const input = require("fs")
   .trim()
   .split("\n")
 
-const n = Number(input[0])
+const N = Number(input[0])
 const numbers = input.slice(1).map(Number)
-const dp = Array.from({length:n},()=>0)
+// 3 7 5 2 6 1 4
+const dp = Array.from({ length: N }, () => 0)
 
-for(let i=0;i<n;i+=1){
+// 다 옮기는 데 최소 수
+// 아이를 옮기는 방법
+// 옮겼을 때 최소 횟수
+
+for (let i = 0; i < N; i += 1) {
   dp[i] = 1
-  for(let j=0;j<i;j+=1){
-    if(numbers[i] > numbers[j] && dp[j]+1 > dp[i]){
-      dp[i] = dp[j]+1
+  for (let j = 0; j < i; j += 1) {
+    if (numbers[i] > numbers[j] && dp[j] + 1 > dp[i]) {
+      dp[i] = dp[j] + 1
     }
+    // console.log("dp", dp)
   }
 }
-console.log(n-Math.max(...dp))
+console.log(N - Math.max(...dp))
