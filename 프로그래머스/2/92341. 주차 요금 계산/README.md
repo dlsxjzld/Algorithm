d4f868d6d870033ac2f01072ea6c71d7ac18059d
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-04 12:20:39 +0900

    [level 2] Title: 주차 요금 계산, Time: 4.65 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.22 ms
+메모리: 34.8 MB, 시간: 4.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 23:34:38
+2025년 10월 04일 12:20:38
 
 ### 문제 설명
 
--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,53 +1,42 @@
 function convert(time){
-    const [hour,minute] = time.split(':').map(Number)
-    return hour*60 +minute
+    const [hour,min] = time.split(':').map(Number)
+    return hour*60 + min
 }
+
 function solution(fees, records) {
     var answer = [];
+    const [defaultTime, defaultFee, perTime,perFee] = fees
     
-    const inoutRecords = new Map() // 입출차 기록
-    const timeRecords = new Map() // 차량 별 시간 기록
+    const inAndOutRecord = new Map()
+    const timeRecord = new Map()
     
-    records.forEach((record,idx)=>{
-        const [time,car,action] = record.split(' ')
-        const convertedTime = convert(time)
-
-        if(!inoutRecords.has(car)){
-            // 차량이 없다면 in 처리
-            inoutRecords.set(car,convertedTime)
+    
+    for(let record of records){
+        const [time,car,status] = record.split(' ')
+        if(status === 'IN'){
+            inAndOutRecord.set(car,convert(time))
         }else{
-            // 차량이 있다면 out 처리하고 시간 기록
-            if(!timeRecords.has(car)){
-                // 시간 기록에 차량이 없는 경우면
-                timeRecords.set(car,convertedTime - inoutRecords.get(car))
-            }else{
-                // 시간 기록에 차량이 있는 경우면
-                timeRecords.set(car,timeRecords.get(car) + convertedTime -inoutRecords.get(car))
-            }
-            inoutRecords.delete(car)
+            const inTime = inAndOutRecord.get(car)
+            timeRecord.set(car, (timeRecord.get(car) ?? 0) + convert(time) - inTime)
+            inAndOutRecord.delete(car)
         }
-    })
-    if(inoutRecords.size > 0){
-        // 출차처리하지 않은 차가 있다면
-        // 입차만 하고 출차를 한번도 하지 않은 차가 있을 수 있으므로
-        // timeRecords.get(car) || 0 으로 설정
-        Array.from(inoutRecords.keys()).forEach(car=>{
-            timeRecords.set(car,(timeRecords.get(car) || 0) + convert('23:59')-inoutRecords.get(car))
-            inoutRecords.delete(car)
-        })
     }
-    const sortedTimeRecords = Array.from(timeRecords.keys()).sort()
-
-    sortedTimeRecords.forEach(car=>{
-        let totalFee = 0
-        const [defaultTime, defaultFee, perTime,perFee] = fees
-        let useTime = Number(timeRecords.get(car))
-        if(useTime <= defaultTime){
-            totalFee = defaultFee
-        }else{
-            totalFee = defaultFee + Math.ceil((useTime-defaultTime)/perTime)*perFee
+    
+    if(inAndOutRecord.size > 0){
+        for(let [car,inTime] of Array.from(inAndOutRecord)){
+            timeRecord.set(car, (timeRecord.get(car) ?? 0) + convert('23:59') - inTime)
         }
-        answer.push(totalFee)
+    }
+    
+    const feeRecord = Array.from(timeRecord).map(([car,time])=>{
+        if(time <= defaultTime){
+            return [car,defaultFee]
+        }else {
+            return [car,defaultFee + Math.ceil((time - defaultTime)/perTime) * perFee]
+        }
     })
+    
+    answer = feeRecord.sort((a,b)=>a[0] - b[0]).map(([car,fee])=>fee)
+    
     return answer;
 }
