fd9fa6d35d9c4728ccd899183a7d625c541c7ecf
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-22 12:27:09 +0900

    [level 2] Title: 타겟 넘버, Time: 15.05 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,20 +1,25 @@
-
 function solution(numbers, target) {
     var answer = 0;
     
-    function dfs(idx,total){
-        if(idx === numbers.length){
-            if(total === target){
+    // 숫자 선택
+    // 더하기, 빼기 선택
+    
+    // 모든 경우를 탐색 후 가능한 방법 찾기
+    
+    function dfs(depth,sum,target,numbers){
+        if(depth === numbers.length){
+            if(sum === target){
                 answer +=1
             }
             return
         }
-
-        dfs(idx+1,total+numbers[idx])
-        dfs(idx+1,total-numbers[idx])
+        
+        dfs(depth+1,sum+numbers[depth],target,numbers)
+        dfs(depth+1,sum-numbers[depth],target,numbers)
+        
     }
-    dfs(0,0)
-
+    
+    dfs(0,0,target,numbers)
     
     
     return answer;
