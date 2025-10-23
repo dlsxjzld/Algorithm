53d5ef55cda24e75dffd0dd3895c9f67c2222c03
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-23 16:00:02 +0900

    [level 3] Title: 네트워크, Time: 0.63 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,37 +1,37 @@
-function bfs(start,graph,visited){
-    const queue = [start]
-    let index = 0
-    while(queue.length>index){
-        const cur = queue[index++]
-        
-        for(let i=0;i<graph.length;i+=1 ){
-            const next =  graph[cur][i]
-            if(!visited[i] && next === 1){
-                visited[i] = true
-                queue.push(i)
-            }
-        }
-    }
-}
-
 function solution(n, computers) {
     var answer = 0;
     const graph = Array.from({length:n},()=>[])
-    const visited = Array.from({length:n},()=>false)
     
     for(let i=0;i<n;i+=1){
-        const cur = computers[i]
         for(let j=0;j<n;j+=1){
-            const next = cur[j]
-
-            if(!visited[j] && next === 1){
-                visited[j] = true
-                bfs(j,computers,visited)
-                answer +=1
+            if(i === j){
+                continue
+            }
+            const connected = computers[i][j]
+            if(connected){
+                graph[i].push(j)   
             }
         }
-
     }
+    
+    function dfs(node,visited){
+        for(let next of graph[node]){
+            if(!visited[next]){
+                visited[next] = true
+                dfs(next,visited)
+            }
+        }
+    }
+    
+    const visited = Array.from({length:n},()=>false)
+    for(let i=0;i<n;i+=1){
+        if(!visited[i]){
+            visited[i] = true
+            dfs(i,visited)
+            answer += 1
+        }
+    }
+    
     
     return answer;
 }
