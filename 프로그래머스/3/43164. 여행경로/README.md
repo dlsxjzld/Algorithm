5a4a9919b2d3f487df3bb975414c19e9699a5366
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-10-27 18:59:50 +0900

    [level 3] Title: 여행경로, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 58.3 MB, 시간: 1146.62 ms
+메모리: 33.4 MB, 시간: 0.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 27일 16:48:56
+2025년 10월 27일 18:59:49
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,59 +1,28 @@
 function solution(tickets) {
     var answer = [];
-    
-    // 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로
-    // graph 만들 때 알파벳 순서가 앞서는 것으로 만들기
-    // 방문한 곳은 다시 가도 된다 -> 중복되는 경로 생기지 않게 하려면 graph에서 갈 수 있는 곳을 하나씩 빼보자
-    
-    const graph = new Map()
-    const visited = new Map()
-    
+    const graph = {}
     for(let [a,b] of tickets){
-        const start = graph.get(a) 
-        if(!start){
-            graph.set(a,[b])
-            visited.set(a,[false])
+        if(!graph[a]){
+            graph[a] = []
+        }
+        graph[a].push(b)
+        graph[a].sort()
+    }
+    const route = []
+    
+    const stack = ["ICN"]
+    
+    while(stack.length >0){
+        const current = stack[stack.length-1]
+        
+        if(graph[current] && graph[current].length>0){
+            const next = graph[current].shift()
+            stack.push(next)
         }else{
-            const newEnd = [...start,b].sort()
-            graph.set(a,newEnd)
-            visited.set(a,[...visited.get(a),false])
+            route.push(stack.pop())
         }
     }
     
-    const checkAllTickets = ()=>{
-        return Array.from(visited).map(([key,visits])=>visits).flat().every((val)=>val ===true)
-    }
-    
-    
-    function dfs(start,arr){
-        if(checkAllTickets()){
-            answer.push([...arr])
-            return
-        }
-        
-        const nextNodes = graph.get(start) ?? []
-        if(!nextNodes.length){
-            return
-        }
-        const nextVisited = visited.get(start)
-        for(let i=0;i<nextNodes.length;i+=1){
-            const next = nextNodes[i]
-            if(!nextVisited[i]){
-                nextVisited[i] = true
-                dfs(next,[...arr,next])
-                nextVisited[i] = false
-            }
-        }
-        
-    }
-    
-    dfs('ICN',['ICN'])
-    
-    answer.sort()
-    return (answer[0])
-    
-
-       
-    
-    
+    answer = route.reverse()
+    return answer;
 }
