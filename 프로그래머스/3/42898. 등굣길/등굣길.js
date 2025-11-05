2ac76b4efff2dfa94ec4bb1cc8ec981a956b98d9
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-11-05 15:48:13 +0900

    [level 3] Title: 등굣길, Time: 2.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,44 +1,35 @@
 function solution(m, n, puddles) {
     var answer = 0;
-    const dp = Array.from({length:n},()=>Array.from({length:m},()=>0))
     
-    puddles.forEach(([c,r])=>{
-        dp[r-1][c-1] = -1
-    })
-    
-    const MAX = 1_000_000_007
-    
-    
-    dp[n-1][m-1] = 1
-    
-    for(let i=n-1;i>=1;i-=1){
-        if(dp[i-1][m-1] !== -1){
-            dp[i-1][m-1] = dp[i][m-1]
-        }else{
-            dp[i-1][m-1] = 0
-        }
+    const graph = Array.from({length:n+1},()=>Array.from({length:m+1},()=>false))
+    for(let [c,r] of puddles){
+        graph[r][c] = true
     }
     
-    for(let j=m-1;j>=1;j-=1){
-        if(dp[n-1][j-1] !== -1){
-            dp[n-1][j-1] = dp[n-1][j] 
-        }else{
-            dp[n-1][j-1] = 0
-        }
-    }
+    // 올 수 있는 방법은 2가지
+    // 오른쪽, 아래
     
-
+    // 더하기 
+    // 누적합?
+    // dp[i][j] = dp[i][j-1] + dp[i-1][j]
+    // graph[i][j] 가 puddles면 하지 않음
+    const dp = Array.from({length:n+1},()=>Array.from({length:m+1},()=>0))
     
-    for(let i=n-2;i>=0;i-=1){
-        for(let j=m-2;j>=0;j-=1){
-            if(dp[i][j] === -1){
-                dp[i][j] = 0
-            }else{
-                dp[i][j] = (dp[i+1][j]%MAX) + (dp[i][j+1]%MAX)
+    for(let i=1;i<=n;i+=1){
+        for(let j=1;j<=m;j+=1){
+            if(i===1 && j === 1){
+                dp[1][1] = 1
+                continue
             }
+            if(graph[i][j]){
+                continue
+            }
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1])%1_000_000_007
         }
     }
-
-    answer = dp[0][0] % MAX
+    // console.log(dp.map((row)=>row.join(' ')).join('\n'))
+    answer = dp[n][m]
+    
+    
     return answer;
 }
