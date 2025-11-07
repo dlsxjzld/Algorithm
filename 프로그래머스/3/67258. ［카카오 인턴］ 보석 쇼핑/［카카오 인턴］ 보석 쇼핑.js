7173cf543adc58e3dcbd092dd3dd07cab4afb42a
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-11-07 09:56:20 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 41.16 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,27 +1,52 @@
 function solution(gems) {
-    const cnt = new Set(gems).size
     var answer = [1,gems.length];
+    let left = 0
+    let right = 0
+    const AllGems = new Set(gems)
+    const AllGemsCount = AllGems.size
     
-    let start=0,end=0
-    const hit = new Map()
-    hit.set(gems[0],1)
+    const myGems = {}
+    const myGemsType = new Set()
     
-    
-    while(end<gems.length){
-        if(hit.size === cnt){
-            if(answer[1]-answer[0]>end-start){
-                answer = [start+1,end+1]
-            }
-            hit.set(gems[start],hit.get(gems[start])-1)
-            if(hit.get(gems[start]) === 0){
-                hit.delete(gems[start])
-            }
-            start ++
-        }else{
-            end++
-            hit.set(gems[end],hit.get(gems[end]) + 1 || 1)
-        }
+    for(let i of AllGems){
+        myGems[i] = 0
     }
     
+    if(AllGemsCount ===1 ){
+        return [1,1]
+    }
+    
+    // 오른쪽으로 갈 때 보석 추가하기
+    // 모든 종류 있으면 answer에 추가하기
+    // 왼쪽에 있는 보석 빼고, 만약 0개면 내 보석 종류에서 제외
+    while(left<=right && right<=gems.length){
+        if(myGemsType.size !== AllGemsCount){
+          if(right<=gems.length-1){
+              myGems[gems[right]]+=1
+            myGemsType.add(gems[right])
+          }
+          
+          right+=1    
+        }
+        
+        
+        if(myGemsType.size === AllGemsCount){
+            const cur = answer[1] - answer[0] + 1
+
+            if(right - left + 1 <= cur){
+                
+                answer = [left+1,right]
+            }
+
+            myGems[gems[left]] -=1
+
+            if(myGems[gems[left]] === 0){
+                myGemsType.delete(gems[left])
+            }
+            left+=1
+
+        }
+
+    }
     return answer;
 }
