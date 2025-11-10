b6d41073415fedf799a01d9b64fc0127bf97792f
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-11-10 20:47:14 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,49 +1,38 @@
 function solution(bandage, health, attacks) {
     var answer = 0;
     // bandage는 [시전 시간, 초당 회복량, 추가 회복량] 
-    // 몬스터에게 공격당해 기술이 취소당하거나 기술이 끝나면 그 즉시 붕대 감기를 다시 사용하며, 
-    // 연속 성공 시간이 0으로 초기화됩니다.
-    // 연속 성공이 되려다가 
-    // 몬스터의 공격이 우선이다 -> 공격이 되어서 체력이 0이하가 되면 연속 성공 이미 초기화 되고 치료 실패
-    const [t,x,y] = bandage
-    let end = attacks[attacks.length-1][0]
-    let start = 0
-    let success = 0
+    // attacks[i]는 [공격 시간, 피해량] 
+    let startTime = 0
+    let endTime = attacks[attacks.length-1][0]
     
-    const attack = {}
-    for(let [time,damage] of attacks){
-        attack[time] = damage
+    const convertedAttacks = {}
+    for(let [attackTime,damage] of attacks){
+        convertedAttacks[attackTime] = damage
     }
-
     let myHealth = health
-    
-    for(let start=1;start<=end;start+=1){
-
-        if(attack[start]){
-            // 공격이라면 연속 성공 초기화
-            // 붕대 못 감음
-            success = 0 
-            myHealth -= attack[start]
-            if(myHealth <=0){
-                myHealth = -1
-                break
+    let succeedCount = 0
+    const [bandTime,recover,plusRecover] = bandage
+    for(let i=1;i<=endTime;i+=1){
+        if(convertedAttacks[i] !== undefined){
+            myHealth -= convertedAttacks[i]
+            succeedCount = 0
+        }else{
+            succeedCount +=1
+            myHealth += recover
+            if(succeedCount === bandTime){
+                succeedCount = 0
+                myHealth += plusRecover
             }
-            continue
+            if(myHealth > health){
+                myHealth = health
+            }
+            
         }
-        // 붕대 감기
-        
-        myHealth += x
-        success +=1
-        if(success%t === 0){
-            myHealth += y
+        if(myHealth <=0){
+            answer = -1
+            break
         }
-        if(myHealth > health){
-            myHealth = health // 최대 체력 유지    
-        }
-        
     }
-    
-    answer = myHealth
-    
+    answer = myHealth <=0 ? -1 : myHealth
     return answer;
 }
