3dacf0a886720c3cb09d0f4703986eb91a113727
신종욱 <66900221+dlsxjzld@users.noreply.github.com>
2025-11-17 10:39:54 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 156 ms, Memory: 71992 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.js
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.js
@@ -1,16 +1,5 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const n = Number(input[0])
-// N은 항상 3×2k 수이다. (3, 6, 12, 24, 48, ...)
-
-// 3
-// 삼각형 1개 + 빈 역삼각형 0개
-
-// 6
-// 삼각형 3 *2+ 빈 역삼각형 1개
-
-// 12
-// 삼각형 6 * 1개
-// 삼각형 6 * 2개 + 빈 역삼각형 1개
 
 const makeTriangle = (n) => {
   if (n === 3) {
@@ -20,8 +9,7 @@ const makeTriangle = (n) => {
     const empty = ' '.repeat(n / 2);
     const first = triangle.map((row) => empty + row + empty);
     const second = triangle.map((row) => row + ' ' + row);
-    const res = [...first, ...second].join('\n');
-    return res;
+    return [...first, ...second].join('\n');
   }
 };
 
